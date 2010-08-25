d417c8325d62bca45f786eeb1cbac070c71ebeac
gestes <gestes@aldebaran-robotics.com>
2010-08-25 10:01:36 +0200

    test_transport: better display

--- test/test_transport/main.cpp
+++ test/test_transport/main.cpp
@@ -15,6 +15,7 @@
 #include <alcommon-ng/transport/transport.hpp>
 #include <alcommon-ng/transport/zeromq/zmqsimpleserver.hpp>
 #include <boost/shared_ptr.hpp>
+#include <boost/timer.hpp>
 
 #ifdef _WIN32
     // CK 28/7/2010 dodgy hack so it compiles
@@ -31,10 +32,19 @@ static const int gLoopCount   = 100000;
 //TCP: 6.6
 //IPC: 6sec => 100000  => 16000 msg/sec
 
+//inproc
+//./sdk/bin/test_transport  0.64s user 0.86s system 67% cpu 1.213 total
+//ipc
+//./sdk/bin/test_transport  1.77s user 2.92s system 103% cpu 3.554 total
+//tcp
+//./sdk/bin/test_transport  1.73s user 3.96s system 106% cpu 4.316 total
+
+
 //static const std::string gServerAddress = "tcp://127.0.0.1:5555";
-static const std::string gServerAddress = "ipc:///tmp/test";
-//static const std::string gServerAddress = "inproc://workers";
+//static const std::string gServerAddress = "ipc:///tmp/test";
+static const std::string gServerAddress = "inproc://workers";
 static const std::string gClientAddress = gServerAddress;
+
 
 //class ResultHandler;
 class TestServer : public AL::Transport::Threadable, public AL::Transport::DataHandler
@@ -68,9 +78,6 @@ int main_server()
 {
   TestServer                server(gServerAddress);
   server.run();
-//  boost::thread             threadServer(boost::bind(&TestServer::run, &server));
-//  while(1)
-//    sleep(5);
   return 0;
 }
 
@@ -81,13 +88,30 @@ int main_client(int clientId)
   std::string            tosend = "bim";
   std::string            torecv;
 
-  for (int i = 0; i< gLoopCount; ++i)
+  std::cout << "Bytes, msg/s, MB/s" << std::endl;
+
+  for (int i = 0; i < 12; ++i)
   {
-    tosend = "bim";
-    torecv = "";
-    client->send(tosend, torecv);
-    assert(tosend == torecv);
+    unsigned int numBytes = (unsigned int)pow(2.0f,(int)i);
+    std::string  request = std::string(numBytes, 'B');
+    boost::timer t;
+    double       elapsed;
+
+    t.restart();
+    for (int j = 0; j< gLoopCount; ++j)
+    {
+      torecv = "";
+      client->send(request, torecv);
+      //assert(tosend == torecv);
+    }
+
+    //
+    elapsed = t.elapsed();
+    float msgPs = 1.0f / ((float)elapsed / (1.0f * gLoopCount) );
+    float mgbPs = (msgPs * numBytes) / (1024 * 1024.0f);
+    std::cout << numBytes << ", " << msgPs << ", " << mgbPs << std::endl;
   }
+  //
   return 0;
 }
 
@@ -115,7 +139,7 @@ int main(int argc, char **argv)
   else
   {
     boost::thread             threadServer(&main_server);
-    //sleep(1);
+    sleep(1);
     boost::thread             threadClient(boost::bind(&main_client, 0));
     threadClient.join();
     //sleep(1);
