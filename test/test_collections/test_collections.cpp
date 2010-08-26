b5b3c726bb8e5dc5541ad1fdfde54566c149fa95
ckilner <ckilner@aldebaran-robotics.com>
2010-08-26 13:57:54 +0200

    ALValue basic tests

--- test/test_collections/test_collections.cpp
+++ test/test_collections/test_collections.cpp
@@ -64,12 +64,79 @@ TEST(VariableValueTest, ToAndFrom_vector_of_VariableValue)
   EXPECT_EQ(s, res);
 }
 
-TEST(VariableValueTest, DISABLED_ToAndFrom_vector_of_float)
+// ---
+
+TEST(VariableValueConvertALValue, ToAndFrom_int)
 {
-  std::vector<float> s;
-  s.push_back((float)0.2f);
+  AL::ALValue s = 1;
   VariableValue v = s;
-  //std::vector<float> res = v;  // doesn't work
-  //EXPECT_EQ(s, res); 
+  AL::ALValue res = v.convertToALValue();
+  EXPECT_EQ(s, res);
 }
+
+TEST(VariableValueConvertALValue, ToAndFrom_float)
+{
+  AL::ALValue s = 0.2f;
+  VariableValue v = s;
+  AL::ALValue res = v.convertToALValue();
+  EXPECT_EQ(s, res);
+}
+
+TEST(VariableValueConvertALValue, ToAndFrom_double)
+{
+  AL::ALValue s = 39709080.97987979;
+  VariableValue v = s;
+  AL::ALValue res = v.convertToALValue();
+  EXPECT_EQ(s, res);
+}
+
+TEST(VariableValueConvertALValue, ToAndFrom_bool)
+{
+  AL::ALValue s = true;
+  VariableValue v = s;
+  AL::ALValue res = v.convertToALValue();
+  EXPECT_EQ(s, res);
+}
+
+TEST(VariableValueConvertALValue, ToAndFrom_string)
+{
+  AL::ALValue s("hello world");
+  VariableValue v = s;
+  AL::ALValue res = v.convertToALValue();
+  EXPECT_EQ(s, res);
+}
+
+TEST(VariableValueConvertALValue, ToAndFrom_vector_of_unsigned_char)
+{
+  AL::ALValue s;
+  std::vector<unsigned char> s1;
+  s1.push_back((unsigned char)1);
+  s.SetBinary(&s1,s1.size());
+  VariableValue v = s;
+  AL::ALValue res = v.convertToALValue();
+  EXPECT_EQ(s, res);
+}
+
+TEST(VariableValueConvertALValue, ToAndFrom_mixedArray)
+{
+  AL::ALValue s;
+  s.arrayPush((int)1);
+  s.arrayPush(std::string("Hello"));
+  VariableValue v = s;
+  AL::ALValue res = v.convertToALValue();
+  EXPECT_EQ(s, res);
+}
+
+TEST(VariableValueConvertALValue, ToAndFrom_vector_float)
+{
+  std::vector<float> f;
+  f.push_back(1.0f);
+  f.push_back(2.1f);
+  AL::ALValue s = f;
+  VariableValue v = s;
+  AL::ALValue res = v.convertToALValue();
+  std::vector<float> ff = res;
+  EXPECT_EQ(f, ff);
+}
+
 
