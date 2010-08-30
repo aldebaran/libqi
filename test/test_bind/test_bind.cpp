998fe78b3c4f0b2501c957033ddc4430de9a3788
gestes <gestes@aldebaran-robotics.com>
2010-08-30 17:03:47 +0200

    test_bind: more test (testier, tastier) ohhh tasty

--- test/test_bind/test_bind.cpp
+++ test/test_bind/test_bind.cpp
@@ -9,6 +9,7 @@
 #include <map>
 #include <alcommon-ng/functor/functor.hpp>
 #include <alcommon-ng/functor/makefunctor.hpp>
+#include <alcommon-ng/functor/callfunctor.hpp>
 #include <alcommon-ng/tools/dataperftimer.hpp>
 #include <cmath>
 
@@ -18,33 +19,67 @@ using AL::Messaging::ReturnValue;
 using AL::Messaging::ArgumentList;
 
 
-int fun0()                                               { return 0; }
-int fun1(int p0)                                         { return p0; }
-int fun2(int p0, int p1)                                 { return p0 + p1; }
-int fun3(int p0, int p1, int p2)                         { return p0 + p1 + p2; }
-int fun4(int p0, int p1, int p2, int p3)                 { return p0 + p1 + p2 + p3; }
-int fun5(int p0, int p1, int p2, int p3, int p4)         { return p0 + p1 + p2 + p3 + p4; }
-int fun6(int p0, int p1, int p2, int p3, int p4, int p5) { return p0 + p1 + p2 + p3 + p4 + p5; }
+int fun0()                                                                                      { return 0; }
+int fun1(const int &p0)                                                                         { return p0; }
+int fun2(const int &p0,const int &p1)                                                           { return p0 + p1; }
+int fun3(const int &p0,const int &p1,const int &p2)                                             { return p0 + p1 + p2; }
+int fun4(const int &p0,const int &p1,const int &p2,const int &p3)                               { return p0 + p1 + p2 + p3; }
+int fun5(const int &p0,const int &p1,const int &p2,const int &p3,const  int &p4)                { return p0 + p1 + p2 + p3 + p4; }
+int fun6(const int &p0,const int &p1,const int &p2,const int &p3,const  int &p4,const  int &p5) { return p0 + p1 + p2 + p3 + p4 + p5; }
 
 struct Foo {
   void voidCall()                                          { return; }
   int intStringCall(const std::string &plouf)              { return plouf.size(); }
 
-  int fun0()                                               { return 0; }
-  int fun1(int p0)                                         { return p0; }
-  int fun2(int p0, int p1)                                 { return p0 + p1; }
-  int fun3(int p0, int p1, int p2)                         { return p0 + p1 + p2; }
-  int fun4(int p0, int p1, int p2, int p3)                 { return p0 + p1 + p2 + p3; }
-  int fun5(int p0, int p1, int p2, int p3, int p4)         { return p0 + p1 + p2 + p3 + p4; }
-  int fun6(int p0, int p1, int p2, int p3, int p4, int p5) { return p0 + p1 + p2 + p3 + p4 + p5; }
+  int fun0()                                                                                      { return 0; }
+  int fun1(const int &p0)                                                                         { return p0; }
+  int fun2(const int &p0,const int &p1)                                                           { return p0 + p1; }
+  int fun3(const int &p0,const int &p1,const int &p2)                                             { return p0 + p1 + p2; }
+  int fun4(const int &p0,const int &p1,const int &p2,const int &p3)                               { return p0 + p1 + p2 + p3; }
+  int fun5(const int &p0,const int &p1,const int &p2,const int &p3,const  int &p4)                { return p0 + p1 + p2 + p3 + p4; }
+  int fun6(const int &p0,const int &p1,const int &p2,const int &p3,const  int &p4,const  int &p5) { return p0 + p1 + p2 + p3 + p4 + p5; }
 };
 
 
-TEST(TestBind, ArgumentNumber) {
+TEST(TestBind, MultiArgMember) {
   Foo          foo;
-  AL::Functor *functor = AL::makeFunctor(&foo, &Foo::fun0);
-  //EXPECT_EQ(0, functor->call());
+  AL::Functor *functor;
+
+  functor = AL::makeFunctor(&foo, &Foo::fun0);
+  EXPECT_EQ(0 , AL::callFunctor<int>(functor));
+  functor = AL::makeFunctor(&foo, &Foo::fun1);
+  EXPECT_EQ(1 , AL::callFunctor<int>(functor, 1));
+  functor = AL::makeFunctor(&foo, &Foo::fun2);
+  EXPECT_EQ(3 , AL::callFunctor<int>(functor, 1, 2));
+  functor = AL::makeFunctor(&foo, &Foo::fun3);
+  EXPECT_EQ(6 , AL::callFunctor<int>(functor, 1, 2, 3));
+  functor = AL::makeFunctor(&foo, &Foo::fun4);
+  EXPECT_EQ(10, AL::callFunctor<int>(functor, 1, 2, 3, 4));
+  functor = AL::makeFunctor(&foo, &Foo::fun5);
+  EXPECT_EQ(15, AL::callFunctor<int>(functor, 1, 2, 3, 4, 5));
+  functor = AL::makeFunctor(&foo, &Foo::fun6);
+  EXPECT_EQ(21, AL::callFunctor<int>(functor, 1, 2, 3, 4, 5, 6));
 }
+
+TEST(TestBind, MultiArgFun) {
+  AL::Functor *functor;
+
+  functor = AL::makeFunctor(&fun0);
+  EXPECT_EQ(0 , AL::callFunctor<int>(functor));
+  functor = AL::makeFunctor(&fun1);
+  EXPECT_EQ(1 , AL::callFunctor<int>(functor, 1));
+  functor = AL::makeFunctor(&fun2);
+  EXPECT_EQ(3 , AL::callFunctor<int>(functor, 1, 2));
+  functor = AL::makeFunctor(&fun3);
+  EXPECT_EQ(6 , AL::callFunctor<int>(functor, 1, 2, 3));
+  functor = AL::makeFunctor(&fun4);
+  EXPECT_EQ(10, AL::callFunctor<int>(functor, 1, 2, 3, 4));
+  functor = AL::makeFunctor(&fun5);
+  EXPECT_EQ(15, AL::callFunctor<int>(functor, 1, 2, 3, 4, 5));
+  functor = AL::makeFunctor(&fun6);
+  EXPECT_EQ(21, AL::callFunctor<int>(functor, 1, 2, 3, 4, 5, 6));
+}
+
 
 TEST(TestBind, VoidCallPerf) {
   Foo           chiche;
