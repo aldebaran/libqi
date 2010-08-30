d60b418f746416d2211ec3ace509e98b8c437323
gestes <gestes@aldebaran-robotics.com>
2010-08-30 10:34:07 +0200

    added tools/sleep.hpp

---
+++ tools/sleep.hpp
@@ -0,0 +1,17 @@
+/*
+** Author(s):
+**  - Cedric GESTES <gestes@aldebaran-robotics.com>
+**
+** Copyright (C) 2010 Aldebaran Robotics
+*/
+
+#ifndef    AL_MESSAGING_SLEEP_HXX_
+# define   AL_MESSAGING_SLEEP_HXX_
+
+#ifdef _WIN32
+#  define sleep(x) Sleep(x * 1000)
+#else
+#  include <unistd.h>
+#endif
+
+#endif    /* !AL_MESSAGING_SLEEP_HXX_ */
