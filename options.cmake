a2c87fff5b8b914955bfcc9d95562d786c61590e
gestes <gestes@aldebaran-robotics.com>
2010-08-31 19:04:39 +0200

    added a protobuf test

--- options.cmake
+++ options.cmake
@@ -5,6 +5,7 @@
 ## Copyright (C) 2010 Aldebaran Robotics
 ##
 
-option(WITH_THRIFT "Enable the use of thrift" OFF)
-option(WITH_ZMQ    "Enable the use of ZMQ"    ON)
-option(WITH_SHM    "Enable the use of shm"    OFF)
+option(WITH_THRIFT   "Enable the use of thrift"   OFF)
+option(WITH_ZMQ      "Enable the use of ZMQ"      ON)
+option(WITH_SHM      "Enable the use of shm"      OFF)
+option(WITH_PROTOBUF "Enable the use of protobuf" OFF)
---
+++ test/test_protobuf_serialization/main.cpp
@@ -0,0 +1,15 @@
+/*
+** Author(s):
+**  - Cedric GESTES <gestes@aldebaran-robotics.com>
+**
+** Copyright (C) 2010 Aldebaran Robotics
+*/
+
+#include <iostream>
+#include <gtest/gtest.h>
+#include <protobuf/message.h>
+
+
+TEST(TestProtoSerialization, Basic) {
+  std::cout << "ProtoTest" << std::endl;
+}
