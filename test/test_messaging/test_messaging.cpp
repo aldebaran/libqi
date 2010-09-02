5ee0b4c40db5889d0054f97ca79a71a7c83014e2
ckilner <ckilner@aldebaran-robotics.com>
2010-09-02 13:36:17 +0200

    Added messaging ping and echo tests

--- test/test_messaging/test_messaging.cpp
+++ test/test_messaging/test_messaging.cpp
@@ -19,12 +19,8 @@
 using namespace AL::Messaging;
 using AL::Test::DataPerfTimer;
 
-
-//using AL::ALPtr;
-
 static const int gThreadCount = 10;
 static const int gLoopCount   = 10000;
-
 
 class ServiceHandler :  public MessageHandler
 {
@@ -34,27 +30,27 @@ public:
   {
     boost::shared_ptr<AL::Messaging::ResultDefinition> res(new AL::Messaging::ResultDefinition());
 
-    if (def.methodName() == "test2")
+    if (def.methodName() == "ping")
     {
-      //printf("Method: test2\n");
-      std::vector<AL::Messaging::VariableValue>    params = def.args();
-      res->value((int)params.front().as<std::string>().size());
+      // do nothing
+    }
+
+    if (def.methodName() == "size")
+    {
+      res->value((int)def.args().front().as<std::string>().size());
     }
 
     else if (def.methodName() == "echo")
     {
-      std::cout << "Method: Echo" << std::endl;
-      std::vector<AL::Messaging::VariableValue>    params = def.args();
-      res->value(params.front());
+      std::string result = def.args().front().as<std::string>();
+      res->value(result);
     }
     return res;
   }
 };
 
-
 static const std::string gServerAddress = "tcp://127.0.0.1:5555";
 static const std::string gClientAddress = "tcp://127.0.0.1:5555";
-
 
 int main_server()
 {
@@ -74,26 +70,44 @@ int main_client(int clientId)
   client.connect(gClientAddress);
   AL::Messaging::ResultDefinition res;
 
-  DataPerfTimer dt;
+  DataPerfTimer dt("Messaging void -> ping -> void");
+  dt.start(gLoopCount);
+  for (int j = 0; j< gLoopCount; ++j)
+  {
+    client.send(CallDefinition("ping"));
+  }
+  dt.stop();
 
+  dt.printHeader("Messaging string -> size -> int");
   for (int i = 0; i < 12; ++i)
   {
     unsigned int                  numBytes = (unsigned int)pow(2.0f,(int)i);
     std::string                   request = std::string(numBytes, 'B');
-    AL::Messaging::CallDefinition def;
-
-    def.methodName() = "test2";
-    def.args().push_back(request);
 
     dt.start(gLoopCount, numBytes);
     for (int j = 0; j< gLoopCount; ++j)
     {
-      res = client.send(def);
+      res = client.send(CallDefinition("size", request));
+      int size = res.value().as<int>();
       //assert(tosend == torecv);
     }
     dt.stop();
   }
 
+  dt.printHeader("Messaging string -> echo -> string");
+  for (int i = 0; i < 12; ++i)
+  {
+    unsigned int                  numBytes = (unsigned int)pow(2.0f,(int)i);
+    std::string                   request = std::string(numBytes, 'B');
+    dt.start(gLoopCount, numBytes);
+    for (int j = 0; j< gLoopCount; ++j)
+    {
+      AL::Messaging::CallDefinition def("echo", std::string(request));
+      res = client.send(def);
+      std::string result = res.value().as<std::string>();
+    }
+    dt.stop();
+  }
 
   return 0;
 }
