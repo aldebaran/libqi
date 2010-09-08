5f20fd8b3355243f110840001b2505cdaf71d5c8
gestes <gestes@aldebaran-robotics.com>
2010-09-08 19:10:43 +0200

    test boost::mpl

--- test/test_signature/test_signature.cpp
+++ test/test_signature/test_signature.cpp
@@ -106,6 +106,224 @@ TEST(TestSignature, ComplexTypeSignature) {
 
 }
 
+#include <boost/function_types/result_type.hpp>
+#include <boost/function_types/parameter_types.hpp>
+#include <boost/function_types/function_arity.hpp>
+
+#include <boost/fusion/algorithm/iteration/for_each.hpp>
+#include <boost/fusion/include/for_each.hpp>
+
+
+#include <boost/mpl/string.hpp>
+#include <boost/mpl/vector.hpp>
+#include <boost/mpl/int.hpp>
+//#include <boost/type_traits/is_float.hpp>
+#include <boost/fusion/include/value_of.hpp>
+#include <boost/utility.hpp>
+template <typename T>
+struct newTypeSignature {
+  typedef boost::mpl::string<'UN', 'KN', 'OW', 'N'> value;
+};
+
+template <>
+struct newTypeSignature<void>  {
+  typedef boost::mpl::string<'v'> value;
+};
+
+template <>
+struct newTypeSignature<bool>  {
+  typedef boost::mpl::string<'b'> value;
+};
+
+template <>
+struct newTypeSignature<char> {
+  typedef boost::mpl::string<'c'> value;
+};
+
+template <>
+struct newTypeSignature<int> {
+  typedef boost::mpl::string<'i'> value;
+};
+
+//TYPE QUALIFIER
+template <typename T>
+struct newTypeSignature<T*> {
+  typedef typename boost::mpl::copy<boost::mpl::string<'*'>::type,
+                                    boost::mpl::back_inserter< typename newTypeSignature<T>::value >
+                                   >::type value;
+};
+
+template <typename T>
+struct newTypeSignature<T&> {
+  typedef typename boost::mpl::copy<boost::mpl::string<'&'>::type,
+                                    boost::mpl::back_inserter< typename newTypeSignature<T>::value >
+                                   >::type value;
+};
+
+template <typename T>
+struct newTypeSignature<const T> {
+  typedef typename boost::mpl::copy<boost::mpl::string<'#'>::type,
+                                    boost::mpl::back_inserter< typename newTypeSignature<T>::value >
+                                   >::type value;
+};
+
+
+template <typename F>
+struct newFunctionSignature {
+  typedef typename newTypeSignature< typename boost::function_types::result_type<F>::type >::value returnValue;
+
+  typedef typename boost::mpl::copy<boost::mpl::string<':'>::type,
+                                    boost::mpl::back_inserter< returnValue >
+                                   >::type returnValueColon;
+
+  typedef typename boost::function_types::parameter_types<F>::type ArgsType;
+
+  //const int val = boost::function_types::function_arity<F>::value arity;
+
+
+//  typedef typename boost::mpl::fold<ArgsType,
+//                                    typename boost::mpl::string<>,
+//                                    typename boost::mpl::copy<newTypeSignature< boost::mpl::_2 >::value,
+//                                                              boost::mpl::back_inserter< boost::mpl::_1 >
+//                                                             >
+//                          >::type argsValue;
+
+
+  typedef typename boost::mpl::iter_fold<ArgsType,
+                                    typename boost::mpl::string<>,
+                                    typename boost::mpl::copy<newTypeSignature< typename boost::mpl::deref<boost::mpl::_2 > >::value,
+                                                              boost::mpl::back_inserter< boost::mpl::_1 >
+                                                             >
+                          >::type argsValue;
+
+
+//  typedef if_< boost::mpl::bigger_than<arity, 0> ,
+//               returnValueColon,
+//               typename boost::mpl::copy< newTypeSignature< boost::mpl::at<ArgsType, boost::mpl::int_<1> >::type >::value,
+//                                          boost::mpl::back_inserter< returnValueColon >
+//                                        >::type value;
+
+
+
+  typedef typename boost::mpl::copy<argsValue,
+                                    boost::mpl::back_inserter< returnValueColon >
+                                   >::type value;
+
+
+//  typedef iter_fold<
+//        s
+//      , state
+//      , apply_wrap2< lambda<op>::type, _1, deref<_2> >
+//      >::type t;
+//  typedef begin<s>::type i1;
+//  typedef apply<op,state,i1>::type state1;
+//  typedef next<i1>::type i2;
+//  typedef apply<op,state1,i2>::type state2;
+//  ...
+//  typedef apply<op,staten-1,in>::type staten;
+//  typedef next<in>::type last;
+//  typedef staten t;
+};
+
+
+
+TEST(TestSignature, TOTO) {
+  //boost::function_types<F>::
+  //boost::remove_pointer<T>::type;
+  std::string sign;
+
+  typedef newTypeSignature<bool>::value va;
+  std::cout << "new type:" << boost::mpl::c_str<va>::value << std::endl;
+
+  typedef newFunctionSignature<void (int, bool)>::value funva;
+  std::cout << "new func:" << boost::mpl::c_str<funva>::value << std::endl;
+
+
+  typedef boost::mpl::string<'t1'>::type t1;
+  typedef boost::mpl::string<'t2'>::type t2;
+  typedef boost::mpl::push_back<t1, boost::mpl::char_<'3'> >::type t3;
+  typedef boost::mpl::insert_range<t1, boost::mpl::end<t1>::type, t2>::type t4;
+  typedef boost::mpl::string<>::type t5;
+
+  typedef boost::mpl::copy< t2, boost::mpl::back_inserter< t1 > >::type t6;
+
+
+  std::cout << "t1:" << boost::mpl::c_str<t1>::value << std::endl;
+  std::cout << "t2:" << boost::mpl::c_str<t2>::value << std::endl;
+  std::cout << "t :" << boost::mpl::c_str<t3>::value << std::endl;
+  std::cout << "t :" << boost::mpl::c_str<t4>::value << std::endl;
+  std::cout << "t :" << boost::mpl::c_str<t5>::value << std::endl;
+  std::cout << "t6:" << boost::mpl::c_str<t6>::value << std::endl;
+
+  typedef boost::mpl::vector<long,float,short,double,float,long,long double> types;
+  typedef boost::mpl::fold<
+        types
+      , boost::mpl::int_<0>
+      , boost::mpl::if_< boost::is_float< boost::mpl::_2>, boost::mpl::next< boost::mpl::_1>, boost::mpl::_1 >
+      >::type number_of_floats;
+
+  BOOST_MPL_ASSERT_RELATION( number_of_floats::value, ==, 4 );
+
+  typedef boost::mpl::vector<bool, int, bool> paf;
+  //result_of::value_of<
+
+  typedef boost::mpl::at<paf, boost::mpl::int_<1> >::type toto;
+
+  //typedef if_<true_,char,long>::type t1;
+
+  typedef newTypeSignature< toto >::value res;
+  std::cout << "t1:" << boost::mpl::c_str<res>::value << std::endl;
+
+
+//  typedef boost::mpl::fold<types,
+//                           boost::mpl::string<>,
+//                           boost::mpl::insert_range< boost::mpl::_1,
+//                                                     boost::mpl::end<boost::mpl::_1>::type,
+//                                                     boost::mpl::string<'ca'>::type >
+//                          >::type argsValue;
+
+
+  typedef boost::mpl::fold<types,
+                           typename boost::mpl::string<>,
+                           typename boost::mpl::copy< boost::mpl::_2, boost::mpl::back_inserter< boost::mpl::_1 > >
+                          >::type argsValue;
+
+//  typedef begin<s>::type i1;
+//  typedef apply<op,state,i1>::type state1;
+//  typedef next<i1>::type i2;
+//  typedef apply<op,state1,i2>::type state2;
+//  ...
+//  typedef apply<op,staten-1,in>::type staten;
+//  typedef next<in>::type last;
+//  typedef staten t;
+
+
+//  typedef list_c<int,5,-1,0,-7,-2,0,-5,4> numbers;
+//  typedef list_c<int,-1,-7,-2,-5> negatives;
+//  typedef reverse_fold<
+//        numbers
+//      , list_c<int>
+//      , if_< less< _2,int_<0> >, push_front<_1,_2,>, _1 >
+//      >::type result;
+
+//  BOOST_MPL_ASSERT(( equal< negatives,result > ));
+
+
+
+//  using namespace boost::mpl;
+//  typedef vector_c<int,5,-1,0,7,2,0,-5,4> numbers;
+//  typedef iter_fold<
+//        numbers
+//      , begin<numbers>::type
+//      , if_< less< deref<boost::mpl::_1>, deref<boost::mpl::_2> >,boost::mpl::_2,_1 >
+//      >::type max_element_iter;
+
+//  BOOST_MPL_ASSERT_RELATION( deref<max_element_iter>::type::value, ==, 7 );
+
+
+}
+
+
 TEST(TestSignature, BasicVoidFunctionSignature) {
   EXPECT_EQ("v:"      , AL::functionSignature(&vfun0));
   EXPECT_EQ("v:i"     , AL::functionSignature(&vfun1));
