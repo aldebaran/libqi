e465605b3c57d555f9ea31205dbdb93f48a7526e
Cedric GESTES <gestes@aldebaran-robotics.com>
2010-11-14 22:42:42 +0100

    test/test_color: little tweak

--- qi/test/test_log/test_log.cpp
+++ qi/test/test_log/test_log.cpp
@@ -1,16 +1,28 @@
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
+
 #include <qi/log.hpp>
+#include <qi/log/consoleloghandler.hpp>
+
+//using qi::log::ConsoleLogHandler;
 
 int main(int argc, char **argv)
 {
-  qiFatal("fatal: %d\n", 42);
-  qiError("error: %d\n", 42);
-  qiWarning("warning: %d\n", 42);
-  qiInfo("info: %d\n", 42);
-  qiDebug("debug: %d\n", 42);
+  //ConsoleLogHandler clh;
+  //qi::log::LogFunctionPtr p = boost::bind<void>(&ConsoleLogHandler::log, &clh, _1, _2, _3, _4, _5, _6);
+  //boost::function6<void, qi::log::LogLevel, const char *, const char *, int, const char*, va_list> fn;
+  //fn = boost::bind<void>(&ConsoleLogHandler::log, &clh, _1, _2, _3, _4, _5, _6);
+  //qi::log::setLogHandler(fn);
 
-  qisFatal   << "fatal" << 42 << std::endl;
-  qisError   << "error" << 42 << std::endl;
-  qisWarning << "warning" << 42 << std::endl;
-  qisInfo    << "info" << 42 << std::endl;
-  qisDebug   << "debug" << 42 << std::endl;
+  qiFatal("%d\n", 42);
+  qiError("%d\n", 42);
+  qiWarning("%d\n", 42);
+  qiInfo("%d\n", 42);
+  qiDebug("%d\n", 42);
+
+  qisFatal   << "f" << 42 << std::endl;
+  qisError   << "e" << 42 << std::endl;
+  qisWarning << "w" << 42 << std::endl;
+  qisInfo    << "i" << 42 << std::endl;
+  qisDebug   << "d" << 42 << std::endl;
 }
