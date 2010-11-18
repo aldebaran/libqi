9f1ad90819b05def36601693e25716ce4da6fa8d
ckilner <ckilner@aldebaran-robotics.com>
2010-11-19 00:17:32 +0100

    Serialization cleaned up header

--- qi/serialization/serializeddata.hxx
+++ qi/serialization/serializeddata.hxx
@@ -1,31 +1,16 @@
 /*
-** serializeddata.hxx
-** Login : <ctaf42@cgestes-de2>
-** Started on  Wed Nov 17 19:46:49 2010 Cedric GESTES
-** $Id$
-**
 ** Author(s):
 **  - Cedric GESTES <gestes@aldebaran-robotics.com>
+**  - Chris KILNER  <ckilner@aldebaran-robotics.com>
 **
-** Copyright (C) 2010 Cedric GESTES
-** This program is free software; you can redistribute it and/or modify
-** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation; either version 3 of the License, or
-** (at your option) any later version.
-**
-** This program is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-** GNU General Public License for more details.
-**
-** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software
-** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+** Copyright (C) 2010 Aldebaran Robotics
 */
+
 
 #ifndef   __QI_SERIALIZATION_SERIALIZEDDATA_HXX__
 #define   __QI_SERIALIZATION_SERIALIZEDDATA_HXX__
 
+//#include <vector>
 
 namespace qi {
   namespace serialization {
