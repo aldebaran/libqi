f398bea9ef148d6494a2c79ffd282fe54b5a44ea
ckilner <ckilner@aldebaran-robotics.com>
2010-11-18 23:05:46 +0100

    Add server doc

--- qi/messaging/server.hpp
+++ qi/messaging/server.hpp
@@ -28,7 +28,8 @@ namespace qi {
     /// <summary> Finaliser. </summary>
     virtual ~Server();
 
-    /// <summary> Default constructor. </summary>
+    /// <summary> Constructs a Server object that can be used to
+    /// advertise services to clients. </summary>
     /// <param name="serverName"> Name of the server. </param>
     /// <param name="serverAddress"> The server address. </param>
     /// <param name="masterAddress"> The master address. </param>
@@ -36,11 +37,44 @@ namespace qi {
                const std::string& serverAddress,
                const std::string& masterAddress);
 
+    /// <summary>
+    /// Makes an object's method available as a service, and registers
+    /// this service with the master so that clients can find it.
+    ///
+    /// The method's call signature is found by introspection and
+    /// combined with the service name to create a service signature.
+    /// This signature is used to find your method and to serialize
+    /// and deserialize outgoing and incoming data.
+    ///
+    /// e.g. server.addService("hello", this, &MyClass::helloMethod);
+    ///
+    /// e.g. server.addService("hello", &myObject, &MyObject::helloMethod);
+    /// </summary>
+    /// <param name="name"> The advertised name of the service.</param>
+    /// <param name="object"> The memory address of the object. This could be
+    /// a "this" pointer if you are adding a method of your class,
+    /// or a "&myObject" for the address of a member object.</param>
+    /// <param name="method"> The memory address of the method. This should fully
+    /// qualify the type of your method such as "&MyClass::myMethod" or
+    /// "&MyObject::objectMethod".</param>
     template <typename OBJECT_TYPE, typename METHOD_TYPE>
     void addService(const std::string& name, OBJECT_TYPE object, METHOD_TYPE method) {
       xAddService(makeSignature(name, method), makeFunctor(object, method));
     }
 
+    /// <summary>
+    /// Makes a function available as a service, and registers
+    /// this service with the master so that clients can find it.
+    ///
+    /// The method's call signature is found by introspection and
+    /// combined with the service name to create a service signature.
+    /// This signature is used to find your method and to serialize
+    /// and deserialize outgoing and incoming data.
+    ///
+    /// e.g. server.addService("hello", &helloFunction);
+    /// </summary>
+    /// <param name="name"> The advertised name of the service.</param>
+    /// <param name="function"> The memory address of the function. e.g. "&globalFunction"</param>
     template <typename FUNCTION_TYPE>
     void addService(const std::string& name, FUNCTION_TYPE function) {
       xAddService(makeSignature(name, function), makeFunctor(function));
