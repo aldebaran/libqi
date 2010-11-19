9286031ac11ac4a24bc0ef1e2f460ada09e07a97
gestes <gestes@aldebaran-robotics.com>
2010-11-19 19:04:42 +0100

    qitool: list all methods

--- bin/qi/qimain.cpp
+++ bin/qi/qimain.cpp
@@ -6,6 +6,7 @@
 */
 
 #include <iostream>
+#include <map>
 #include <qi/messaging.hpp>
 #include <qi/perf/sleep.hpp>
 
@@ -18,10 +19,18 @@ void qi_call(const char *addr) {
   qi::Client client("clicli", std::string(addr));
 
   //client.call("master.listServices::{ss}")
-  std::string locatethismethod("master.listServices::{ss}:");
 
-  std::string result = client.call<std::string>("master.locateService", locatethismethod);
-  std::cout << "result:" << result << std::endl;
+  std::map<std::string, std::string>                 mymap;
+  std::map<std::string, std::string>::const_iterator it;
+
+  mymap = client.call< std::map<std::string, std::string> >("master.listServices");
+  for (it = mymap.begin(); it != mymap.end(); ++it)
+  {
+    std::cout << it->first << " :" << it->second << std::endl;
+  }
+//  std::string locatethismethod("master.listServices::{ss}:");
+//  std::string result = client.call<std::string>("master.locateService", locatethismethod);
+//  std::cout << "result:" << result << std::endl;
 }
 
 int main(int argc, char *argv[])
