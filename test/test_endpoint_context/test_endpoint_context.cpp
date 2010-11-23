a7d13cd1940400cf2527529653344a9b92d6bbf4
ckilner <ckilner@aldebaran-robotics.com>
2010-11-23 19:08:03 +0100

    Cleanup ip tests

--- test/test_endpoint_context/test_endpoint_context.cpp
+++ test/test_endpoint_context/test_endpoint_context.cpp
@@ -89,40 +89,49 @@ TEST(EndpointContext, getIPAddresses)
   for (unsigned int i = 0; i < ips.size(); ++i) {
     std::cout << "IP: " << ips[i] << std::endl;
   }
-  //ASSERT_NE(ip, std::string())  << "IP Address was empty";
+  ASSERT_NE(0, ips.size())  << "No IP Addresses returned";
 }
 
-TEST(EndpointContext, isValidAddress)
+TEST(EndpointContext, isValidAddressRetValue)
 {
   std::pair<std::string, std::string> res;
-  std::string test = "ckilner-linux-po:5555";
+  std::string test = "127.0.0.1:5555";
   bool b = qi::detail::isValidAddress(test, res);
-  std::cout << test << " " << b <<  " " << res.first << ":" << res.second << std::endl;
-
-  test = "bollocks";
-  res.first = "";
-  res.second = "";
-  b = qi::detail::isValidAddress(test, res);
-  std::cout << test << " " << b <<  " " << res.first << ":" << res.second << std::endl;
-
-  test = "";
-  res.first = "";
-  res.second = "";
-  b = qi::detail::isValidAddress(test, res);
-  std::cout << test << " " << b <<  " " << res.first << ":" << res.second << std::endl;
-
-  test = "127.0.0.1:ouoiu";
-  res.first = "";
-  res.second = "";
-  b = qi::detail::isValidAddress(test, res);
-  std::cout << test << " " << b <<  " " << res.first << ":" << res.second << std::endl;
-
-  test = "172.16.1.12:8080";
-  res.first = "";
-  res.second = "";
-  b = qi::detail::isValidAddress(test, res);
-  std::cout << test << " " << b <<  " " << res.first << ":" << res.second << std::endl;
+  ASSERT_EQ(true, b) << "127.0.0.1:5555 was considered invalid";
 }
+
+TEST(EndpointContext, isValidAddressIP)
+{
+  std::pair<std::string, std::string> res;
+  std::string test = "127.0.0.1:5555";
+  bool b = qi::detail::isValidAddress(test, res);
+  ASSERT_EQ("127.0.0.1", res.first) << "Should have returned an IP";
+}
+
+TEST(EndpointContext, isValidAddressPort)
+{
+  std::pair<std::string, std::string> res;
+  std::string test = "127.0.0.1:5555";
+  bool b = qi::detail::isValidAddress(test, res);
+  ASSERT_EQ("5555", res.second) << "Should have returned a port";
+}
+
+TEST(EndpointContext, isValidAddressRetValueInvalid)
+{
+  std::pair<std::string, std::string> res;
+  std::string test = "127.0.0.655:5555";
+  bool b = qi::detail::isValidAddress(test, res);
+  ASSERT_EQ(false, b) << "127.0.0.655:5555 was considered valid";
+}
+
+TEST(EndpointContext, isValidAddressIPRubbish)
+{
+  std::pair<std::string, std::string> res;
+  std::string test = "rubbish";
+  bool b = qi::detail::isValidAddress(test, res);
+   ASSERT_EQ(false, b) << "rubbish was considered valid";
+}
+
 
 TEST(EndpointContext, EndpointContext)
 {
@@ -172,56 +181,17 @@ TEST(EndpointContextPerf, 10000getUUID)
   }
 }
 
-TEST(EndpointContextPerf, 10000PrimaryPublicIPAddress)
-{
-  std::string s;
-  for(unsigned int i=0; i<10000; i++) {
-    s = getPrimaryPublicIPAddress();
-  }
-}
+//TEST(EndpointContextPerf, 10000PrimaryPublicIPAddress)
+//{
+//  std::string s;
+//  for(unsigned int i=0; i<10000; i++) {
+//    s = getPrimaryPublicIPAddress();
+//  }
+//}
 
 // --------------------------------------- GET IP ADDRESSES WIN
-//
-//#include <winsock2.h>
-//
-//// Add 'ws2_32.lib' to your linker options
-//
-//WSADATA WSAData;
-//
-//// Initialize winsock dll
-//if(::WSAStartup(MAKEWORD(1, 0), &WSAData))
-//{
-//  // Error handling
-//}
-//
-//// Get local host name
-//char szHostName[128] = "";
-//
-//if(::gethostname(szHostName, sizeof(szHostName)))
-//{
-//  // Error handling -> call 'WSAGetLastError()'
-//}
-//
-//// Get local IP addresses
-//struct sockaddr_in SocketAddress;
-//struct hostent     *pHost        = 0;
-//
-//pHost = ::gethostbyname(szHostName);
-//if(!pHost)
-//{
-//  // Error handling -> call 'WSAGetLastError()'
-//}
-//
-//char aszIPAddresses[10][16]; // maximum of ten IP addresses
-//
-//for(int iCnt = 0; ((pHost->h_addr_list[iCnt]) && (iCnt < 10)); ++iCnt)
-//{
-//  memcpy(&SocketAddress.sin_addr, pHost->h_addr_list[iCnt], pHost->h_length);
-//  strcpy(aszIPAddresses[iCnt], inet_ntoa(SocketAddress.sin_addr));
-//}
-//
-//// Cleanup
-//WSACleanup();
+
+
 
 //
 //#undef UNICODE
@@ -378,3 +348,163 @@ TEST(EndpointContextPerf, 10000PrimaryPublicIPAddress)
 //    return 0;
 //}
 //
+
+
+//
+//
+//#include <winsock2.h>
+//#include <iphlpapi.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#pragma comment(lib, "IPHLPAPI.lib")
+//
+//#define MALLOC(x) HeapAlloc(GetProcessHeap(), 0, (x))
+//#define FREE(x) HeapFree(GetProcessHeap(), 0, (x))
+//
+///* Note: could also use malloc() and free() */
+//
+//int __cdecl main()
+//{
+//
+//    /* Declare and initialize variables */
+//
+//// It is possible for an adapter to have multiple
+//// IPv4 addresses, gateways, and secondary WINS servers
+//// assigned to the adapter. 
+////
+//// Note that this sample code only prints out the 
+//// first entry for the IP address/mask, and gateway, and
+//// the primary and secondary WINS server for each adapter. 
+//
+//    PIP_ADAPTER_INFO pAdapterInfo;
+//    PIP_ADAPTER_INFO pAdapter = NULL;
+//    DWORD dwRetVal = 0;
+//    UINT i;
+//
+///* variables used to print DHCP time info */
+//    struct tm newtime;
+//    char buffer[32];
+//    errno_t error;
+//
+//    ULONG ulOutBufLen = sizeof (IP_ADAPTER_INFO);
+//    pAdapterInfo = (IP_ADAPTER_INFO *) MALLOC(sizeof (IP_ADAPTER_INFO));
+//    if (pAdapterInfo == NULL) {
+//        printf("Error allocating memory needed to call GetAdaptersinfo\n");
+//        return 1;
+//    }
+//// Make an initial call to GetAdaptersInfo to get
+//// the necessary size into the ulOutBufLen variable
+//    if (GetAdaptersInfo(pAdapterInfo, &ulOutBufLen) == ERROR_BUFFER_OVERFLOW) {
+//        FREE(pAdapterInfo);
+//        pAdapterInfo = (IP_ADAPTER_INFO *) MALLOC(ulOutBufLen);
+//        if (pAdapterInfo == NULL) {
+//            printf("Error allocating memory needed to call GetAdaptersinfo\n");
+//            return 1;
+//        }
+//    }
+//
+//    if ((dwRetVal = GetAdaptersInfo(pAdapterInfo, &ulOutBufLen)) == NO_ERROR) {
+//        pAdapter = pAdapterInfo;
+//        while (pAdapter) {
+//            printf("\tComboIndex: \t%d\n", pAdapter->ComboIndex);
+//            printf("\tAdapter Name: \t%s\n", pAdapter->AdapterName);
+//            printf("\tAdapter Desc: \t%s\n", pAdapter->Description);
+//            printf("\tAdapter Addr: \t");
+//            for (i = 0; i < pAdapter->AddressLength; i++) {
+//                if (i == (pAdapter->AddressLength - 1))
+//                    printf("%.2X\n", (int) pAdapter->Address[i]);
+//                else
+//                    printf("%.2X-", (int) pAdapter->Address[i]);
+//            }
+//            printf("\tIndex: \t%d\n", pAdapter->Index);
+//            printf("\tType: \t");
+//            switch (pAdapter->Type) {
+//            case MIB_IF_TYPE_OTHER:
+//                printf("Other\n");
+//                break;
+//            case MIB_IF_TYPE_ETHERNET:
+//                printf("Ethernet\n");
+//                break;
+//            case MIB_IF_TYPE_TOKENRING:
+//                printf("Token Ring\n");
+//                break;
+//            case MIB_IF_TYPE_FDDI:
+//                printf("FDDI\n");
+//                break;
+//            case MIB_IF_TYPE_PPP:
+//                printf("PPP\n");
+//                break;
+//            case MIB_IF_TYPE_LOOPBACK:
+//                printf("Lookback\n");
+//                break;
+//            case MIB_IF_TYPE_SLIP:
+//                printf("Slip\n");
+//                break;
+//            default:
+//                printf("Unknown type %ld\n", pAdapter->Type);
+//                break;
+//            }
+//
+//            printf("\tIP Address: \t%s\n",
+//                   pAdapter->IpAddressList.IpAddress.String);
+//            printf("\tIP Mask: \t%s\n", pAdapter->IpAddressList.IpMask.String);
+//
+//            printf("\tGateway: \t%s\n", pAdapter->GatewayList.IpAddress.String);
+//            printf("\t***\n");
+//
+//            if (pAdapter->DhcpEnabled) {
+//                printf("\tDHCP Enabled: Yes\n");
+//                printf("\t  DHCP Server: \t%s\n",
+//                       pAdapter->DhcpServer.IpAddress.String);
+//
+//                printf("\t  Lease Obtained: ");
+//                /* Display local time */
+//                error = _localtime32_s(&newtime, (__time32_t*) &pAdapter->LeaseObtained);
+//                if (error)
+//                    printf("Invalid Argument to _localtime32_s\n");
+//                else {
+//                    // Convert to an ASCII representation 
+//                    error = asctime_s(buffer, 32, &newtime);
+//                    if (error)
+//                        printf("Invalid Argument to asctime_s\n");
+//                    else
+//                        /* asctime_s returns the string terminated by \n\0 */
+//                        printf("%s", buffer);
+//                }
+//
+//                printf("\t  Lease Expires:  ");
+//                error = _localtime32_s(&newtime, (__time32_t*) &pAdapter->LeaseExpires);
+//                if (error)
+//                    printf("Invalid Argument to _localtime32_s\n");
+//                else {
+//                    // Convert to an ASCII representation 
+//                    error = asctime_s(buffer, 32, &newtime);
+//                    if (error)
+//                        printf("Invalid Argument to asctime_s\n");
+//                    else
+//                        /* asctime_s returns the string terminated by \n\0 */
+//                        printf("%s", buffer);
+//                }
+//            } else
+//                printf("\tDHCP Enabled: No\n");
+//
+//            if (pAdapter->HaveWins) {
+//                printf("\tHave Wins: Yes\n");
+//                printf("\t  Primary Wins Server:    %s\n",
+//                       pAdapter->PrimaryWinsServer.IpAddress.String);
+//                printf("\t  Secondary Wins Server:  %s\n",
+//                       pAdapter->SecondaryWinsServer.IpAddress.String);
+//            } else
+//                printf("\tHave Wins: No\n");
+//            pAdapter = pAdapter->Next;
+//            printf("\n");
+//        }
+//    } else {
+//        printf("GetAdaptersInfo failed with error: %d\n", dwRetVal);
+//
+//    }
+//    if (pAdapterInfo)
+//        FREE(pAdapterInfo);
+//
+//    return 0;
+//}
