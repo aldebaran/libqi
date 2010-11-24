ce90ec55bc766ef4254f8ab8b9d969eb13ee4ea8
ckilner <ckilner@aldebaran-robotics.com>
2010-11-24 13:52:13 +0100

    use boost::program_options for command parsing ... yurch!

--- bin/qi/qimain.cpp
+++ bin/qi/qimain.cpp
@@ -10,15 +10,15 @@
 #include <map>
 #include <qi/messaging.hpp>
 #include <qi/perf/sleep.hpp>
+#include <boost/program_options.hpp>
 
-void usage()
-{
-  std::cout << "qi [address]" << std::endl;
-}
+namespace po = boost::program_options;
 
 void qi_call(std::string addr) {
-  qi::Client client("qi client", addr);
-
+  qi::Client client("qi command line client", addr);
+  if (!client.isInitialized()) {
+    return;
+  }
   //client.call("master.listServices::{ss}")
 
   typedef std::map<std::string, std::string>  StringMap;
@@ -31,21 +31,46 @@ void qi_call(std::string addr) {
   {
     std::cout << it->first << " :" << it->second << std::endl;
   }
-//  std::string locatethismethod("master.listServices::{ss}:");
-//  std::string result = client.call<std::string>("master.locateService", locatethismethod);
-//  std::cout << "result:" << result << std::endl;
+  //  std::string locatethismethod("master.listServices::{ss}:");
+  //  std::string result = client.call<std::string>("master.locateService", locatethismethod);
+  //  std::cout << "result:" << result << std::endl;
 }
 
 int main(int argc, char *argv[])
 {
-  std::string masterAddress;
-  if (argc > 2)
-    usage();
-  if (argc == 2)
-    masterAddress = argv[1];
-  else
-    masterAddress = "127.0.0.1:5555";
+  // declare the program options
+  po::options_description desc("Usage:\n  qi masterAddress [options]\nOptions");
+  desc.add_options() 
+    ("help", "Print this help.")
+    ("master-address",
+    po::value<std::string>()->default_value(std::string("127.0.0.1:5555")),
+    "The master address");
 
-  qi_call(masterAddress);
+  // allow master address to be specified as the first arg
+  po::positional_options_description pos;
+  pos.add("master-address", 1);
+
+  // parse and store
+  po::variables_map vm;
+  try {
+    po::store(po::command_line_parser(argc, argv).
+      options(desc).positional(pos).run(), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+      std::cout << desc << "\n";
+      return 0;
+    }
+
+    if(vm.count("master-address")==1) {
+      std::string address = vm["master-address"].as<std::string>();
+      qi_call(address);
+    } else {
+      std::cout << desc << "\n";
+    }
+  } catch (const boost::program_options::error&) {
+    std::cout << desc << "\n";
+  }
+
   return 0;
 }
--- bin/qimaster/qimaster.cpp
+++ bin/qimaster/qimaster.cpp
@@ -9,23 +9,49 @@
 #include <cstdlib>
 #include <qi/messaging/master.hpp>
 #include <qi/perf/sleep.hpp>
+#include <boost/program_options.hpp>
 
-void usage()
-{
-  std::cout << "qimaster [address]" << std::endl;
-  exit(1);
-}
+namespace po = boost::program_options;
 
 int main(int argc, char *argv[])
 {
-  std::string masterAddress = "127.0.0.1:5555";
-  if (argc > 2)
-    usage();
-  if (argc == 2)
-    masterAddress = argv[1];
-  qi::Master master(masterAddress);
+   // declare the program options
+  po::options_description desc("Usage:\n  qi-master masterAddress [options]\nOptions");
+  desc.add_options() 
+    ("help", "Print this help.")
+    ("master-address",
+    po::value<std::string>()->default_value(std::string("127.0.0.1:5555")),
+    "The master address");
 
-  while (1)
-    sleep(1);
+  // allow master address to be specified as the first arg
+  po::positional_options_description pos;
+  pos.add("master-address", 1);
+
+  // parse and store
+  po::variables_map vm;
+  try {
+    po::store(po::command_line_parser(argc, argv).
+      options(desc).positional(pos).run(), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+      std::cout << desc << "\n";
+      return 0;
+    }
+
+    if(vm.count("master-address")==1) {
+      std::string masterAddress = vm["master-address"].as<std::string>();
+      qi::Master master(masterAddress);
+      if (master.isInitialized()) {
+        while (1)
+          sleep(1);
+      }
+    } else {
+      std::cout << desc << "\n";
+    }
+  } catch (const boost::program_options::error&) {
+    std::cout << desc << "\n";
+  }
+
   return 0;
 }
