a5091550c09ca51c39ea0f51fc3bc519ca45afac
ckilner <ckilner@aldebaran-robotics.com>
2010-11-30 15:06:03 +0100

    Make c bindings compile on strict compilers

--- test/test_c_bindings/test_c_bindings.c
+++ test/test_c_bindings/test_c_bindings.c
@@ -10,6 +10,8 @@
 
 int main(int argc, char **argv)
 {
+  char *result;
+
   qi_client_t *client = qi_client_create("simplecli", argv[1]);
 
   qi_message_t *message = qi_message_create();
@@ -20,7 +22,7 @@ int main(int argc, char **argv)
 
   qi_client_call(client, "master.locateService::s:s", message, ret);
 
-  char *result = qi_message_read_string(ret);
+  result = qi_message_read_string(ret);
   printf("locate returned: %s\n", result);
   return 0;
 }
