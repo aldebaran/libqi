e13f26f7d3ac68abf538009f51cee9ef95e51f31
ckilner <ckilner@aldebaran-robotics.com>
2010-12-03 14:35:04 +0100

    formatting

--- qi/messaging/client.hpp
+++ qi/messaging/client.hpp
@@ -48,7 +48,8 @@ namespace qi {
     /// The address of the master that is used to find services
     /// e.g. "127.0.0.1:5555"
     /// </param>
-    Client(const std::string& clientName, const std::string& masterAddress = "127.0.0.1:5555");
+    Client(const std::string& clientName,
+      const std::string& masterAddress = "127.0.0.1:5555");
 
     virtual ~Client();
 
