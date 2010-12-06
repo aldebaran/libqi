c5747ebccbf2d55b77d29a6d31dc10933537638c
ckilner <ckilner@aldebaran-robotics.com>
2010-12-06 12:17:58 +0100

    Added a zmq forwarder test ( a binary in fact ... no test )

---
+++ test/test_transport_forwarder/test_transport_forwarder.cpp
@@ -0,0 +1,15 @@
+/*
+** Author(s):
+**  - Chris Kilner <gestes@aldebaran-robotics.com>
+**
+** Copyright (C) 2010 Aldebaran Robotics
+*/
+
+#include <qi/transport/detail/zmq/zmq_forwarder.hpp>
+
+int main(int argc, char **argv)
+{
+  qi::transport::ZMQForwarder f;
+  f.run("tcp://127.0.0.1:6000", "tcp://127.0.0.1:6001");
+  return 0;
+}
