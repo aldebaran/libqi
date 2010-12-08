526cf054083993ddbd7b432e355a1e0250ef870f
gestes <gestes@aldebaran-robotics.com>
2010-12-08 20:21:23 +0100

    doc: fix the example and make it compile by default.
    examples should always be correct.

--- examples/example_qi_advertise.cpp
+++ examples/example_qi_advertise.cpp
@@ -9,7 +9,8 @@ int main(int argc, char *argv[])
 {
   // Create the server, giving it a name that helps to track it
   qi::Server server("deepThought");
+
   // Add the service, giving it a name.
-  server.addService("deepThought.getMeaningOfLife", &getMeaningOfLife);
+  server.advertiseService("deepThought.getMeaningOfLife", &getMeaningOfLife);
   // FIXME: need a nice way to sleep
 }
