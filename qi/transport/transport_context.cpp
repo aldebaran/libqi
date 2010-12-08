992ac86b938d267b12256675fe1d37ace6ea2dc2
gestes <gestes@aldebaran-robotics.com>
2010-12-08 12:18:14 +0100

    transport: added TransportContext

---
+++ qi/transport/transport_context.cpp
@@ -0,0 +1,20 @@
+/*
+*  Author(s):
+*  - Chris  Kilner <ckilner@aldebaran-robotics.com>
+*  - Cedric Gestes <gestes@aldebaran-robotics.com>
+*
+*  Copyright (C) 2010 Aldebaran Robotics
+*/
+
+#include <qi/transport/transport_context.hpp>
+#include <qi/log.hpp>
+
+namespace qi {
+  namespace transport {
+
+    TransportContext::TransportContext()
+    {
+    }
+
+  }
+}
---
+++ qi/transport/transport_context.hpp
@@ -0,0 +1,30 @@
+#pragma once
+/*
+*  Author(s):
+*  - Chris  Kilner <ckilner@aldebaran-robotics.com>
+*  - Cedric Gestes <gestes@aldebaran-robotics.com>
+*
+*  Copyright (C) 2010 Aldebaran Robotics
+*/
+
+
+#ifndef _QI_TRANSPORT_TRANSPORT_CONTEXT_HPP_
+#define _QI_TRANSPORT_TRANSPORT_CONTEXT_HPP_
+
+#include <string>
+#include <qi/transport/buffer.hpp>
+
+namespace qi {
+  namespace transport {
+
+    class TransportContext {
+    public:
+      TransportContext();
+
+    protected:
+    };
+
+  }
+}
+
+#endif  // _QI_TRANSPORT_TRANSPORT_CLIENT_HPP_
