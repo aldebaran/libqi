f53c619c61c64aa6375e6f0156741f458dd561e9
ckilner <ckilner@aldebaran-robotics.com>
2010-12-13 21:40:13 +0100

    Add Options for Examples and Perf tests

--- options.cmake
+++ options.cmake
@@ -8,3 +8,6 @@
 option(WITH_THRIFT   "Enable the use of thrift"   OFF)
 option(WITH_ZMQ      "Enable the use of ZMQ"      ON)
 option(WITH_PROTOBUF "Enable the use of protobuf" OFF)
+
+option(WITH_PERF     "Build perfomance tests"    OFF)
+option(WITH_EXAMPLES "Build examples"            OFF)
