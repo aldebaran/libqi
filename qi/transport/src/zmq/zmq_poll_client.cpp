48ed4e8b191faec3f2943e76caa2108a568c9387
gestes <gestes@aldebaran-robotics.com>
2010-12-14 15:25:34 +0100

    transport: zmq: create a ZMQPollClient classes, that implement a zmq receive with timeout

---
+++ qi/transport/src/zmq/zmq_poll_client.cpp
@@ -0,0 +1,60 @@
+/*
+*  Author(s):
+*  - Chris  Kilner <ckilner@aldebaran-robotics.com>
+*  - Cedric Gestes <gestes@aldebaran-robotics.com>
+*
+*  Copyright (C) 2010 Aldebaran Robotics
+*/
+
+#include <qi/transport/src/zmq/zmq_poll_client.hpp>
+#include <qi/exceptions/exceptions.hpp>
+#include <iostream>
+#include <qi/log.hpp>
+
+namespace qi {
+  namespace transport {
+    namespace detail {
+      ZMQPollClient::ZMQPollClient(zmq::socket_t &socket)
+        : _zsocket(socket),
+          _first_time(1)
+      {
+        _items[0].socket  = _zsocket;
+        _items[0].fd      = 0;
+        _items[0].events  = ZMQ_POLLIN;
+        _items[0].revents = 0;
+      }
+
+      //return -1 on error, 0 otherwise
+      int ZMQPollClient::pollRecv(long timeout) {
+        int             rc = 0;
+
+        rc = zmq::poll(_items, 1, timeout);
+        qisDebug << "ZMQPollClient: timeout:" << timeout << std::endl;
+        if ((rc <= 0) || (!(_items[0].revents & ZMQ_POLLIN)))
+          return -1;
+        return 0;
+      }
+
+      void ZMQPollClient::recv(zmq::message_t *msg, long usTimeout)
+      {
+        if (_first_time) {
+          int rc = 0;
+          long elapsed = 0;
+          _first_time = 0;
+          do {
+            rc = pollRecv(1000);
+            elapsed += 1;
+          } while (rc < 0 && elapsed < usTimeout);
+          if (rc < 0)
+            throw qi::transport::Exception("no response");
+        }
+        else
+        {
+          pollRecv(usTimeout);
+        }
+        _zsocket.recv(msg);
+      }
+    }
+  }
+}
+
---
+++ qi/transport/src/zmq/zmq_poll_client.hpp
@@ -0,0 +1,45 @@
+#pragma once
+/*
+*  Author(s):
+*  - Cedric Gestes <gestes@aldebaran-robotics.com>
+*  - Chris  Kilner <ckilner@aldebaran-robotics.com>
+*
+*  Copyright (C) 2010 Aldebaran Robotics
+*/
+
+
+#ifndef _QI_TRANSPORT_SRC_ZMQ_ZMQ_POLL_CLIENT_HPP_
+#define _QI_TRANSPORT_SRC_ZMQ_ZMQ_POLL_CLIENT_HPP_
+
+# include <qi/transport/buffer.hpp>
+# include <qi/transport/src/client_backend.hpp>
+# include <zmq.hpp>
+
+namespace qi {
+  namespace transport {
+    namespace detail {
+
+      /// <summary> Simple zmq helper, that will handle receive timeout nicely
+      /// </summary>
+      class ZMQPollClient {
+      public:
+        ZMQPollClient(zmq::socket_t &socket);
+
+        /// <summary> zmq::recv with timeout </summary>
+        /// <param name="msg"> a pointer to a zmq message </param>
+        /// <param name="msg"> a microsecond timeout </param>
+        void recv(zmq::message_t *msg, long usTimeout);
+
+      protected:
+        int pollRecv(long usTimeout);
+
+      protected:
+        zmq::socket_t   &_zsocket;
+        zmq_pollitem_t   _items[1];
+        bool             _first_time;
+      };
+    }
+  }
+}
+
+#endif  // _QI_TRANSPORT_SRC_ZMQ_ZMQ_POLL_CLIENT_HPP_
