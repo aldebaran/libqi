85399bfd16566f8933bc190741935dff544cd0d8
ckilner <ckilner@aldebaran-robotics.com>
2010-12-20 12:10:18 +0100

    Reworked forwarder as zmq_*_backend, transport_*, *_backend

---
+++ qi/transport/src/zmq/zmq_forwarder_backend.cpp
@@ -0,0 +1,47 @@
+/*
+*  Author(s):
+*  - Chris  Kilner <ckilner@aldebaran-robotics.com>
+*  - Cedric Gestes <gestes@aldebaran-robotics.com>
+*
+*  Copyright (C) 2010 Aldebaran Robotics
+*/
+
+#include <qi/transport/src/zmq/zmq_forwarder_backend.hpp>
+
+namespace qi {
+  namespace transport {
+    namespace detail {
+
+      ZMQForwarderBackend::ZMQForwarderBackend(
+        const std::vector<std::string>& inAddresses,
+        const std::vector<std::string>& outAddresses,
+        zmq::context_t &context) :
+          _context(context),
+          _in_socket(_context, ZMQ_SUB),
+          _out_socket(_context, ZMQ_PUB),
+           ForwarderBackend(inAddresses, outAddresses)
+      {
+        _in_socket.setsockopt(ZMQ_SUBSCRIBE, "", 0);
+      }
+
+      ZMQForwarderBackend::~ZMQForwarderBackend() {}
+
+      void ZMQForwarderBackend::bind() {
+        std::vector<std::string>::const_iterator it;
+        for(it = _inAddresses.begin(); it != _inAddresses.end(); ++it) {
+          _in_socket.bind( (*it).c_str());
+        }
+        for(it = _outAddresses.begin(); it != _outAddresses.end(); ++it) {
+          _out_socket.bind( (*it).c_str());
+        }
+      }
+
+      void ZMQForwarderBackend::run()
+      {
+        // FIXME: how can this be torn down?
+        zmq::device(ZMQ_FORWARDER, _in_socket, _out_socket);
+      }
+    }
+  }
+}
+
---
+++ qi/transport/transport_forwarder.cpp
@@ -0,0 +1,68 @@
+/*
+*  Author(s):
+*  - Cedric Gestes <gestes@aldebaran-robotics.com>
+*  - Chris  Kilner <ckilner@aldebaran-robotics.com>
+*
+*  Copyright (C) 2010 Aldebaran Robotics
+*/
+
+
+#include <string>
+#include <qi/log.hpp>
+#include <qi/transport/transport_forwarder.hpp>
+#include <qi/transport/transport_context.hpp>
+#include <qi/transport/src/forwarder_backend.hpp>
+#include <qi/transport/src/zmq/zmq_forwarder_backend.hpp>
+
+namespace qi {
+  namespace transport {
+
+    TransportForwarder::TransportForwarder(const TransportForwarder& rhs) :
+      _isInitialized(rhs._isInitialized),
+      _transportContext(rhs._transportContext),
+      _backend(rhs._backend) {}
+
+    TransportForwarder::TransportForwarder(TransportContext &context)
+      : _isInitialized(false),
+        _transportContext(context),
+        _backend(NULL)
+    {
+    }
+
+    TransportForwarder::~TransportForwarder() {
+      if (_backend != NULL) {
+        delete(_backend);
+        _backend = NULL;
+      }
+    }
+
+    void TransportForwarder::bind(
+      const std::vector<std::string>& inEndpoints,
+      const std::vector<std::string>& outEndpoints)
+    {
+      try {
+        _backend = new detail::ZMQForwarderBackend(
+          inEndpoints,
+          outEndpoints,
+          _transportContext.getContext<zmq::context_t>(inEndpoints[0])
+          );
+        _backend->bind();
+
+        _isInitialized = true;
+      } catch(const zmq::error_t& e) {
+        qisError << "Failed to bind the transport forwarder. Reason: "
+          << e.what() << std::endl;
+        throw(e);
+      }
+    }
+
+    void TransportForwarder::run() {
+      _backend->run();
+    }
+
+    bool TransportForwarder::isInitialized() {
+      return _isInitialized;
+    }
+  }
+}
+
