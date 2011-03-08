8eeb53071804de9a3c9d9b17427d2d1b36f8b097
gestes <gestes@aldebaran-robotics.com>
2011-03-08 18:47:08 +0100

    python: message to/from python

--- bindings/python/qi/message.py
+++ bindings/python/qi/message.py
@@ -6,61 +6,17 @@
 ## Copyright (C) 2010, 2011 Aldebaran Robotics
 ##
 
-import _qi as qi
-
-def _write_single(message, signature, data):
-    if s == "i":
-        message.write_int(d)
-        return (1, 1)
-    elif s == 's':
-        message.write_string(d)
-        return (1, 1)
-    elif s == '[':
-        return _write_list(message, signature[i:], data[:di])
-
-def _write_list(message, signature, data):
-    length = message.write_int(len(data))
-    (i, di) = (1, 1)
-    for i in range(length):
-        (i, di) += message.read_single(message, signature[i:], data[di:])
-    return (1, di)
-
-def _write_map(message, signature, data):
-    length = message.write_int(len(data))
-    (i, di) = (1, 1)
-    for i in range(length):
-        (i, di) += _write_single(message, signature[i:], data[di:])
-        (i, di) += _write_single(message, signature[i:], data[di:])
-    return (1, di)
-
-def _write(message, signature, data):
-    di = 0
-    length = len(signature)
-    dlength = len(data)
-    while i < length:
-        i  += 1
-        di += 1
-        (i, di) = _write_single(message, signature[i:], data[di:])
-    if di != dlength:
-        print "Fail in _write(%s): %d != %d" % (signature, di, dlenght)
-
-def _read(message, signature, data):
-    pass
+import _qi
+import qi.signature
 
 class Message:
     def __init__(self):
-        self.pmessage = qi.qi_create_message()
+        self.pmessage = _qi.qi_message_create()
 
-    def decode(self, signature):
-        return _read(self, signature)
-
-    def fill(self, signature, data):
-        _write(self, signature, data)
-
-    def write_bool(self, data)  : _qi.qi_message_write_bool(self.pmessage, data)
-    def write_char(self, data)  : _qi.qi_message_write_char(self.pmessage, data)
-    def write_int(self, data)   : _qi.qi_message_write_int(self.pmessage, data)
-    def write_float(self, data) : _qi.qi_message_write_float(self.pmessage, data)
+    def write_bool(self  , data): _qi.qi_message_write_bool(self.pmessage, data)
+    def write_char(self  , data): _qi.qi_message_write_char(self.pmessage, data)
+    def write_int(self   , data): _qi.qi_message_write_int(self.pmessage, data)
+    def write_float(self , data): _qi.qi_message_write_float(self.pmessage, data)
     def write_double(self, data): _qi.qi_message_write_double(self.pmessage, data)
     def write_string(self, data): _qi.qi_message_write_string(self.pmessage, data)
 
@@ -72,10 +28,118 @@ class Message:
     def read_string(self) : return _qi.qi_message_read_string(self.pmessage)
 
 
-def message_to_python(message):
-    #return a python type
-    pass
 
-def python_to_message(data, signature):
-    #return a message
-    pass
+
+def single_value_to_python(sig, message):
+    """ convert the first value of a message based on the first type in the sig
+    """
+    print "single value: %s" % (sig[0])
+    if sig[0] == 'b':
+        return True if message.read_bool() else False
+    elif sig[0] == 'c':
+        return message.read_char()
+    elif sig[0] == 'i':
+        return message.read_int()
+    elif sig[0] == 'f':
+        return message.read_float()
+    elif sig[0] == 'd':
+        return message.read_double()
+    elif sig[0] == 's':
+        print "aie aie aie"
+        return message.read_string()
+    elif sig[0] == '[':
+        print "oups"
+        ret = list()
+        subsig = qi.signature.split(sig[1:-1])
+        if len(subsig) != 1:
+            raise qi.signature.BadSignatureException("what?")
+        c = message.read_int()
+        for i in range(c):
+            ret.append(single_value_to_python(subsig[0], message))
+        return ret
+    elif sig[0] == '{':
+        ret = dict()
+        subsig = qi.signature.split(sig[1:-1])
+        if len(subsig) != 2:
+            raise qi.signature.BadSignatureException("what?")
+        c = message.read_int()
+        for i in range(c):
+            k = single_value_to_python(subsig[0], message)
+            v = single_value_to_python(subsig[1], message)
+            ret[k] = v
+        return ret
+    raise qi.signature.BadSignatureException("what?")
+
+def message_to_python(sig, message):
+    """ convert a message to a native python type.
+        result could be :
+        - None
+        - a single value
+        - a list of values
+
+        a value could be a POD, a list, a map
+    """
+    sigsplit = qi.signature.split(sig)
+
+    if len(sigsplit) == 0:
+        return None
+    elif len(sigsplit) == 1:
+        return single_value_to_python(sigsplit[0], message)
+    return [ single_value_to_python(s, message) for s in sigsplit ]
+
+def single_value_to_message(sig, data, message):
+    """ write a python type to a message
+    """
+    if sig[0] == 'b':
+        message.write_bool(data)
+        return
+    elif sig[0] == 'c':
+        message.write_char(data)
+        return
+    elif sig[0] == 'i':
+        message.write_int(int(data))
+        return
+    elif sig[0] == 'f':
+        message.write_float(float(data))
+        return
+    elif sig[0] == 'd':
+        message.write_double(float(data))
+        return
+    elif sig[0] == 's':
+        print "towrite:", data
+        message.write_string(str(data))
+        return
+    elif sig[0] == '[':
+        listlen = len(data)
+        subsig = qi.signature.split(sig[1:-1])
+        message.write_int(listlen)
+        assert(len(subsig) == 1)
+        for i in range(listlen):
+            single_value_to_message(subsig[0], data[i], message)
+        return
+    elif sig[0] == '{':
+        dictlen = len(data)
+        subsig = qi.signature.split(sig[1:-1])
+        assert(len(subsig) == 2)
+        message.write_int(dictlen)
+        for k,v in data.iteritems():
+            single_value_to_message(subsig[0], k, message)
+            single_value_to_message(subsig[1], v, message)
+        return
+    raise qi.signature.BadSignatureException("what?")
+
+def python_to_message(signature, *args):
+    """ write a list of python type to a message
+    """
+    sigsplit = qi.signature.split(signature)
+
+    if len(sigsplit) != len(args):
+        raise qi.signature.BadSignatureException()
+
+    message = qi.Message()
+    #message.write_string(signature)
+
+    for s,a in zip(sigsplit, args):
+        print "converting %s: %s" % (s, a)
+        single_value_to_message(s, a, message)
+    return message
---
+++ bindings/python/qi/message_test.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+##
+## Author(s):
+##  - Cedric GESTES <gestes@aldebaran-robotics.com>
+##
+## Copyright (C) 2011 Aldebaran Robotics
+##
+
+import qi.message
+
+
+def test_message():
+    msg = qi.message.Message()
+    msg.write_bool(1)
+    msg.write_char("c")
+    msg.write_int(42)
+    msg.write_float(42.0)
+    msg.write_double(42.0)
+    msg.write_string("str")
+
+    #array [s]
+    msg.write_int(2)
+    msg.write_string("1")
+    msg.write_string("2")
+
+    #map [ss]
+    msg.write_int(2)
+    msg.write_string("1a")
+    msg.write_string("1b")
+    msg.write_string("2a")
+    msg.write_string("2b")
+
+    #map {s[s]}
+    msg.write_int(2)
+
+    msg.write_string("1")
+    msg.write_int(2)
+    msg.write_string("1a")
+    msg.write_string("1b")
+
+    msg.write_string("2")
+    msg.write_int(2)
+    msg.write_string("2a")
+    msg.write_string("2b")
+
+    ret = qi.message.message_to_python("bcifds[s]{ss}{s[s]}", msg)
+    assert(ret == [True, 'c', 42, 42.0, 42.0, 'str', ['1', '2'],
+                   {'1a': '1b', '2a': '2b'},
+                   {'1': ['1a', '1b'], '2': ['2a', '2b']}])
+    print "ret:", ret
+
+    msg2 = qi.message.python_to_message("bcifds[s]{ss}{s[s]}", *ret)
+    print "msg2"
+    ret2 = qi.message.message_to_python("bcifds[s]{ss}{s[s]}", msg2)
+    assert(ret2 == [True, 'c', 42, 42.0, 42.0, 'str', ['1', '2'],
+                    {'1a': '1b', '2a': '2b'},
+                    {'1': ['1a', '1b'], '2': ['2a', '2b']}])
+    print "ret2:", ret
+
+
+
+
+if __name__ == "__main__":
+    test_message()
