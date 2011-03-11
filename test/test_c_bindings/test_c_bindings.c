9037acaad30000264d1dc33c8069c835e974be61
gestes <gestes@aldebaran-robotics.com>
2011-03-11 10:55:42 +0100

    c: add bindings to test_c_bindings

--- test/test_c_bindings/test_c_bindings.c
+++ test/test_c_bindings/test_c_bindings.c
@@ -8,6 +8,12 @@
 #include <qi/qi.h>
 #include <stdio.h>
 
+
+void branle(qi_message_t *params, qi_message_t *ret, void *data)
+{
+  printf("paf la branle!\n");
+}
+
 int main(int argc, char **argv)
 {
   char *result;
@@ -22,9 +28,26 @@ int main(int argc, char **argv)
 //  qi_message_write_string()
 //  qi_client_call(client, "master.locateService::s:ss", message, ret);
 
-  result = qi_master_locate_service(client, "master.locateService::s:ss");
+  //result = qi_master_locate_service(client, "master.locateService::s:ss");
 
   //result = qi_message_read_string(ret);
   printf("locate returned: %s\n", result);
+
+
+  qi_server_t *server = qi_server_create("serv");
+  qi_server_connect(server, argv[1]);
+  qi_server_advertise_service(server, "bande:::", &branle, 0);
+  sleep(1);
+
+  qi_message_t *message = qi_message_create();
+  qi_message_t *ret = qi_message_create();
+
+  qi_message_write_string(message, "bande:::");
+//  qi_message_write_string(message, "master.listServices::{ss}:");
+//  qi_message_write_string()
+  qi_client_call(client, "bande:::", message, ret);
+
+  sleep(1);
   return 0;
 }
+
