d4be19a73ae6c25a298d55f3074a5013bbaf532c
gestes <gestes@aldebaran-robotics.com>
2011-03-15 15:22:03 +0100

    python: rewrite python from/to python using the new signature stuff

--- bindings/python/qipython.cpp
+++ bindings/python/qipython.cpp
@@ -9,59 +9,39 @@
 #include "qipython.hpp"
 #include <Python.h>
 
-static PyObject *qi_value_to_python_list(qi_signature_t *sig, qi_message_t *msg)
+static PyObject *qi_value_to_python_list(const char *sig, qi_message_t *msg)
 {
   int       size    = qi_message_read_int(msg);
-  int       retcode = qi_signature_next(sig);
   int       i       = 0;
   PyObject *lst     = 0;
 
-  if (retcode != 0)
-    return 0;
-
   lst = PyList_New(size);
   for (;i < size; ++i) {
-    PyList_Append(lst, qi_value_to_python(sig->current, msg));
+    PyList_Append(lst, qi_value_to_python(sig, msg));
   }
   return lst;
 }
 
 
-static PyObject *qi_value_to_python_dict(qi_signature_t *sig, qi_message_t *msg)
+static PyObject *qi_value_to_python_dict(const char *sigk, const char *sigv, qi_message_t *msg)
 {
   int       size    = qi_message_read_int(msg);
-  int       retcode = 0;
   int       i       = 0;
   PyObject *map     = 0;
 
-  retcode = qi_signature_next(sig);
-  if (retcode != 0)
-    return 0;
-  //need to store sk, because the next call to qi_signature_next will change sig->current
-  char *sk = strdup(sig->current);
-
-  retcode = qi_signature_next(sig);
-  if (retcode != 0) {
-    free(sk);
-    return 0;
-  }
-  char *sv = sig->current;
-
   map = PyDict_New();
   for (;i < size; ++i) {
-    PyDict_SetItem(map, qi_value_to_python(sk, msg), qi_value_to_python(sv, msg));
+    PyDict_SetItem(map, qi_value_to_python(sigk, msg), qi_value_to_python(sigv, msg));
   }
-
-  free(sk);
-  free(sv);
   return map;
 }
 
-PyObject *qi_value_to_python(qi_signature_t *sig, qi_message_t *msg)
+PyObject *qi_value_to_python(const char *sig, qi_message_t *msg)
 {
   PyObject *ret = 0;
+  int retcode;
 
-  switch (sig->current[0]) {
+  switch (sig[0]) {
   case QI_BOOL:
     return PyBool_FromLong(qi_message_read_bool(msg));
   case QI_CHAR:
@@ -76,9 +56,38 @@ PyObject *qi_value_to_python(qi_signature_t *sig, qi_message_t *msg)
     //TODO return PyString_FromStringAndSize(msg->readString(), val.size());
     return PyString_FromString(qi_message_read_string(msg));
   case QI_LIST:
-      return qi_value_to_python_list(sig, msg);
+    {
+      qi_signature_t *subsig = qi_signature_create_subsignature(sig);
+      retcode = qi_signature_next(subsig);
+      if (retcode != 0)
+        return 0;
+      ret = qi_value_to_python_list(subsig->current, msg);
+      qi_signature_destroy(subsig);
+      return ret;
+    }
   case QI_MAP:
-      return qi_value_to_python_dict(sig, msg);
+    {
+      const char *k;
+      const char *v;
+      qi_signature_t *subsig = qi_signature_create_subsignature(sig);
+      retcode = qi_signature_next(subsig);
+      if (retcode != 0)
+        return 0;
+      k = subsig->current;
+      retcode = qi_signature_next(subsig);
+      if (retcode != 0)
+        return 0;
+      v = subsig->current;
+      ret = qi_value_to_python_dict(k, v, msg);
+      qi_signature_destroy(subsig);
+      return ret;
+    }
+  case QI_TUPPLE:
+      //TODO
+      return 0;
+  case QI_MESSAGE:
+      //TODO
+      return 0;
   default:
     return 0;
   }
@@ -101,7 +110,7 @@ PyObject *qi_message_to_python(const char *signature, qi_message_t *msg)
     return Py_None;
   }
   while (retcode == 0) {
-    PyObject *obj = qi_value_to_python(sig, msg);
+    PyObject *obj = qi_value_to_python(sig->current, msg);
     retcode = qi_signature_next(sig);
     if (retcode != 2) {
       Py_XDECREF(obj);
@@ -120,9 +129,10 @@ PyObject *qi_message_to_python(const char *signature, qi_message_t *msg)
   return ret;
 }
 
-int qi_value_to_message(qi_signature_t *sig, PyObject *data, qi_message_t *msg)
+static int qi_value_to_message(const char *sig, PyObject *data, qi_message_t *msg)
 {
-  switch (sig->current[0]) {
+  int retcode;
+  switch (sig[0]) {
   case QI_BOOL:
     qi_message_write_bool(msg, PyInt_AsLong(data));
     return 0;
@@ -143,21 +153,70 @@ int qi_value_to_message(qi_signature_t *sig, PyObject *data, qi_message_t *msg)
     return 0;
   case QI_LIST:
     {
-      //qi_message_write_python_list()
       int size = PyList_Size(data);
+      qi_message_write_int(msg, size);
+
+      PyObject *currentObj = PyIter_Next(data);
       int i = 0;
-      for (; i < size; ++i) {
-        //TODO
-        //qi_value_to_message()
+      //TODO: assert size = iter count
+      while (currentObj) {
+        qi_signature_t *subsig = qi_signature_create_subsignature(sig);
+        retcode = qi_signature_next(subsig);
+        if (retcode != 0)
+          return retcode;
+        qi_value_to_message(subsig->current, currentObj, msg);
+        qi_signature_destroy(subsig);
+        currentObj = PyIter_Next(data);
+        ++i;
+      }
+      if (i != size) {
+        printf("aie aie aie\n");
+        return 1;
       }
     }
     return 0;
       //return qi_value_to_python_list(sig, msg);
   case QI_MAP:
-      break;
-      ;
-      //return qi_value_to_python_dict(sig, msg);
+    {
+      int size = PyDict_Size(data);
+      qi_message_write_int(msg, size);
+
+      PyObject *key, *value;
+      Py_ssize_t pos = 0;
+
+      //TODO: assert size = iter count
+      while (PyDict_Next(data, &pos, &key, &value)) {
+        char *k = 0;
+        char *v = 0;
+        qi_signature_t *subsig = qi_signature_create_subsignature(sig);
+        retcode = qi_signature_next(subsig);
+        if (retcode != 0)
+          return retcode;
+        k = subsig->current;
+
+        retcode = qi_signature_next(subsig);
+        if (retcode != 0)
+          return retcode;
+        v = subsig->current;
+
+        qi_value_to_message(k, key, msg);
+        qi_value_to_message(v, value, msg);
+        qi_signature_destroy(subsig);
+      }
       return 0;
+    }
+  case QI_TUPPLE:
+    {
+      qi_signature_t *subsig = qi_signature_create_subsignature(sig);
+      retcode = qi_signature_next(subsig);
+      if (retcode != 0)
+        return retcode;
+      qi_python_to_message(subsig->current, msg, data);
+      qi_signature_destroy(subsig);
+      return 0;
+    }
+  case QI_MESSAGE:
+      return 1;
   default:
     return 1;
   }
@@ -172,6 +231,7 @@ int qi_python_to_message(const char *signature, qi_message_t *msg, PyObject *dat
   //if none => return
   if (Py_None == data)
   {
+    qi_signature_destroy(sig);
     if (strlen(signature)) {
       printf("WTF?\n");
       return 2;
@@ -181,17 +241,19 @@ int qi_python_to_message(const char *signature, qi_message_t *msg, PyObject *dat
 
   //if single type => convert and return
   if (!PyIter_Check(data)) {
-    qi_value_to_message(sig, data, msg);
+    qi_value_to_message(signature, data, msg);
+    qi_signature_destroy(sig);
     return 0;
   }
 
   PyObject *currentObj = PyIter_Next(data);
   retcode = qi_signature_next(sig);
   while(retcode == 0 && currentObj) {
-    qi_value_to_message(sig, currentObj, msg);
+    qi_value_to_message(sig->current, currentObj, msg);
     currentObj = PyIter_Next(data);
     retcode = qi_signature_next(sig);
   };
+  qi_signature_destroy(sig);
   return retcode;
 }
 
@@ -223,6 +285,7 @@ void qi_server_advertise_python_service(qi_server_t *server, const char *name, P
 {
   if (!PyCallable_Check(func)) {
     printf("Fail... func is not callable\n");
+    return;
   }
   printf("register callback\n");
   qi_server_advertise_service(server, name, &_qi_server_callback, static_cast<void *>(func));
