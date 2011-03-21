89b1ba61fb45cb0015189926bf65cc29832edc1e
gestes <gestes@aldebaran-robotics.com>
2011-03-21 11:08:28 +0100

    c: add a log api

---
+++ bindings/c/qi/log.cpp
@@ -0,0 +1,27 @@
+/*
+**
+** Author(s):
+**  -  <gestes@aldebaran-robotics.com>
+**
+** Copyright (C) 2011 Aldebaran Robotics
+*/
+
+#include <stdarg.h>
+#include <qi/log.h>
+#include <qi/log.hpp>
+
+/**
+ * call this to make some log
+ */
+void qi_log(const QiLogLevel  verb,
+            const char       *file,
+            const char       *fct,
+            const int         line,
+            const char       *fmt, ...)
+{
+  va_list vl;
+  va_start(vl, fmt);
+  qi::log::log(static_cast<qi::log::LogLevel>(verb), file, fct, line, fmt, vl);
+  va_end(vl);
+}
+;
---
+++ bindings/c/qi/log.h
@@ -0,0 +1,72 @@
+#pragma once
+/*
+** Author(s):
+**  - Cedric Gestes <gestes@aldebaran-robotics.com>
+**
+** Copyright (C) 2011 Aldebaran Robotics
+*/
+
+#ifndef         _C_QI_LOG_H_
+# define        _C_QI_LOG_H_
+
+#include <stdarg.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+typedef enum e_qi_log_level {
+  QI_SILENT = 0,
+  QI_FATAL,
+  QI_ERROR,
+  QI_WARNING,
+  QI_INFO,
+  QI_DEBUG,
+} QiLogLevel;
+
+#ifdef NO_QI_DEBUG
+# define qi_debug(...)
+#else
+# define qi_debug(...)   qi_log(QI_DEBUG,   __FILE__, __FUNCTION__, __LINE__, __VA_ARGS__)
+#endif
+
+#ifdef NO_QI_INFO
+# define qi_info(...)
+#else
+#define qi_info(...)     qi_log(QI_INFO,    __FILE__, __FUNCTION__, __LINE__, __VA_ARGS__)
+#endif
+
+#ifdef NO_QI_WARNING
+# define qi_warning(...)
+#else
+# define qi_warning(...) qi_log(QI_WARNING, __FILE__, __FUNCTION__, __LINE__, __VA_ARGS__)
+#endif
+
+#ifdef NO_QI_ERROR
+# define qi_error(...)
+#else
+# define qi_error(...)   qi_log(QI_ERROR,   __FILE__, __FUNCTION__, __LINE__, __VA_ARGS__)
+#endif
+
+#ifdef NO_QI_FATAL
+# define qi_fatal(...)
+#else
+# define qi_fatal(...)   qi_log(QI_FATAL,   __FILE__, __FUNCTION__, __LINE__, __VA_ARGS__)
+#endif
+
+
+/**
+ * call this to make some log
+ */
+void qi_log(const QiLogLevel  verb,
+            const char       *file,
+            const char       *fct,
+            const int         line,
+            const char       *fmt, ...);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* !LOG_H_ */
