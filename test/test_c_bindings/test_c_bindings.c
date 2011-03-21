5f9310f1a1d8c8a2bc50868d6bcdcadd28d29671
gestes <gestes@aldebaran-robotics.com>
2011-03-21 11:53:33 +0100

    test_c_bindings: usage and basic argument errors handling

--- test/test_c_bindings/test_c_bindings.c
+++ test/test_c_bindings/test_c_bindings.c
@@ -2,22 +2,29 @@
 ** Author(s):
 **  - Cedric GESTES <gestes@aldebaran-robotics.com>
 **
-** Copyright (C) 2010 Aldebaran Robotics
+** Copyright (C) 2010, 2011 Aldebaran Robotics
 */
 
 #include <qi/qi.h>
 #include <stdio.h>
 
 
-void branle(const char *signature, qi_message_t *params, qi_message_t *ret, void *data)
+void brandade(const char *signature, qi_message_t *params, qi_message_t *ret, void *data)
 {
-  printf("paf la branle!\n");
+  printf("paf la brandade!\n");
+}
+
+void usage() {
+  printf("usage: test_c_bindings address:port\n");
+  exit(1);
 }
 
 int main(int argc, char **argv)
 {
   char *result;
 
+  if (argc <= 1)
+    usage();
   qi_client_t *client = qi_client_create("simplecli");
   qi_client_connect(client, argv[1]);
   //qi_message_t *message = qi_message_create();
@@ -36,16 +43,16 @@ int main(int argc, char **argv)
 
   qi_server_t *server = qi_server_create("serv");
   qi_server_connect(server, argv[1]);
-  qi_server_advertise_service(server, "bande:::", &branle, 0);
+  qi_server_advertise_service(server, "brandade:::", &brandade, 0);
   sleep(1);
 
   qi_message_t *message = qi_message_create();
   qi_message_t *ret = qi_message_create();
 
-  qi_message_write_string(message, "bande:::");
+  qi_message_write_string(message, "brandade:::");
 //  qi_message_write_string(message, "master.listServices::{ss}:");
 //  qi_message_write_string()
-  qi_client_call(client, "bande:::", message, ret);
+  qi_client_call(client, "brandade:::", message, ret);
 
   sleep(1);
   return 0;
