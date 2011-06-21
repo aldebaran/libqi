ebd340e87a907170c9d2d290df1d6b35394cd062
hcuche <hcuche@aldebaran-robotics.com>
2011-06-21 10:50:42 +0200

    noaqi: Test logRT.

--- tests/test_log.cpp
+++ tests/test_log.cpp
@@ -9,40 +9,99 @@
 //   log      : "balblalsal",
 // }
 
+#include <rttools/rtthread.h>
+
+class MyThread: public RtThread
+{
+  virtual void* execute();
+};
+
+void *MyThread::execute()
+{
+  periodInit();
+  int i = 0;
+  while (1) {
+    periodWait();
+
+    qiLogFatal("core.log.test1", "%d\n", 41);
+    qiLogError("core.log.test1", "%d\n", 42);
+    qiLogWarning("core.log.test1", "%d\n", 43);
+    qiLogInfo("core.log.test1", "%d\n", 44);
+    qiLogVerbose("core.log.test1", "%d\n", 45);
+    qiLogDebug("core.log.test1", "%d\n", 46);
+
+    qiLogFatal("core.log.test2")   << "f" << 4 << std::endl;
+    qiLogError("core.log.test2")   << "e" << 4 << std::endl;
+    qiLogWarning("core.log.test2") << "w" << 4 << std::endl;
+    qiLogInfo("core.log.test2")    << "i" << 4 << std::endl;
+    qiLogVerbose("core.log.test2") << "v" << 4 << std::endl;
+    qiLogDebug("core.log.test2")   << "d" << 4 << std::endl;
+
+    qiLogFatal("core.log.test3", "%d", 21)   << "f" << 4 << std::endl;
+    qiLogError("core.log.test3", "%d", 21)   << "e" << 4 << std::endl;
+    qiLogWarning("core.log.test3", "%d", 21) << "w" << 4 << std::endl;
+    qiLogInfo("core.log.test3", "%d", 21)    << "i" << 4 << std::endl;
+    qiLogVerbose("core.log.test3", "%d", 21) << "v" << 4 << std::endl;
+    qiLogDebug("core.log.test3", "%d", 21)   << "d" << 4 << std::endl;
+
+    //c style
+    qiLogWarning("core.log.test4", "oups my buffer is too bad: %x\n", 0x0BADCAFE);
+
+    //c++ style
+    qiLogError("core.log.test4") << "dont drink and drive, just smoke and fly"
+                                 << "- what? " << 42 << "- oh.. I prefer!" << std::endl;
+
+    //mixup style
+    qiLogInfo("core.log.test4", "%d %d", 41, 42) << 43 << 44 << std::endl;
+    ++i;
+  }
+}
+
+
 int main(int argc, char **argv)
 {
-  //  qi::log::init("myprogramname", argc, argv);
+  qi::log::setVerbosity(qi::log::debug);
 
-  qiLogFatal("core.log.test1", "%d\n", 41);
-  qiLogError("core.log.test1", "%d\n", 42);
-  qiLogWarning("core.log.test1", "%d\n", 43);
-  qiLogInfo("core.log.test1", "%d\n", 44);
-  qiLogVerbose("core.log.test1", "%d\n", 45);
-  qiLogDebug("core.log.test1", "%d\n", 46);
+  MyThread* myThread = new MyThread();
+  myThread->setRealtime(SCHED_FIFO, 35);
 
-  qiLogFatal("core.log.test2")   << "f" << 4 << std::endl;
-  qiLogError("core.log.test2")   << "e" << 4 << std::endl;
-  qiLogWarning("core.log.test2") << "w" << 4 << std::endl;
-  qiLogInfo("core.log.test2")    << "i" << 4 << std::endl;
-  qiLogVerbose("core.log.test2") << "v" << 4 << std::endl;
-  qiLogDebug("core.log.test2")   << "d" << 4 << std::endl;
+  //10ms like the dcm
+  myThread->setPeriod(10000);
+  myThread->create();
+  myThread->join();
 
-  qiLogFatal("core.log.test3", "%d", 21)   << "f" << 4 << std::endl;
-  qiLogError("core.log.test3", "%d", 21)   << "e" << 4 << std::endl;
-  qiLogWarning("core.log.test3", "%d", 21) << "w" << 4 << std::endl;
-  qiLogInfo("core.log.test3", "%d", 21)    << "i" << 4 << std::endl;
-  qiLogVerbose("core.log.test3", "%d", 21) << "v" << 4 << std::endl;
-  qiLogDebug("core.log.test3", "%d", 21)   << "d" << 4 << std::endl;
+  // for (int i = 0; i < 1000; i++)
+  // {
+  //   qiLogFatal("core.log.test1", "%d\n", 41);
+  //   qiLogError("core.log.test1", "%d\n", 42);
+  //   qiLogWarning("core.log.test1", "%d\n", 43);
+  //   qiLogInfo("core.log.test1", "%d\n", 44);
+  //   qiLogVerbose("core.log.test1", "%d\n", 45);
+  //   qiLogDebug("core.log.test1", "%d\n", 46);
 
-  //c style
-  qiLogWarning("core.log.test4", "oups my buffer is too bad: %x\n", 0x0BADCAFE);
+  //   qiLogFatal("core.log.test2")   << "f" << 4 << std::endl;
+  //   qiLogError("core.log.test2")   << "e" << 4 << std::endl;
+  //   qiLogWarning("core.log.test2") << "w" << 4 << std::endl;
+  //   qiLogInfo("core.log.test2")    << "i" << 4 << std::endl;
+  //   qiLogVerbose("core.log.test2") << "v" << 4 << std::endl;
+  //   qiLogDebug("core.log.test2")   << "d" << 4 << std::endl;
 
-  //c++ style
-  qiLogError("core.log.test4") << "dont drink and drive, just smoke and fly"
-                         << "- what? " << 42 << "- oh.. I prefer!" << std::endl;
+  //   qiLogFatal("core.log.test3", "%d", 21)   << "f" << 4 << std::endl;
+  //   qiLogError("core.log.test3", "%d", 21)   << "e" << 4 << std::endl;
+  //   qiLogWarning("core.log.test3", "%d", 21) << "w" << 4 << std::endl;
+  //   qiLogInfo("core.log.test3", "%d", 21)    << "i" << 4 << std::endl;
+  //   qiLogVerbose("core.log.test3", "%d", 21) << "v" << 4 << std::endl;
+  //   qiLogDebug("core.log.test3", "%d", 21)   << "d" << 4 << std::endl;
 
-  //mixup style
-  qiLogInfo("core.log.test4", "%d %d", 41, 42) << 43 << 44 << std::endl;
+  //   //c style
+  //   qiLogWarning("core.log.test4", "oups my buffer is too bad: %x\n", 0x0BADCAFE);
 
-  std::cout << "I've just finished to log!" << std::endl;
+  //   //c++ style
+  //   qiLogError("core.log.test4") << "dont drink and drive, just smoke and fly"
+  //                                << "- what? " << 42 << "- oh.. I prefer!" << std::endl;
+
+  //   //mixup style
+  //   qiLogInfo("core.log.test4", "%d %d", 41, 42) << 43 << 44 << std::endl;
+  // }
+  // std::cout << "I've just finished to log!" << std::endl;
 }
