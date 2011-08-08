9d6d9ae1d1041cce5b55c4e8e65559ada116dbb5
hcuche <hcuche@aldebaran-robotics.com>
2011-08-08 18:27:01 +0200

    various: replace qi::PathException to QiException.

--- tests/test_qipaththrow.cpp
+++ tests/test_qipaththrow.cpp
@@ -13,26 +13,27 @@
 
 #include <qi/path/sdklayout.hpp>
 #include <qi/path.hpp>
+#include <qi/error.hpp>
 
 
 //should not throw, it depends on qi::program()
 #if 0
 TEST(qiPathThrowTests, qiPathFuncThrow)
 {
-  ASSERT_THROW({qi::path::sdkPrefix();}, qi::PathException);
-  ASSERT_THROW({qi::path::getSdkPrefixes();}, qi::PathException);
-  ASSERT_THROW({qi::path::addOptionalSdkPrefix("/build/sdk");}, qi::PathException);
-  ASSERT_THROW({qi::path::clearOptionalSdkPrefix();}, qi::PathException);
-  ASSERT_THROW({qi::path::findBin("qipath_test");}, qi::PathException);
-  ASSERT_THROW({qi::path::findLib("libqi.so");}, qi::PathException);
-  ASSERT_THROW({qi::path::findConf("naoqi", "autoinit");}, qi::PathException);
-  ASSERT_THROW({qi::path::findData("naoqi", "");}, qi::PathException);
-  ASSERT_THROW({qi::path::confPaths();}, qi::PathException);
-  ASSERT_THROW({qi::path::dataPaths();}, qi::PathException);
-  ASSERT_THROW({qi::path::binPaths();}, qi::PathException);
-  ASSERT_THROW({qi::path::libPaths();}, qi::PathException);
-  ASSERT_THROW({qi::path::userWritableDataPath("naoqi", "");}, qi::PathException);
-  ASSERT_THROW({qi::path::userWritableConfPath("naoqi", "");}, qi::PathException);
+  ASSERT_THROW({qi::path::sdkPrefix();}, qi::os::QiException);
+  ASSERT_THROW({qi::path::getSdkPrefixes();}, qi::os::QiException);
+  ASSERT_THROW({qi::path::addOptionalSdkPrefix("/build/sdk");}, qi::os::QiException);
+  ASSERT_THROW({qi::path::clearOptionalSdkPrefix();}, qi::os::QiException);
+  ASSERT_THROW({qi::path::findBin("qipath_test");}, qi::os::QiException);
+  ASSERT_THROW({qi::path::findLib("libqi.so");}, qi::os::QiException);
+  ASSERT_THROW({qi::path::findConf("naoqi", "autoinit");}, qi::os::QiException);
+  ASSERT_THROW({qi::path::findData("naoqi", "");}, qi::os::QiException);
+  ASSERT_THROW({qi::path::confPaths();}, qi::os::QiException);
+  ASSERT_THROW({qi::path::dataPaths();}, qi::os::QiException);
+  ASSERT_THROW({qi::path::binPaths();}, qi::os::QiException);
+  ASSERT_THROW({qi::path::libPaths();}, qi::os::QiException);
+  ASSERT_THROW({qi::path::userWritableDataPath("naoqi", "");}, qi::os::QiException);
+  ASSERT_THROW({qi::path::userWritableConfPath("naoqi", "");}, qi::os::QiException);
 }
 
 TEST(qiPathThrowTests, sdkLayoutFuncThrow)
@@ -40,20 +41,20 @@ TEST(qiPathThrowTests, sdkLayoutFuncThrow)
 
   qi::SDKLayout* sdkl = new qi::SDKLayout();
 
-  ASSERT_THROW({sdkl->sdkPrefix();}, qi::PathException);
-  ASSERT_THROW({sdkl->getSdkPrefixes();}, qi::PathException);
-  ASSERT_THROW({sdkl->addOptionalSdkPrefix("/build/sdk");}, qi::PathException);
-  ASSERT_THROW({sdkl->clearOptionalSdkPrefix();}, qi::PathException);
-  ASSERT_THROW({sdkl->findBin("qipath_test");}, qi::PathException);
-  ASSERT_THROW({sdkl->findLib("libqi.so");}, qi::PathException);
-  ASSERT_THROW({sdkl->findConf("naoqi", "autoinit");}, qi::PathException);
-  ASSERT_THROW({sdkl->findData("naoqi", "");}, qi::PathException);
-  ASSERT_THROW({sdkl->confPaths();}, qi::PathException);
-  ASSERT_THROW({sdkl->dataPaths();}, qi::PathException);
-  ASSERT_THROW({sdkl->binPaths();}, qi::PathException);
-  ASSERT_THROW({sdkl->libPaths();}, qi::PathException);
-  ASSERT_THROW({sdkl->userWritableDataPath("naoqi", "");}, qi::PathException);
-  ASSERT_THROW({sdkl->userWritableConfPath("naoqi", "");}, qi::PathException);
+  ASSERT_THROW({sdkl->sdkPrefix();}, qi::os::QiException);
+  ASSERT_THROW({sdkl->getSdkPrefixes();}, qi::os::QiException);
+  ASSERT_THROW({sdkl->addOptionalSdkPrefix("/build/sdk");}, qi::os::QiException);
+  ASSERT_THROW({sdkl->clearOptionalSdkPrefix();}, qi::os::QiException);
+  ASSERT_THROW({sdkl->findBin("qipath_test");}, qi::os::QiException);
+  ASSERT_THROW({sdkl->findLib("libqi.so");}, qi::os::QiException);
+  ASSERT_THROW({sdkl->findConf("naoqi", "autoinit");}, qi::os::QiException);
+  ASSERT_THROW({sdkl->findData("naoqi", "");}, qi::os::QiException);
+  ASSERT_THROW({sdkl->confPaths();}, qi::os::QiException);
+  ASSERT_THROW({sdkl->dataPaths();}, qi::os::QiException);
+  ASSERT_THROW({sdkl->binPaths();}, qi::os::QiException);
+  ASSERT_THROW({sdkl->libPaths();}, qi::os::QiException);
+  ASSERT_THROW({sdkl->userWritableDataPath("naoqi", "");}, qi::os::QiException);
+  ASSERT_THROW({sdkl->userWritableConfPath("naoqi", "");}, qi::os::QiException);
 
   delete sdkl;
 }
