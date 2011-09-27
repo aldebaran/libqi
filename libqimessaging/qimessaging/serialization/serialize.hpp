c5646b9f50b9637f909bcb165f28ef01eb3375a3
Cedric GESTES <gestes@aldebaran-robotics.com>
2011-09-27 20:22:50 +0200

    serialization: refactor serialization for map/list

--- libqimessaging/qimessaging/serialization/serialize.hpp
+++ libqimessaging/qimessaging/serialization/serialize.hpp
@@ -4,7 +4,7 @@
 *  - Chris  Kilner <ckilner@aldebaran-robotics.com>
 *  - Cedric Gestes <gestes@aldebaran-robotics.com>
 *
-*  Copyright (C) 2010 Aldebaran Robotics
+*  Copyright (C) 2010, 2011 Aldebaran Robotics
 */
 
 
@@ -16,53 +16,6 @@
 #include <qimessaging/signature.hpp>
 #include <boost/preprocessor/seq/for_each.hpp>
 #include <boost/preprocessor/tuple/elem.hpp>
-
-#define _QI_SERIALIZATION_REFLECT_ELEM(r, data, elem)  \
-  qi::serialization::serialize<BOOST_PP_TUPLE_ELEM(2, 0, elem)>::data(sd, val.BOOST_PP_TUPLE_ELEM(2, 1, elem));
-
-#define QI_SERIALIZATION_REFLECT(TYPE, MEMBERS)                                     \
-namespace qi { namespace serialization {                                            \
-  template<> struct serialize<TYPE> {                                               \
-    static inline void write(qi::Message &sd, const TYPE &val) {                    \
-      BOOST_PP_SEQ_FOR_EACH(_QI_SERIALIZATION_REFLECT_ELEM, write, MEMBERS)         \
-    }                                                                               \
-                                                                                    \
-    static inline void read(qi::Message &sd, TYPE &val) {                           \
-      BOOST_PP_SEQ_FOR_EACH(_QI_SERIALIZATION_REFLECT_ELEM, read, MEMBERS)          \
-    }                                                                               \
-  };                                                                                \
-}}
-
-#define QI_SERIALIZATION_ALLOW_VISITOR(TYPE)                                        \
- friend class qi::serialization::serialize<TYPE>;
-
-//Inline this function => they do nothing, they just call Message method
-//we keep vector/map not inlined at the moment because they take space.
-#define QI_SIMPLE_SERIALIZER(Name, Type)                                 \
-  template <>                                                         \
-  struct serialize<Type>  {                                           \
-    static inline void write(Message &sd, const Type &val) {   \
-      sd.write##Name(val);                                            \
-    }                                                                 \
-    static inline void read(Message &sd, Type &val) {          \
-      sd.read##Name(val);                                             \
-    }                                                                 \
-  };
-
-
-namespace qi {
-  namespace serialization {
-
-    /// serialize a c++ type to a message
-    /// \ingroup Serialization
-    // Enable is need for conditional template specialization
-    template <typename T, class Enable = void>
-    struct serialize {
-      //empty to crash a compile time when the type cant be serialized
-    };
-
-  }
-}
 
 #if 0
 #include <iostream>
@@ -91,6 +44,150 @@ namespace qi {
 # define __QI_DEBUG_SERIALIZATION_CONTAINER_W(x, c)
 # define __QI_DEBUG_SERIALIZATION_CONTAINER_R(x, c)
 #endif
+
+#define _QI_SERIALIZATION_REFLECT_ELEM(r, data, elem)  \
+  qi::serialization::serialize<BOOST_PP_TUPLE_ELEM(2, 0, elem)>::data(sd, val.BOOST_PP_TUPLE_ELEM(2, 1, elem));
+
+#define QI_SERIALIZATION_REFLECT(TYPE, MEMBERS)                                     \
+namespace qi { namespace serialization {                                            \
+  template<> struct serialize<TYPE> {                                               \
+    static inline void write(qi::Message &sd, const TYPE &val) {                    \
+      BOOST_PP_SEQ_FOR_EACH(_QI_SERIALIZATION_REFLECT_ELEM, write, MEMBERS)         \
+    }                                                                               \
+                                                                                    \
+    static inline void read(qi::Message &sd, TYPE &val) {                           \
+      BOOST_PP_SEQ_FOR_EACH(_QI_SERIALIZATION_REFLECT_ELEM, read, MEMBERS)          \
+    }                                                                               \
+  };                                                                                \
+}}
+
+#define QI_SERIALIZATION_ALLOW_VISITOR(TYPE)                                        \
+ friend class qi::serialization::serialize<TYPE>;
+
+//Inline this function => they do nothing, they just call Message method
+//we keep vector/map not inlined at the moment because they take space.
+#define QI_SIMPLE_SERIALIZER(Name, Type)                                   \
+  template <>                                                              \
+  struct serialize<Type>  {                                                \
+    static inline void write(Message &sd, const Type &val) {               \
+      sd.write##Name(val);                                                 \
+    }                                                                      \
+    static inline void read(Message &sd, Type &val) {                      \
+      sd.read##Name(val);                                                  \
+    }                                                                      \
+  };
+
+
+#define QI_LIST_SERIALIZER(TYPE)                                           \
+  template<typename U>                                                     \
+  struct serialize< TYPE<U> >  {                                           \
+    typedef TYPE<U> _typefordebug;                                         \
+                                                                           \
+    static void write(Message &sd, const TYPE<U> &v) {                     \
+      sd.writeInt(v.size());                                               \
+      if (v.size()) {                                                      \
+        typename TYPE<U>::const_iterator it = v.begin();                   \
+        typename TYPE<U>::const_iterator end = v.end();                    \
+        for (; it != end; ++it) {                                          \
+          serialize<U>::write(sd, *it);                                    \
+        }                                                                  \
+      }                                                                    \
+      __QI_DEBUG_SERIALIZATION_CONTAINER_W(_typefordebug, v)               \
+    }                                                                      \
+    static void write(Message &sd, TYPE<U> &v) {                           \
+      sd.writeInt(v.size());                                               \
+      if (v.size()) {                                                      \
+        typename TYPE<U>::iterator it = v.begin();                         \
+        typename TYPE<U>::iterator end = v.end();                          \
+        for (; it != end; ++it) {                                          \
+          serialize<U>::write(sd, *it);                                    \
+        }                                                                  \
+      }                                                                    \
+      __QI_DEBUG_SERIALIZATION_CONTAINER_W(_typefordebug, v);              \
+    }                                                                      \
+    static void read(Message &sd, TYPE<U> &v) {                            \
+      int sz;                                                              \
+      sd.readInt(sz);                                                      \
+      v.clear();                                                           \
+      if (sz) {                                                            \
+        v.resize(sz);                                                      \
+        typename TYPE<U>::iterator it = v.begin();                         \
+        typename TYPE<U>::iterator end = v.end();                          \
+        for (; it != end; ++it) {                                          \
+          serialize<U>::read(sd, *it);                                     \
+        }                                                                  \
+      }                                                                    \
+      __QI_DEBUG_SERIALIZATION_CONTAINER_R(_typefordebug, v);              \
+    }                                                                      \
+  };
+
+
+#define QI_MAP_SERIALIZER(TYPE)                                          \
+  template<typename K, typename V>                                       \
+  struct serialize< TYPE<K, V> >  {                                      \
+    typedef TYPE<K, V> _typefordebug;                                    \
+                                                                         \
+    static void write(Message &sd, const TYPE<K, V> &m) {                \
+      sd.writeInt(m.size());                                             \
+      if (m.size()) {                                                    \
+        typename TYPE<K,V>::const_iterator it = m.begin();               \
+        typename TYPE<K,V>::const_iterator end = m.end();                \
+        for (; it != end; ++it) {                                        \
+          serialize<K>::write(sd, it->first);                            \
+          serialize<V>::write(sd, it->second);                           \
+        }                                                                \
+      }                                                                  \
+      __QI_DEBUG_SERIALIZATION_CONTAINER_W(_typefordebug, m);            \
+    }                                                                    \
+                                                                         \
+    static void write(Message &sd, TYPE<K, V> &m) {                      \
+      sd.writeInt(m.size());                                             \
+      if (m.size()) {                                                    \
+        typename TYPE<K,V>::iterator it = m.begin();                     \
+        typename TYPE<K,V>::iterator end = m.end();                      \
+        for (; it != end; ++it) {                                        \
+          serialize<K>::write(sd, it->first);                            \
+          serialize<V>::write(sd, it->second);                           \
+        }                                                                \
+      }                                                                  \
+      __QI_DEBUG_SERIALIZATION_CONTAINER_W(_typefordebug, m);            \
+    }                                                                    \
+                                                                         \
+    static void read(Message &sd, TYPE<K, V>  &m) {                      \
+      int sz;                                                            \
+      sd.readInt(sz);                                                    \
+      m.clear();                                                         \
+                                                                         \
+      if (sz) {                                                          \
+        for(int i=0; i < sz; ++i) {                                      \
+          K k;                                                           \
+          V v;                                                           \
+          serialize<K>::read(sd, k);                                     \
+          serialize<V>::read(sd, v);                                     \
+          m[k] = v;                                                      \
+        }                                                                \
+      }                                                                  \
+      __QI_DEBUG_SERIALIZATION_CONTAINER_R(_typefordebug, m);            \
+    }                                                                    \
+  };
+
+
+
+namespace qi {
+  namespace serialization {
+
+    /// serialize a c++ type to a message
+    /// \ingroup Serialization
+    // Enable is need for conditional template specialization
+    template <typename T, class Enable = void>
+    struct serialize {
+      //empty to crash a compile time when the type cant be serialized
+    };
+
+  }
+}
+
+
 
 #include <qimessaging/serialization/serialize_pod.hxx>
 #include <qimessaging/serialization/serialize_stl.hxx>
--- libqimessaging/qimessaging/serialization/serialize_qt.hxx
+++ libqimessaging/qimessaging/serialization/serialize_qt.hxx
@@ -4,7 +4,7 @@
 *  - Chris  Kilner <ckilner@aldebaran-robotics.com>
 *  - Cedric Gestes <gestes@aldebaran-robotics.com>
 *
-*  Copyright (C) 2010 Aldebaran Robotics
+*  Copyright (C) 2010, 2011 Aldebaran Robotics
 */
 
 #ifndef _QI_SERIALIZATION_SERIALIZE_QT_HXX_
@@ -19,121 +19,22 @@
 namespace qi {
   namespace serialization {
 
-    //QI_SIMPLE_SERIALIZER(String, QString);
-    //Inline this function => they do nothing, they just call Message method
-    //we keep vector/map not inlined at the moment because they take space.
     template <>
     struct serialize<QString>  {
       static inline void write(Message &sd, const QString &val) {
-        std::string hack((char *)val.data(), val.size());
+        std::string hack = val.toStdString();
         sd.writeString(hack);
       }
       static inline void read(Message &sd, QString &val) {
         std::string hack;
         sd.readString(hack);
-        val.setRawData(hack.data(), hack.size());
+        val = QString::fromStdString(hack);
       }
     };
 
-    template<typename U>
-    struct serialize< QVector<U> >  {
-
-      static void write(Message &sd, const QVector<U> &v) {
-        sd.writeInt(v.size());
-        if (v.size()) {
-          // we should find out if the contents is a fixed size type
-          // and directly assign the contents if we can
-          typename QVector<U>::const_iterator it = v.begin();
-          typename QVector<U>::const_iterator end = v.end();
-          for (; it != end; ++it) {
-            serialize<U>::write(sd, *it);
-          }
-        }
-        __QI_DEBUG_SERIALIZATION_CONTAINER_W(QVector<U>, v);
-      }
-
-      // non-const write
-      static void write(Message &sd, QVector<U> &v) {
-        sd.writeInt(v.size());
-        if (v.size()) {
-          // we should find out if the contents is a fixed size type
-          // and directly assign the contents if we can
-          typename QVector<U>::iterator it = v.begin();
-          typename QVector<U>::iterator end = v.end();
-          for (; it != end; ++it) {
-            serialize<U>::write(sd, *it);
-          }
-        }
-        __QI_DEBUG_SERIALIZATION_CONTAINER_W(QVector<U>, v);
-      }
-
-      static void read(Message &sd, QVector<U> &v) {
-        int sz;
-        sd.readInt(sz);
-        v.clear();
-
-        if (sz) {
-          v.resize(sz);
-          typename QVector<U>::iterator it = v.begin();
-          typename QVector<U>::iterator end = v.end();
-          for (; it != end; ++it) {
-            serialize<U>::read(sd, *it);
-          }
-        }
-        __QI_DEBUG_SERIALIZATION_CONTAINER_R(QVector<U>, v);
-      }
-    };
-
-    template<typename K, typename V>
-    struct serialize< QMap<K, V> >  {
-
-      static void write(Message &sd, const QMap<K, V> &m) {
-        sd.writeInt(m.size());
-        if (m.size()) {
-          typename QMap<K, V>::const_iterator it = m.begin();
-          typename QMap<K, V>::const_iterator end = m.end();
-          for (; it != end; ++it) {
-            serialize<K>::write(sd, it->first);
-            serialize<V>::write(sd, it->second);
-          }
-        }
-        typedef QMap<K, V> debugMap;
-        __QI_DEBUG_SERIALIZATION_CONTAINER_W(debugMap, m);
-      }
-
-      // non-const write
-      static void write(Message &sd, QMap<K, V> &m) {
-        sd.writeInt(m.size());
-        if (m.size()) {
-          typename QMap<K, V>::iterator it = m.begin();
-          typename QMap<K, V>::iterator end = m.end();
-          for (; it != end; ++it) {
-            serialize<K>::write(sd, it->first);
-            serialize<V>::write(sd, it->second);
-          }
-        }
-        typedef QMap<K, V> debugMap;
-        __QI_DEBUG_SERIALIZATION_CONTAINER_W(debugMap, m);
-      }
-
-      static void read(Message &sd, QMap<K, V>  &m) {
-        int sz;
-        sd.readInt(sz);
-        m.clear();
-
-        if (sz) {
-          for(int i=0; i < sz; ++i) {
-            K k;
-            V v;
-            serialize<K>::read(sd, k);
-            serialize<V>::read(sd, v);
-            m[k] = v;
-          }
-        }
-        typedef QMap<K, V> debugMap;
-        __QI_DEBUG_SERIALIZATION_CONTAINER_R(debugMap, m);
-      }
-    };
+    QI_LIST_SERIALIZER(QVector);
+    QI_LIST_SERIALIZER(QList);
+    QI_MAP_SERIALIZER(QMap);
 
   }
 }
