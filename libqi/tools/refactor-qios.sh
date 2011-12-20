dbff066af85b248dafa37f9cb6e520111ac6060d
Cedric GESTES <gestes@aldebaran-robotics.com>
2011-12-20 11:20:56 +0100

    remove libqi from the repo

--- libqi/tools/refactor-qios.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-##
-## Copyright (C) 2011 Aldebaran Robotics
-##
-
-# Temporary fix for qi::path API changes
-# findConfiguration -> findConf and so on
-
-replacer $@ 'qi::os::tmp[ \t]*\('   'qi::os::tmpdir('
