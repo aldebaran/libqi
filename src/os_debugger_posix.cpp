d81ac7bbf9830077131047a57fe507060a25bf7a
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2012-01-20 16:12:54 +0100

    re-add implementation of qi::oss:checkdbg
    Was removed by mistake, causing link errors for anyone using
    this function
    
    Change-Id: If6d589883705ba3e773d10f61839b460a6a1c03c

---
+++ src/os_debugger_posix.cpp
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2012 Aldebaran Robotics. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the COPYING file.
+ */
+
+#include <qi/os.hpp>
+
+#include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+
+#ifdef __APPLE__
+# define MY_PTRACE_ATTACH PT_ATTACH
+# define MY_PTRACE_CONT   PT_CONTINUE
+# define MY_PTRACE_DETACH PT_DETACH
+#else
+# define MY_PTRACE_ATTACH PTRACE_ATTACH
+# define MY_PTRACE_CONT   PTRACE_CONT
+# define MY_PTRACE_DETACH PTRACE_DETACH
+#endif
+
+namespace qi {
+  namespace os {
+
+    int checkdbg()
+    {
+      int pid = fork();
+      int status;
+      int res;
+
+      if (pid == -1) {
+        perror("fork");
+        return -1;
+      }
+
+      if (pid == 0) {
+        int ppid = getppid();
+
+        /* Child */
+        if (ptrace(MY_PTRACE_ATTACH, ppid, NULL, NULL) == 0)
+        {
+          /* Wait for the parent to stop and continue it */
+          ::waitpid(ppid, NULL, 0);
+          ptrace(MY_PTRACE_CONT, NULL, NULL, NULL);
+
+          /* Detach */
+          ptrace(MY_PTRACE_DETACH, getppid(), NULL, NULL);
+
+          /* We were the tracers, so gdb is not present */
+          res = 0;
+        }
+        else
+        {
+          /* Trace failed so gdb is present */
+          res = 1;
+        }
+        _exit(res);
+      }
+      else
+      {
+        ::waitpid(pid, &status, 0);
+        res = WEXITSTATUS(status);
+      }
+      return res;
+    }
+
+  }
+};
---
+++ src/os_debugger_win32.cpp
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2012 Aldebaran Robotics. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the COPYING file.
+ */
+
+#include <qi/os.hpp>
+#include <Windows.h>
+
+namespace qi {
+  namespace os {
+
+    int checkdbg() {
+	  return IsDebuggerPresent();
+      //TODO: this function is not implemented under windows
+      return 0;
+    }
+
+  }
+};
+
+
