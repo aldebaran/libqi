84367a63a390abcf37cda51ca767218aefcdfc84
Cedric GESTES <gestes@aldebaran-robotics.com>
2012-02-01 10:55:48 +0100

    qimessaging: cleanup server.h
    Change-Id: Id97ebdb41f370d682559e47bf398bbca5985b8ce

--- libqimessaging/qimessaging/server.h
+++ libqimessaging/qimessaging/server.h
@@ -3,7 +3,7 @@
 ** Author(s):
 **  - Cedric GESTES <gestes@aldebaran-robotics.com>
 **
-** Copyright (C) 2010, 2011 Aldebaran Robotics
+** Copyright (C) 2010, 2011, 2012 Aldebaran Robotics
 */
 
 #ifndef _QIMESSAGING_SERVER_H_
@@ -23,36 +23,8 @@ extern "C"
   void         qi_server_destroy(qi_server_t *server);
   void         qi_server_connect(qi_server_t *server, const char *address);
 
-  void         qi_server_advertise_service(qi_server_t *server  , const char *method_signature, BoundMethod func, void *data);
-  void         qi_server_unadvertise_service(qi_server_t *server, const char *method_signature);
-
-  /// <summary>Advertises to the master that you wish to publish data
-  /// of type "PUBLISH_TYPE" to a topic of name "topicName".</summary>
-  /// <param name="topicName">The name of the topic you wish to publish to.</param>
-  /// <param name="isManyToMany">Allows many to many publishing</param>
-  void qi_server_advertise_topic(const char *topic_name, const char *signature, const int is_many_to_many);
-
-  /// <summary> Unadvertises a topic</summary>
-  /// <param name="topicName">The name of the topic you wish to
-  /// unadvertise.</param>
-  void qi_server_unadvertise_topic(const char *topic_name);
-
-  /// <summary>
-  /// Publishes messages to an existing topic.
-  /// e.g. publisher.publish("/time/hour_of_the_day", 10);
-  /// A subscriber would be able to subscribe to the above with
-  /// subscriber.subscribe("time/hour_of_the_day", &handler);
-  /// where the handler expects an int type.
-  /// </summary>
-  /// <param name="topicName">The name of the topic you want to publish to.
-  /// By convention, topic names use forward slashes as a separator
-  /// e.g. "/time/hour_of_the_day"
-  /// </param>
-  /// <param name="publishData">The typed data that you wish to publish</param>
-  /// <returns>void</returns>
-  /// <seealso cref="qi::Subscriber"/>
-  void qi_server_publish(const char *topic_name, const char *signature, qi_message_t *message);
-
+  bool         qi_server_register_object(qi_server_t *server, const char *address, qi_object_t *object);
+  bool         qi_server_unregister_object(qi_server_t *server, const char *address);
 
 #ifdef __cplusplus
 }
