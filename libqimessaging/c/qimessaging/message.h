856f2066c73ed750533243213f5fb7b8352cad0d
Laurent LEC <llec@aldebaran-robotics.com>
2012-02-10 10:32:37 +0100

    Refresh C Message APIs.

--- libqimessaging/c/qimessaging/message.h
+++ libqimessaging/c/qimessaging/message.h
@@ -2,6 +2,7 @@
 **
 ** Author(s):
 **  - Cedric GESTES <gestes@aldebaran-robotics.com>
+**  - Laurent LEC <llec@aldebaran-robotics.com>
 **
 ** Copyright (C) 2010, 2011 Aldebaran Robotics
 */
@@ -16,27 +17,40 @@ extern "C"
 {
 #endif
 
-  typedef void qi_message_t;
-
+  typedef struct {} qi_message_t;
+  typedef enum { call, answer, event, error, none } qi_message_type_t;
 
   QIMESSAGING_API qi_message_t *qi_message_create();
   QIMESSAGING_API void          qi_message_destroy(qi_message_t *msg);
 
-  QIMESSAGING_API void          qi_message_write_bool(qi_message_t   *msg, const char b);
-  QIMESSAGING_API void          qi_message_write_char(qi_message_t   *msg, const char b);
-  QIMESSAGING_API void          qi_message_write_int(qi_message_t    *msg, const int i);
-  QIMESSAGING_API void          qi_message_write_float(qi_message_t  *msg, const float f);
-  QIMESSAGING_API void          qi_message_write_double(qi_message_t *msg, const double d);
-  QIMESSAGING_API void          qi_message_write_string(qi_message_t *msg, const char *);
-  QIMESSAGING_API void          qi_message_write_raw(qi_message_t    *msg, const char *, unsigned int size);
+  QIMESSAGING_API void          qi_message_set_id(qi_message_t *msg, unsigned int id);
+  QIMESSAGING_API unsigned int  qi_message_get_id(qi_message_t *msg);
+  QIMESSAGING_API void          qi_message_set_src(qi_message_t *msg, char *src);
+  QIMESSAGING_API char         *qi_message_get_src(qi_message_t *msg);
+  QIMESSAGING_API void          qi_message_set_dst(qi_message_t *msg, char *dst);
+  QIMESSAGING_API char         *qi_message_get_dst(qi_message_t *msg);
+  QIMESSAGING_API void          qi_message_set_path(qi_message_t *msg, char *path);
+  QIMESSAGING_API char         *qi_message_get_path(qi_message_t *msg);
+  QIMESSAGING_API void          qi_message_set_data(qi_message_t *msg, char *data);
+  QIMESSAGING_API char         *qi_message_get_data(qi_message_t *msg);
+  QIMESSAGING_API void          qi_message_set_type(qi_message_t *msg, qi_message_type_t type);
+  QIMESSAGING_API qi_message_type_t qi_message_get_type(qi_message_t *msg);
 
-  QIMESSAGING_API char   qi_message_read_bool(qi_message_t   *msg);
-  QIMESSAGING_API char   qi_message_read_char(qi_message_t   *msg);
-  QIMESSAGING_API int    qi_message_read_int(qi_message_t    *msg);
-  QIMESSAGING_API float  qi_message_read_float(qi_message_t  *msg);
-  QIMESSAGING_API double qi_message_read_double(qi_message_t *msg);
-  QIMESSAGING_API char  *qi_message_read_string(qi_message_t *msg);
-  QIMESSAGING_API char  *qi_message_read_raw(qi_message_t    *msg, unsigned int *size);
+  QIMESSAGING_API void          qi_message_write_bool(qi_message_t *msg, char b);
+  QIMESSAGING_API void          qi_message_write_char(qi_message_t   *msg, char b);
+  QIMESSAGING_API void          qi_message_write_int(qi_message_t    *msg, int i);
+  QIMESSAGING_API void          qi_message_write_float(qi_message_t  *msg, float f);
+  QIMESSAGING_API void          qi_message_write_double(qi_message_t *msg, double d);
+  QIMESSAGING_API void          qi_message_write_string(qi_message_t *msg, const char *s);
+  QIMESSAGING_API void          qi_message_write_raw(qi_message_t    *msg, const char *s, unsigned int size);
+
+  QIMESSAGING_API char          qi_message_read_bool(qi_message_t   *msg);
+  QIMESSAGING_API char          qi_message_read_char(qi_message_t   *msg);
+  QIMESSAGING_API int           qi_message_read_int(qi_message_t    *msg);
+  QIMESSAGING_API float         qi_message_read_float(qi_message_t  *msg);
+  QIMESSAGING_API double        qi_message_read_double(qi_message_t *msg);
+  QIMESSAGING_API char         *qi_message_read_string(qi_message_t *msg);
+  QIMESSAGING_API char         *qi_message_read_raw(qi_message_t    *msg, unsigned int *size);
 
 
 #ifdef __cplusplus
--- libqimessaging/c/src/message.cpp
+++ libqimessaging/c/src/message.cpp
@@ -1,115 +1,248 @@
 /*
 ** Author(s):
 **  - Cedric GESTES <gestes@aldebaran-robotics.com>
+**  - Laurent LEC <llec@aldebaran-robotics.com>
 **
 ** Copyright (C) 2010, 2011 Aldebararan Robotics
 */
 
 #include <qimessaging/message.h>
 #include <qimessaging/datastream.hpp>
+#include <qimessaging/message.hpp>
 #include <cstring>
+#include <cstdlib>
+#include <cassert>
+
+typedef struct
+{
+  qi::DataStream *ds;
+  qi::Message    *msg;
+} qi_message_data_t;
+
 
 /// Message
 qi_message_t *qi_message_create()
 {
-  qi::DataStream *pmsg = new qi::DataStream();
-  return (qi_message_t *)pmsg;
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(malloc(sizeof(qi_message_data_t)));
+  m->ds = new qi::DataStream();
+  m->msg = new qi::Message();
+
+  return reinterpret_cast<qi_message_t*>(m);
 }
 
 void qi_message_destroy(qi_message_t *msg)
 {
-  qi::DataStream *pmsg = static_cast<qi::DataStream *>(msg);
-  delete pmsg;
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
+
+  delete m->ds;
+  delete m->msg;
+
+  free(m);
 }
 
-
-void qi_message_write_bool(qi_message_t *msg, const char b)
+void qi_message_set_type(qi_message_t *msg, qi_message_type_t type)
 {
-  qi::DataStream *pmsg = static_cast<qi::DataStream *>(msg);
-  *pmsg << b;
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
+
+  switch (type)
+  {
+    case call:
+      m->msg->setType(qi::Message::Call);
+    case answer:
+      m->msg->setType(qi::Message::Answer);
+    case event:
+      m->msg->setType(qi::Message::Event);
+    case error:
+      m->msg->setType(qi::Message::Error);
+    case none:
+      m->msg->setType(qi::Message::None);
+    default:
+      assert(false);
+  }
 }
 
-void qi_message_write_char(qi_message_t *msg, const char c)
+qi_message_type_t qi_message_get_type(qi_message_t *msg)
 {
-  qi::DataStream *pmsg = static_cast<qi::DataStream *>(msg);
-  *pmsg << c;
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
+
+  switch (m->msg->type())
+  {
+    case qi::Message::Call:
+      return call;
+    case qi::Message::Answer:
+      return answer;
+    case qi::Message::Event:
+      return event;
+    case qi::Message::Error:
+      return error;
+    case qi::Message::None:
+      return none;
+    default:
+      assert(false);
+  }
 }
 
-void qi_message_write_int(qi_message_t *msg, const int i)
+void qi_message_set_id(qi_message_t *msg, unsigned int id)
 {
-  qi::DataStream *pmsg = static_cast<qi::DataStream *>(msg);
-  *pmsg << i;
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
+
+  m->msg->setId(id);
 }
 
-void qi_message_write_float(qi_message_t *msg, const float f)
+unsigned int qi_message_get_id(qi_message_t *msg)
 {
-  qi::DataStream *pmsg = static_cast<qi::DataStream *>(msg);
-  *pmsg << f;
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
+
+  return m->msg->id();
 }
 
-void qi_message_write_double(qi_message_t *msg, const double d)
+void qi_message_set_src(qi_message_t *msg, char *src)
 {
-  qi::DataStream *pmsg = static_cast<qi::DataStream *>(msg);
-  *pmsg << d;
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
+
+  m->msg->setSource(src);
+}
+
+char *qi_message_get_src(qi_message_t *msg)
+{
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
+  std::string r = m->msg->source();
+
+  return strdup(r.c_str());
+}
+
+void qi_message_set_dst(qi_message_t *msg, char *dst)
+{
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
+
+  m->msg->setDestination(dst);
+}
+
+char *qi_message_get_dst(qi_message_t *msg)
+{
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
+  std::string r = m->msg->destination();
+
+  return strdup(r.c_str());
+}
+
+void qi_message_set_path(qi_message_t *msg, char *path)
+{
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
+
+  m->msg->setPath(path);
+}
+
+char *qi_message_get_path(qi_message_t *msg)
+{
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
+  std::string r = m->msg->path();
+
+  return strdup(r.c_str());
+}
+
+void qi_message_set_data(qi_message_t *msg, char *data)
+{
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
+
+  m->msg->setData(data);
+}
+
+char *qi_message_get_data(qi_message_t *msg)
+{
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
+  std::string r = m->msg->data();
+
+  return strdup(r.c_str());
+}
+
+void qi_message_write_bool(qi_message_t *msg, char b)
+{
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
+  *m->ds << b;
+}
+
+void qi_message_write_char(qi_message_t *msg, char c)
+{
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
+  *m->ds << c;
+}
+
+void qi_message_write_int(qi_message_t *msg, int i)
+{
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
+  *m->ds << i;
+}
+
+void qi_message_write_float(qi_message_t *msg, float f)
+{
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
+  *m->ds << f;
+}
+
+void qi_message_write_double(qi_message_t *msg, double d)
+{
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
+  *m->ds << d;
 }
 
 void qi_message_write_string(qi_message_t *msg, const char *s)
 {
-  qi::DataStream *pmsg = static_cast<qi::DataStream *>(msg);
-  *pmsg << std::string(s);
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
+  *m->ds << std::string(s);
 }
 
 void qi_message_write_raw(qi_message_t *msg, const char *s, unsigned int size)
 {
-  qi::DataStream *pmsg = static_cast<qi::DataStream *>(msg);
-  *pmsg << std::string(s, size);
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
+  *m->ds << std::string(s, size);
 }
 
-
-
-
-char  qi_message_read_bool(qi_message_t *msg) {
-  qi::DataStream *pmsg = static_cast<qi::DataStream *>(msg);
+char qi_message_read_bool(qi_message_t *msg)
+{
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
   bool b;
-  *pmsg >> b;
+  *m->ds >> b;
   return b;
-
 }
 
-char  qi_message_read_char(qi_message_t *msg) {
-  qi::DataStream *pmsg = static_cast<qi::DataStream *>(msg);
+char qi_message_read_char(qi_message_t *msg)
+{
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
   char c;
-  *pmsg >> c;
+  *m->ds >> c;
   return c;
 }
 
 int qi_message_read_int(qi_message_t *msg)
 {
-  qi::DataStream *pmsg = static_cast<qi::DataStream *>(msg);
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
   int i;
-  *pmsg >> i;
+  *m->ds >> i;
   return i;
 }
 
-float qi_message_read_float(qi_message_t *msg) {
-  qi::DataStream *pmsg = static_cast<qi::DataStream *>(msg);
+float qi_message_read_float(qi_message_t *msg)
+{
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
   float f;
-  *pmsg >> f;
+  *m->ds >> f;
   return f;
 }
 
-double qi_message_read_double(qi_message_t *msg) {
-  qi::DataStream *pmsg = static_cast<qi::DataStream *>(msg);
+double qi_message_read_double(qi_message_t *msg)
+{
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
   double d;
-  *pmsg >> d;
+  *m->ds >> d;
   return d;
 }
 
 char *qi_message_read_string(qi_message_t *msg)
 {
-  qi::DataStream *pmsg = static_cast<qi::DataStream *>(msg);
+  qi_message_data_t *m = reinterpret_cast<qi_message_data_t*>(msg);
   std::string s;
-  *pmsg >> s;
+  *m->ds >> s;
   //TODO: buffer overflow
 #ifdef _WIN32
   return _strdup(s.c_str());
