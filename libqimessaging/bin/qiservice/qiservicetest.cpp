10508ae420e8be1789d3df41e969c2bdf55fb6ea
Herve CUCHE <hcuche@aldebaran-robotics.com>
2012-02-10 11:08:14 +0100

    create class qiservicetest and clean up source code
    Change-Id: Ia0f9b90b53cc6c9a32c1ecd644e9c442aec4445b

---
+++ libqimessaging/bin/qiservice/qiservicetest.cpp
@@ -0,0 +1,79 @@
+/*
+** Author(s):
+**  - Cedric GESTES <gestes@aldebaran-robotics.com>
+**
+** Copyright (C) 2012 Aldebaran Robotics
+*/
+
+#include <iostream>
+#include <vector>
+#include <map>
+
+#include <qimessaging/transport.hpp>
+#include <qimessaging/datastream.hpp>
+#include <qimessaging/session.hpp>
+#include <qi/os.hpp>
+
+#include "qiservicetest.hpp"
+
+namespace qi
+{
+  ServiceTest::ServiceTest()
+  {
+    nthd = new qi::NetworkThread();
+    ts = new qi::TransportServer();
+    ts->setDelegate(this);
+  }
+
+  ServiceTest::~ServiceTest()
+  {
+    delete ts;
+    delete nthd;
+  }
+
+  void ServiceTest::start(const std::string &address)
+  {
+    size_t begin = 0;
+    size_t end = 0;
+    end = address.find(":");
+
+    std::string ip = address.substr(begin, end);
+    begin = end + 1;
+
+    unsigned int port;
+    std::stringstream ss(address.substr(begin));
+    ss >> port;
+
+    ts->start(ip, port, nthd->getEventBase());
+  }
+
+  void ServiceTest::onConnected(const qi::Message &msg)
+  {
+  }
+
+  void ServiceTest::onWrite(const qi::Message &msg)
+  {
+  }
+
+  void ServiceTest::onRead(const qi::Message &msg)
+  {
+    std::cout << "read  qiservice" << std::endl;
+
+    if (msg.path() == "reply")
+      reply(msg);
+
+  }
+
+  void ServiceTest::reply(const qi::Message &msg)
+  {
+    qi::Message retval;
+    retval.setType(qi::Message::Answer);
+    retval.setId(msg.id());
+    retval.setSource(msg.destination());
+    retval.setDestination(msg.source());
+    retval.setPath(msg.path());
+    retval.setData("msg.data()");
+
+    ts->send(retval);
+  }
+}; // !qi
---
+++ libqimessaging/bin/qiservice/qiservicetestmain.cpp
@@ -0,0 +1,83 @@
+/*
+** Author(s):
+**  - Herve Cuche <hcuche@aldebaran-robotics.com>
+**
+** Copyright (C) 2012 Aldebaran Robotics
+*/
+
+#include <iostream>
+#include <string>
+
+#include <boost/program_options.hpp>
+
+#include <qi/os.hpp>
+#include "qiservicetest.hpp"
+
+namespace po = boost::program_options;
+
+int main(int argc, char *argv[])
+{
+  // declare the program options
+  po::options_description desc("Usage:\n  qi-service masterAddress [options]\nOptions");
+  desc.add_options()
+      ("help", "Print this help.")
+      ("master-address",
+       po::value<std::string>()->default_value(std::string("127.0.0.1:5555")),
+       "The master address");
+
+  // allow master address to be specified as the first arg
+  po::positional_options_description pos;
+  pos.add("master-address", 1);
+
+  // parse and store
+  po::variables_map vm;
+  try
+  {
+    po::store(po::command_line_parser(argc, argv).
+              options(desc).positional(pos).run(), vm);
+    po::notify(vm);
+
+    if (vm.count("help"))
+    {
+      std::cout << desc << "\n";
+      return 0;
+    }
+
+    if (vm.count("master-address") == 1)
+    {
+      qi::Session       session;
+      qi::ServiceTest       st;
+
+      st.start("127.0.0.1:9571");
+      std::cout << "ready." << std::endl;
+
+      qi::EndpointInfo e;
+      e.ip = "127.0.0.1";
+      e.port = 9571;
+      e.type = "tcp";
+
+      std::string masterAddress = vm["master-address"].as<std::string>();
+
+      session.setName("serviceTest");
+      session.setDestination("qi.master");
+      session.connect(masterAddress);
+      session.waitForConnected();
+      session.registerEndpoint(e);
+
+      while (1)
+        qi::os::sleep(1);
+
+      session.unregisterEndpoint(e);
+    }
+    else
+    {
+      std::cout << desc << "\n";
+    }
+  }
+  catch (const boost::program_options::error&)
+  {
+    std::cout << desc << "\n";
+  }
+
+  return 0;
+}
