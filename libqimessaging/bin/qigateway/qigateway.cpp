e3686e15412742bd7207af151874035470b1439e
Herve CUCHE <hcuche@aldebaran-robotics.com>
2012-02-10 11:07:04 +0100

    Create a class qigateway and clean up source code
    Change-Id: Ib2ba0401dfbc70f107337fb26f2c87e230305171

--- libqimessaging/bin/qigateway/qigateway.cpp
+++ libqimessaging/bin/qigateway/qigateway.cpp
@@ -1,41 +1,39 @@
 /*
 ** Author(s):
-**  - Cedric GESTES <gestes@aldebaran-robotics.com>
+**  - Herve Cuche <hcuche@aldebaran-robotics.com>
 **
-** Copyright (C) 2010, 2012 Aldebaran Robotics
+** Copyright (C) 2012 Aldebaran Robotics
 */
 
 #include <iostream>
 #include <vector>
 #include <map>
+
 #include <qimessaging/transport.hpp>
 #include <qimessaging/datastream.hpp>
 #include <qimessaging/session.hpp>
 #include <qi/os.hpp>
-#include <boost/program_options.hpp>
 
-namespace po = boost::program_options;
+#include "qigateway.hpp"
 
 static int id = 300;
 
-
-class Gateway : public qi::TransportServerDelegate
+namespace qi
 {
-public:
-  Gateway()
+  Gateway::Gateway()
   {
     _nthd = new qi::NetworkThread();
-
     _ts = new qi::TransportServer();
     _ts->setDelegate(this);
   }
 
-  ~Gateway()
+  Gateway::~Gateway()
   {
     delete _ts;
+    delete _nthd;
   }
 
-  void start(const std::string &address)
+  void Gateway::start(const std::string &address)
   {
     qi::EndpointInfo e;
     size_t begin = 0;
@@ -53,59 +51,56 @@ public:
     _ts->start(e.ip, e.port, _nthd->getEventBase());
   }
 
-  virtual void onConnected(const qi::Message &msg)
+  void Gateway::onConnected(const qi::Message &msg)
   {
-    //    std::cout << "connected qiservice: " << msg << std::endl;
   }
 
-  virtual void onWrite(const qi::Message &msg)
+  void Gateway::onWrite(const qi::Message &msg)
   {
-    //    std::cout << "written qiservice: " << msg << std::endl;
   }
 
-  virtual void onRead(const qi::Message &msg)
+  void Gateway::onRead(const qi::Message &msg)
   {
-    std::cout << "read qigateway: " << msg << std::endl;
+    std::cout << "qigateway read" << std::endl;
 
     if (msg.path() == "services")
     {
       services(msg);
-      return;
     }
-
-    if (msg.path() == "service")
+    else if (msg.path() == "service")
     {
       service(msg);
-      return;
-    }
-
-    qi::TransportSocket* ts;
-    std::map<std::string, qi::TransportSocket*>::iterator it = _serviceConnection.find(msg.destination());
-    // no connection to service
-    if (it == _serviceConnection.end())
-    {
-      ts = _session.service(msg.destination());
-      _serviceConnection[msg.destination()] = ts;
     }
     else
     {
-      ts = it->second;
+      qi::TransportSocket* ts;
+      std::map<std::string, qi::TransportSocket*>::iterator it = _serviceConnection.find(msg.destination());
+      // no connection to service
+      if (it == _serviceConnection.end())
+      {
+        ts = _session.service(msg.destination());
+        _serviceConnection[msg.destination()] = ts;
+      }
+      else
+      {
+        ts = it->second;
+      }
+
+      qi::Message fwd(msg);
+      fwd.setId(id++);
+      fwd.setSource("gateway");
+      ts->send(fwd);
+      ts->waitForId(fwd.id());
+      qi::Message ans;
+      ts->read(fwd.id(), &ans);
+      ans.setId(msg.id());
+      ans.setDestination(msg.source());
+
+      _ts->send(ans);
     }
-
-    qi::Message fwd(msg);
-    fwd.setId(id++);
-    fwd.setSource("gateway");
-    ts->send(fwd);
-    ts->waitForId(fwd.id());
-    qi::Message ans;
-    ts->read(fwd.id(), &ans);
-    ans.setId(msg.id());
-    ans.setDestination(msg.source());
-
-    _ts->send(ans);
   }
 
-  void services(const qi::Message &msg)
+  void Gateway::services(const qi::Message &msg)
   {
     std::vector<std::string> result = _session.services();
 
@@ -124,7 +119,7 @@ public:
   }
 
 
-  void service(const qi::Message &msg)
+  void Gateway::service(const qi::Message &msg)
   {
     qi::DataStream d;
     d << _endpoints;
@@ -140,8 +135,8 @@ public:
     _ts->send(retval);
   }
 
-  void registerGateway(const std::string &masterAddress,
-                       const std::string &gatewayAddress)
+  void Gateway::registerGateway(const std::string &masterAddress,
+                                const std::string &gatewayAddress)
   {
     qi::EndpointInfo e;
 
@@ -163,7 +158,7 @@ public:
     _session.registerEndpoint(e);
   }
 
-  void unregisterGateway(const std::string &gatewayAddress)
+  void Gateway::unregisterGateway(const std::string &gatewayAddress)
   {
     qi::EndpointInfo e;
 
@@ -181,80 +176,4 @@ public:
     _session.unregisterEndpoint(e);
   }
 
-private:
-  qi::Session          _session;
-  qi::NetworkThread   *_nthd;
-  qi::TransportServer *_ts;
-  std::vector<qi::EndpointInfo>               _endpoints;
-  std::map<std::string, qi::TransportSocket*> _serviceConnection;
-};
-
-
-int main(int argc, char *argv[])
-{
-  // declare the program options
-  po::options_description desc("Usage:\n  qi-service masterAddress [options]\nOptions");
-  desc.add_options()
-      ("help", "Print this help.")
-      ("master-address",
-       po::value<std::string>()->default_value(std::string("127.0.0.1:5555")),
-       "The master address")
-      ("gateway-address",
-       po::value<std::string>()->default_value(std::string("127.0.0.1:12345")),
-       "The gateway address");
-
-  // allow master address to be specified as the first arg
-  po::positional_options_description pos;
-  pos.add("master-address", 1);
-
-  // parse and store
-  po::variables_map vm;
-  try
-  {
-    po::store(po::command_line_parser(argc, argv).
-              options(desc).positional(pos).run(), vm);
-    po::notify(vm);
-
-    if (vm.count("help"))
-    {
-      std::cout << desc << "\n";
-      return 0;
-    }
-
-    if (vm.count("master-address") == 1 &&
-        vm.count("gateway-address") == 1 )
-    {
-      std::string gatewayAddress = vm["gateway-address"].as<std::string>();
-      std::string masterAddress = vm["master-address"].as<std::string>();
-
-      Gateway           gate;
-      gate.start(gatewayAddress);
-      std::cout << "ready." << std::endl;
-
-      gate.registerGateway(masterAddress, gatewayAddress);
-
-//      qi::Message msg;
-//      msg.setId(1000);
-//      msg.setSource("gateway");
-//      msg.setDestination("qi.master");
-//      msg.setPath("services");
-//      msg.setType(qi::Message::Call);
-//      gate.services(msg);
-
-      while (1)
-        qi::os::sleep(1);
-
-      gate.unregisterGateway(gatewayAddress);
-    }
-    else
-    {
-      std::cout << desc << "\n";
-    }
-  }
-  catch (const boost::program_options::error&)
-  {
-    std::cout << desc << "\n";
-  }
-
-  return 0;
-}
+} // !qi
---
+++ libqimessaging/bin/qigateway/qigatewaymain.cpp
@@ -0,0 +1,78 @@
+/*
+** Author(s):
+**  - Herve Cuche <hcuche@aldebaran-robotics.com>
+**
+** Copyright (C) 2012 Aldebaran Robotics
+*/
+
+#include <iostream>
+#include <string>
+
+#include <boost/program_options.hpp>
+
+#include <qi/os.hpp>
+#include "qigateway.hpp"
+
+
+namespace po = boost::program_options;
+
+int main(int argc, char *argv[])
+{
+  // declare the program options
+  po::options_description desc("Usage:\n  qi-service masterAddress [options]\nOptions");
+  desc.add_options()
+      ("help", "Print this help.")
+      ("master-address",
+       po::value<std::string>()->default_value(std::string("127.0.0.1:5555")),
+       "The master address")
+      ("gateway-address",
+       po::value<std::string>()->default_value(std::string("127.0.0.1:12345")),
+       "The gateway address");
+
+  // allow master address to be specified as the first arg
+  po::positional_options_description pos;
+  pos.add("master-address", 1);
+
+  // parse and store
+  po::variables_map vm;
+  try
+  {
+    po::store(po::command_line_parser(argc, argv).
+              options(desc).positional(pos).run(), vm);
+    po::notify(vm);
+
+    if (vm.count("help"))
+    {
+      std::cout << desc << "\n";
+      return 0;
+    }
+
+    if (vm.count("master-address") == 1 &&
+        vm.count("gateway-address") == 1 )
+    {
+      std::string gatewayAddress = vm["gateway-address"].as<std::string>();
+      std::string masterAddress = vm["master-address"].as<std::string>();
+
+      qi::Gateway gate;
+      gate.start(gatewayAddress);
+      std::cout << "ready." << std::endl;
+
+      gate.registerGateway(masterAddress, gatewayAddress);
+
+      while (1)
+        qi::os::sleep(1);
+
+      gate.unregisterGateway(gatewayAddress);
+    }
+    else
+    {
+      std::cout << desc << "\n";
+    }
+  }
+  catch (const boost::program_options::error&)
+  {
+    std::cout << desc << "\n";
+  }
+
+  return 0;
+}
