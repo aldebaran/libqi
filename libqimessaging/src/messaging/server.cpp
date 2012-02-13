1b6ac1e9396ed811d0d2be5bacd6cad41bce581a
Cedric GESTES <gestes@aldebaran-robotics.com>
2012-02-13 16:59:14 +0100

    server: use new TransportSocketInterface
    Change-Id: I10da0bd07f2c6c7a93a0dd2b383311c0df5f8dae

--- libqimessaging/src/messaging/server.cpp
+++ libqimessaging/src/messaging/server.cpp
@@ -13,7 +13,46 @@
 
 namespace qi {
 
-  class ServerPrivate {
+  class ServerPrivate : public TransportServerInterface, public TransportSocketInterface {
+  public:
+
+    virtual void newConnection() {
+      TransportSocket *socket = _ts.nextPendingConnection();
+      if (!socket)
+        return;
+      socket->setDelegate(this);
+    };
+
+    virtual void onRead(TransportSocket *client, const qi::Message &msg) {
+      qi::Object *obj;
+
+      obj = _services[msg.destination()];
+      qi::DataStream ds(msg.data());
+      qi::DataStream rs;
+
+      obj->metaCall(msg.path(), "", ds, rs);
+
+      qi::Message retval;
+      retval.setType(qi::Message::Answer);
+      retval.setId(msg.id());
+      retval.setSource(msg.destination());
+      retval.setDestination(msg.source());
+      retval.setPath(msg.path());
+      retval.setData(rs.str());
+      client->send(retval);
+    };
+
+    virtual void onWrite(TransportSocket *client, const qi::Message &msg) {
+    };
+
+    virtual void onConnected(TransportSocket *client, const Message &msg)
+    {
+    }
+
+    virtual void onDisconnected(TransportSocket *client, const Message &msg)
+    {
+    }
+
   public:
     std::map<std::string, qi::Object*> _services;
     TransportServer                    _ts;
@@ -30,40 +69,13 @@ namespace qi {
     delete _p;
   }
 
-
   void Server::start(const std::string &addr, unsigned short port, qi::NetworkThread *base) {
-
+    _p->_ts.setDelegate(_p);
     _p->_ts.start(addr, port, base->getEventBase());
   };
 
   void Server::advertiseService(const std::string &name, qi::Object *obj) {
     _p->_services[name] = obj;
-  };
-
-  void Server::onConnected(const qi::Message &msg) {
-  };
-
-  void Server::onWrite(const qi::Message &msg) {
-
-  };
-
-  void Server::onRead(const qi::Message &msg) {
-    qi::Object *obj;
-
-    obj = _p->_services[msg.destination()];
-    qi::DataStream ds(msg.data());
-    qi::DataStream rs;
-
-    obj->metaCall(msg.path(), "", ds, rs);
-
-    qi::Message retval;
-    retval.setType(qi::Message::Answer);
-    retval.setId(msg.id());
-    retval.setSource(msg.destination());
-    retval.setDestination(msg.source());
-    retval.setPath(msg.path());
-    retval.setData(rs.str());
-    //_p->_ts.send(retval);
   };
 
 }
