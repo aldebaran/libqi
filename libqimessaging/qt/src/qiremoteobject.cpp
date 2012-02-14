0611aef04242347a98df9b64f47361aee3eeb333
Cedric GESTES <gestes@aldebaran-robotics.com>
2012-02-14 12:29:19 +0100

    add qiremoteobject and qmetaobjectbuilder (from qt5)
    Change-Id: Ic7d5549e87c64e1446d1294860c3c2fa4a411dbc

---
+++ libqimessaging/qt/src/qiremoteobject.cpp
@@ -0,0 +1,62 @@
+/*
+** Author(s):
+**  - Cedric GESTES <gestes@aldebaran-robotics.com>
+**
+** Copyright (C) 2012 Aldebaran Robotics
+*/
+
+#include <iostream>
+#include "qiremoteobject_p.h"
+#include "qmetaobjectbuilder_p.h"
+
+class QiRemoteObjectPrivate {
+public:
+  qi::TransportSocket *socket;
+  std::string          dest;
+  QMetaObject         *meta;
+};
+
+
+QiRemoteObject::QiRemoteObject(qi::TransportSocket *ts, const std::string &dest)
+  : _p (new QiRemoteObjectPrivate)
+{
+  QMetaObjectBuilder mob;
+  mob.setClassName("QiRemoteObject");
+  //QMetaMethodBuilder b = sup.addSignal("errorUnrecoverableIPCFault(QService::UnrecoverableIPCError)");
+  //d->ipcfailure = b.index();
+  _p->meta = sup.toMetaObject();
+}
+
+QiRemoteObject::~QiRemoteObject()
+{
+}
+
+const QMetaObject* QiRemoteObject::metaObject() const {
+  std::cout << "metaObject" << std::endl;
+  return _p->meta;
+};
+
+int                QiRemoteObject::qt_metacall(QMetaObject::Call c, int id, void **a) {
+  std::cout << "qt_metacall" << std::endl;
+};
+
+void              *QiRemoteObject::qt_metacast(const char* className) {
+  std::cout << "qt_metacast" << std::endl;
+}
+
+
+//virtual void metaCall(const std::string &method, const std::string &sig, qi::DataStream &in, qi::DataStream &out) {
+//  qi::Message msg;
+//  msg.setId(200);
+//  msg.setSource("ouame");
+//  msg.setDestination(_dest);
+//  msg.setPath(method);
+//  msg.setData(in.str());
+//  _ts->send(msg);
+//  _ts->waitForId(msg.id());
+
+//  qi::Message ret;
+//  _ts->read(msg.id(), &ret);
+//  out.str(ret.data());
+//}
+
---
+++ libqimessaging/qt/src/qiremoteobject_p.h
@@ -0,0 +1,32 @@
+/*
+** Author(s):
+**  - Cedric GESTES <gestes@aldebaran-robotics.com>
+**
+** Copyright (C) 2012 Aldebaran Robotics
+*/
+
+#ifndef   	QIREMOTEOBJECT_P_H_
+# define   	QIREMOTEOBJECT_P_H_
+
+#include <QtCore/qobject.h>
+
+namespace qi {
+  class TransportSocket;
+};
+
+class QiRemoteObjectPrivate;
+class QiRemoteObject : public QObject {
+public:
+  explicit QiRemoteObject(qi::TransportSocket *ts, const std::string &dest);
+  virtual ~QiRemoteObject();
+
+  // provide custom Q_OBJECT implementation
+  virtual const QMetaObject* metaObject() const;
+  virtual int qt_metacall(QMetaObject::Call c, int id, void **a);
+  virtual void *qt_metacast(const char* className);
+
+protected:
+  QiRemoteObjectPrivate *_p;
+};
+
+#endif 	    /* !QIREMOTEOBJECT_P_H_ */
