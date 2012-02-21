02bcaedd7718022683cf118cae02a99f8e48888a
Cedric GESTES <gestes@aldebaran-robotics.com>
2012-02-21 16:42:34 +0100

    read example_qi_signature_pp
    Change-Id: I4329c49a1990e286cc27b535e1babc13b7d49230

--- libqimessaging/examples/example_qi_signature_pp.cpp
+++ libqimessaging/examples/example_qi_signature_pp.cpp
@@ -7,18 +7,21 @@ int main()
   //pretty print representation of a signature
 
   //POD
-  std::cout << "b                    : " <<  qi::signatureToString("b") << std::endl;
-  std::cout << "i                    : " <<  qi::signatureToString("i") << std::endl;
-  std::cout << "f                    : " <<  qi::signatureToString("f") << std::endl;
-  std::cout << "d                    : " <<  qi::signatureToString("d") << std::endl;
-  std::cout << "s                    : " <<  qi::signatureToString("s") << std::endl;
-
-  //protobuf currently throws qi::BadSignatureError
-  //std::cout << "@ALCompat.ALValue@   : " <<  qi::signatureToString("@ALCompat.ALValue@") << std::endl;
+  std::cout << "b                    : " <<  qi::Signature("b").toSTLSignature() << std::endl;
+  std::cout << "i                    : " <<  qi::Signature("i").toSTLSignature() << std::endl;
+  std::cout << "f                    : " <<  qi::Signature("f").toSTLSignature() << std::endl;
+  std::cout << "d                    : " <<  qi::Signature("d").toSTLSignature() << std::endl;
+  std::cout << "s                    : " <<  qi::Signature("s").toSTLSignature() << std::endl;
 
   //STL types
-  std::cout << "[i]                  : " <<  qi::signatureToString("[i]") << std::endl;
-  std::cout << "{is}                 : " <<  qi::signatureToString("{is}") << std::endl;
+  std::cout << "[i]                  : " <<  qi::Signature("[i]").toSTLSignature() << std::endl;
+  std::cout << "{is}                 : " <<  qi::Signature("{is}").toSTLSignature() << std::endl;
+
+
+  //Qt types
+  std::cout << "({is}**)             : " <<  qi::Signature("({is}**)").toQtSignature() << std::endl;
+  std::cout << "({is}**)             : " <<  qi::Signature("({is}**)").toQtSignature(true) << std::endl;
+  std::cout << "(s{is}**[s][{si}])   : " <<  qi::Signature("(s{is}**[s][{si}])").toQtSignature(true) << std::endl;
 
   return 0;
 }
