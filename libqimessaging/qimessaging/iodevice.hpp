7cc813b7d2c95bc603ebdd6515689f73de1cb2e2
Cedric GESTES <gestes@aldebaran-robotics.com>
2012-02-22 18:56:00 +0100

    cleanup
    Change-Id: Ic0b4f7b4e09d56484e9b51a0f0168bded91bf697

--- libqimessaging/qimessaging/iodevice.hpp
+++ libqimessaging/qimessaging/iodevice.hpp
@@ -19,9 +19,6 @@ namespace qi
     virtual int            read(void *data, size_t size) = 0;
     virtual void          *peek(size_t size)             = 0;
     virtual int            drain(size_t size)            = 0;
-    //TODO:
-    //virtual void          *data() = 0;
-    //virtual void           setData(void *data) = 0;
 
     virtual size_t         size() const = 0;
   }; // !IODevice
