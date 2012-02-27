b6bb71a59f3704bcb50ef69f281e0a5a64363c4d
Laurent LEC <llec@aldebaran-robotics.com>
2012-02-27 17:37:13 +0100

    Add missing setPath in QiRemoteObject::qt_metacall().
    Change-Id: I2c194d55295c4c3783f4f5a82fc539f324e10f0a

--- libqimessaging/qt/src/qiremoteobject.cpp
+++ libqimessaging/qt/src/qiremoteobject.cpp
@@ -83,6 +83,7 @@ int                QiRemoteObject::qt_metacall(QMetaObject::Call c, int id, void
 
   msg.setType(qi::Message::Type_Call);
   msg.setService(_p->serviceId);
+  msg.setPath(qi::Message::Path_Main);
   msg.setFunction(id - _p->meta->methodOffset());
   _p->socket->send(msg);
 
