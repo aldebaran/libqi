dc75c9b077b5207ea1e1b28f1cf2f2eead20d0c8
Laurent LEC <llec@aldebaran-robotics.com>
2012-03-05 14:14:17 +0100

    Draft for qi::shared_ptr<T>.
    Change-Id: Ifa8db05c12f856e01abb64f855926c76cffc15cc
    Reviewed-on: http://gerrit.aldebaran.lan:8080/286
    Reviewed-by: Cedric GESTES <gestes@aldebaran-robotics.com>
    Tested-by: Cedric GESTES <gestes@aldebaran-robotics.com>

---
+++ qi/shared_ptr.hpp
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2012 Aldebaran Robotics. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the COPYING file.
+ */
+
+#pragma once
+
+#ifndef _LIBQI_QI_SHARED_PTR_HPP_
+#define _LIBQI_QI_SHARED_PTR_HPP_
+
+#include <qi/atomic.hpp>
+#include <qi/log.hpp>
+
+namespace qi
+{
+  template <typename T>
+  class SharedPtr
+  {
+  public:
+    SharedPtr(T *ptr)
+      : _ptr(ptr)
+      , _refcount(new qi::atomic<long>(1))
+    {
+    }
+
+    ~SharedPtr()
+    {
+      if (--(*_refcount) == 0)
+      {
+        delete _ptr;
+        delete _refcount;
+      }
+    }
+
+    SharedPtr(const SharedPtr<T> &sp)
+    {
+      /*
+       * Note that this line is racy.
+       * If someone is deleting _refcount,
+       * it cannot be used below.
+       */
+      if (++(*_refcount) != 1)
+      {
+        _ptr = sp._ptr;
+      }
+      else
+      {
+        qiLogDebug("qi.log.shared_ptr")
+                  << "tried to copy a shared pointer targeted for deletion"
+                  << std::endl;
+      }
+
+      return *this;
+    }
+
+    SharedPtr& operator=(SharedPtr<T> &sp)
+    {
+      // release the current pointer
+      if (--(*_refcount) == 0)
+      {
+        delete _ptr;
+        delete _refcount;
+      }
+      _ptr = sp._ptr;
+      _refcount = sp._refcount;
+    }
+
+    T *operator*(SharedPtr<T> &sp)
+    {
+      return sp._ptr;
+    }
+
+  private:
+    T                *_ptr;
+    qi::atomic<long> *_refcount;
+  };
+}
+
+#endif // _LIBQI_QI_SHARED_PTR_HPP_
