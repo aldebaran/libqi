834acf47c33e5d0a75e8357237e9086fd76cb8df
Cedric GESTES <gestes@aldebaran-robotics.com>
2012-03-06 22:42:22 +0100

    perf_pool
    Change-Id: Iae4c9d85f97f4f5e6bd2a1628b3c467eab5b4199
    Reviewed-on: http://gerrit.aldebaran.lan:8080/309
    Reviewed-by: Laurent LEC <llec@aldebaran-robotics.com>
    Tested-by: Laurent LEC <llec@aldebaran-robotics.com>

---
+++ libqimessaging/perfs/perf_pool.cpp
@@ -0,0 +1,51 @@
+/*
+** Author(s):
+**  - Cedric GESTES <gestes@aldebaran-robotics.com>
+**
+** Copyright (C) 2010 Aldebaran Robotics
+*/
+
+#include <iostream>
+#include "dataperftimer.hpp"
+#include <boost/pool/pool.hpp>
+#include <boost/pool/object_pool.hpp>
+
+struct MyStruct {
+  char b[3223];
+  int x;
+};
+
+
+static const int gLoopCount = 10000000;
+int main(int argc, char **argv)
+{
+  qi::perf::DataPerfTimer dp;
+
+  unsigned long long toto = 0;
+
+  boost::object_pool<MyStruct> op;
+  dp.start(gLoopCount);
+  for (int i = 0; i < gLoopCount; ++i) {
+    MyStruct *ms = op.malloc();
+    ms->x = 2;
+    toto += (unsigned long long)ms;
+    op.free(ms);
+  }
+  dp.stop();
+  //only need to use toto (to force the code used)
+  std::cout << "toto:" << (int)toto << std::endl;
+
+  dp.start(gLoopCount);
+  for (int i = 0; i < gLoopCount; ++i) {
+    MyStruct *ms = new MyStruct;
+    ms->x = 2;
+    toto += (unsigned long long)ms;
+    free(ms);
+  }
+  dp.stop();
+  //only need to use toto (to force the code used)
+  std::cout << "toto:" << (int)toto << std::endl;
+
+  return 0;
+}
+
