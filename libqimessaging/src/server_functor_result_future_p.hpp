cacd6cc05c1c529515155fc60051847969caa6dc
Cedric GESTES <gestes@aldebaran-robotics.com>
2012-03-07 02:48:37 +0100

    added ServerFunctorResultFuture
    Change-Id: I8c3c7dfc0dffbf6863ac40fa03b8516fc2842ca9
    Reviewed-on: http://gerrit.aldebaran.lan:8080/313
    Reviewed-by: Laurent LEC <llec@aldebaran-robotics.com>
    Tested-by: Laurent LEC <llec@aldebaran-robotics.com>

---
+++ libqimessaging/src/server_functor_result_future_p.hpp
@@ -0,0 +1,33 @@
+/*
+** Author(s):
+**  - Cedric GESTES <gestes@aldebaran-robotics.com>
+**
+** Copyright (C) 2012 Aldebaran Robotics
+*/
+
+#ifndef  SERVER_FUNCTOR_RETURN_FUTURE_P_HPP_
+# define SERVER_FUNCTOR_RETURN_FUTURE_P_HPP_
+
+namespace qi {
+
+  class ServerFunctorResultPromise : public FunctorResultPromiseBase {
+  public:
+    ServerFunctorResultPromise(TransportSocket *client, const qi::Message &req)
+      : _client(client)
+    {
+      _retval.buildReplyFrom(req);
+    };
+
+    inline virtual void setValue(qi::FunctorResult &result) {
+      _retval.setBuffer(result.buffer());
+      _client->send(_retval);
+    }
+
+  private:
+    qi::Message          _retval;
+    qi::TransportSocket *_client;
+  };
+
+}
+
+#endif
