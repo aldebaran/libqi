087861b9de2ed30cb109bc39f2be6332eb62b54d
platform <platform@aldebaran-robotics.com>
2012-03-13 16:26:52 +0100

    Fix qi::atomic for Windows.
    Change-Id: I0dc162432a31517e7f746a84e88a225b8ed2af05
    Reviewed-on: http://gerrit.aldebaran.lan:8080/369
    Reviewed-by: Laurent LEC <llec@aldebaran-robotics.com>
    Tested-by: Laurent LEC <llec@aldebaran-robotics.com>

--- qi/atomic.hpp
+++ qi/atomic.hpp
@@ -9,14 +9,12 @@
 #ifndef _LIBQI_QI_ATOMIC_HPP_
 #define _LIBQI_QI_ATOMIC_HPP_
 
-#ifdef _MSC_VER
-# include <windows.h>
-#endif
+# include <qi/config.hpp>
 
 namespace qi
 {
   template <typename T>
-  class atomic
+  class QI_API atomic
   {
   public:
     atomic()
@@ -41,46 +39,6 @@ namespace qi
   private:
     T _value;
   };
-
-#ifdef __GNUC__
-    template <typename T>
-    T atomic<T>::operator++()
-    {
-      return __sync_add_and_fetch(&_value, 1);
-    }
-
-    template <typename T>
-    T atomic<T>::operator--()
-    {
-      return __sync_sub_and_fetch(&_value, 1);
-    }
-#endif
-
-#ifdef _MSC_VER
-  template<>
-  short atomic<short>::operator++()
-  {
-    return _InterlockedIncrement16(&_value);
-  }
-
-  template<>
-  short atomic<short>::operator--()
-  {
-    return _InterlockedDecrement16(&_value);
-  }
-
-  template <>
-  long atomic<long>::operator++()
-  {
-    return _InterlockedIncrement(&_value);
-  }
-
-  template <>
-  long atomic<long>::operator--()
-  {
-    return _InterlockedDecrement(&_value);
-  }
-#endif
 }
 
 #endif // _LIBQI_QI_ATOMIC_HPP_
