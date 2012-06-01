ee18ecc1431d20c5048d6f049e56e91c9bbcc0c2
Cedric GESTES <gestes@aldebaran-robotics.com>
2012-06-01 16:35:08 +0200

    transportSocket (libevent): better error handling and message
    Change-Id: I8c49b83e7821f3f2c7c15afb989dd1337ea29b8f
    Reviewed-on: http://gerrit.aldebaran.lan:8080/1540
    Reviewed-by: hcuche <hcuche@aldebaran-robotics.com>
    Tested-by: hcuche <hcuche@aldebaran-robotics.com>
    Reviewed-by: proullon <proullon@aldebaran-robotics.com>
    Tested-by: proullon <proullon@aldebaran-robotics.com>

--- libqimessaging/src/transport_socket_libevent_p.cpp
+++ libqimessaging/src/transport_socket_libevent_p.cpp
@@ -126,38 +126,22 @@ namespace qi
       if (tcd)
         tcd->onSocketConnected(self);
     }
-#ifdef WIN32
-    /* On windows, BEV_EVENT_EOF doesn't pop on service kill
-    ** but BEV_EVENT_READING and BEV_EVENT_ERROR suggest an error while reading.
-    ** For a better fix, need to find out how to make libevent pop the proper error. (pr)
-    */
-    else if ((events & BEV_EVENT_READING) && (events & BEV_EVENT_ERROR))
-#else
-    else if (events & BEV_EVENT_EOF)
-#endif
+    else if ((events & BEV_EVENT_EOF) || (events & BEV_EVENT_ERROR))
     {
       bufferevent_free(bev);
       bev = 0;
-      connected = false;
-      //for waitForId
-      cond.notify_all();
-      if (tcd)
-        tcd->onSocketDisconnected(self);
-      // connection has been closed, do any clean up here
-    }
-    else if (events & BEV_EVENT_ERROR)
-    {
-      bufferevent_free(bev);
-      bev = 0;
+      bool wasco = connected;
       connected = false;
       //for waitForId
       cond.notify_all();
 
       if (tcd)
         tcd->onSocketConnectionError(self);
+      if (wasco && tcd)
+        tcd->onSocketDisconnected(self);
 
       // check errno to see what error occurred
-      qiLogError("qimessaging.TransportSocketLibevent")  << "Cannot connect" << std::endl;
+      qiLogVerbose("qimessaging.TransportSocketLibevent")  << "socket terminate (" << errno << "): " << strerror(errno) << std::endl;
     }
     else if (events & BEV_EVENT_TIMEOUT)
     {
