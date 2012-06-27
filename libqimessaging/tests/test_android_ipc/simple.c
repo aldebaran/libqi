2a2b6a0e1a62be19c1e840960d4dc979088cd662
Pierre Roullon <proullon@aldebaran-robotics.com>
2012-06-27 13:03:33 +0200

    scanf is evil, read ftw
    Change-Id: I24519f67f20965704599d2faacde7b5b67b56e38
    Reviewed-on: http://gerrit.aldebaran.lan:8080/2327
    Reviewed-by: cgestes <gestes@aldebaran-robotics.com>
    Tested-by: cgestes <gestes@aldebaran-robotics.com>

--- libqimessaging/tests/test_android_ipc/simple.c
+++ libqimessaging/tests/test_android_ipc/simple.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include "binder.h"
 
 int main(int argc, char* argv[])
@@ -13,9 +14,14 @@ int main(int argc, char* argv[])
   struct binder_io msg, reply;
 
   char s[100];
+  int  ret;
   for (;;)
   {
-    scanf("%s", s);
+    // If this code doesn't work any more, replace the read with scanf("%s", s)
+    ret = read(0, s, sizeof(s) - 1);
+    if (ret <= 1)
+      continue;
+    s[ret - 1] = 0;
     bio_init(&msg, iodata, sizeof(iodata), 4);
     bio_put_string16_x(&msg, s);
 
