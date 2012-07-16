dcb555c6b24799502ec7b7e429df51ece0d5681d
Herve CUCHE <hcuche@aldebaran-robotics.com>
2012-07-16 10:43:57 +0200

    Add session tests auto.
    Change-Id: I393d6497f1699ca1da258d8e26219cf3bdd1a988
    Reviewed-on: http://gerrit.aldebaran.lan:8080/2996
    Reviewed-by: proullon <proullon@aldebaran-robotics.com>
    Reviewed-by: llec <llec@aldebaran-robotics.com>
    Reviewed-by: hcuche <hcuche@aldebaran-robotics.com>
    Tested-by: hcuche <hcuche@aldebaran-robotics.com>

---
+++ libqimessaging/tests/test_session.cpp
@@ -0,0 +1,171 @@
+/*
+ ** Author(s):
+ **  - Herve Cuche <hcuche@aldebaran-robotics.com>
+ **
+ ** Copyright (C) 2010, 2012 Aldebaran Robotics
+ */
+
+#include <vector>
+#include <iostream>
+#include <string>
+
+#include <gtest/gtest.h>
+
+#include <qimessaging/session.hpp>
+#include <qimessaging/object.hpp>
+#include <qimessaging/server.hpp>
+#include <qimessaging/service_directory.hpp>
+#include <qimessaging/gateway.hpp>
+#include <qi/os.hpp>
+
+static std::string reply(const std::string &msg)
+{
+  return msg;
+}
+
+static std::string connectionAddr;
+
+
+TEST(QiSession, simpleConnectionToSd)
+{
+  qi::Session session;
+  session.connect(connectionAddr);
+  bool connected = session.waitForConnected(5000);
+
+  EXPECT_TRUE(connected);
+
+  session.disconnect();
+  bool disconnected = session.waitForDisconnected(5000);
+
+  EXPECT_TRUE(disconnected);
+}
+
+TEST(QiSession, simpleConnectionToNonReachableSd)
+{
+  qi::Session session;
+  session.connect("tcp://127.0.1.123:1245");
+  bool connected = session.waitForConnected(5000);
+
+  EXPECT_FALSE(connected);
+
+  session.disconnect();
+  bool disconnected = session.waitForDisconnected(5000);
+
+  EXPECT_TRUE(disconnected);
+}
+
+TEST(QiSession, simpleConnectionToInvalidAddrToSd)
+{
+  qi::Session session;
+  session.connect("tcp://0.0.0.0:0");
+  bool connected = session.waitForConnected(5000);
+
+  EXPECT_FALSE(connected);
+
+  session.disconnect();
+  bool disconnected = session.waitForDisconnected(5000);
+
+  EXPECT_TRUE(disconnected);
+}
+
+TEST(QiSession, simpleConnectionToInvalidSd)
+{
+  qi::Session session;
+  session.connect("invalidAddress");
+  bool connected = session.waitForConnected(5000);
+
+  EXPECT_FALSE(connected);
+
+  session.disconnect();
+  bool disconnected = session.waitForDisconnected(5000);
+
+  EXPECT_TRUE(disconnected);
+}
+
+TEST(QiSession, getSimpleService)
+{
+  qi::Session session;
+  session.connect(connectionAddr);
+  bool connected = session.waitForConnected(5000);
+  EXPECT_TRUE(connected);
+
+  qi::Object obj;
+  qi::Server srv;
+  obj.advertiseMethod("reply", &reply);
+
+  unsigned int servicePort = qi::os::findAvailablePort(0);
+  std::stringstream serviceAddr;
+  serviceAddr << "tcp://127.0.0.1:" << servicePort;
+
+  srv.listen(&session, serviceAddr.str());
+  unsigned int id = srv.registerService("serviceTest", &obj);
+
+  qi::Object *object = session.service("serviceTest");
+  EXPECT_TRUE(object);
+
+  delete object;
+  srv.close();
+  session.disconnect();
+  bool disconnected = session.waitForDisconnected(5000);
+
+  EXPECT_TRUE(disconnected);
+}
+
+TEST(QiSession, getUnregisterService)
+{
+  qi::Session session;
+  session.connect(connectionAddr);
+  bool connected = session.waitForConnected(5000);
+
+  EXPECT_TRUE(connected);
+
+  qi::Object *object = session.service("serviceTest");
+  EXPECT_FALSE(object);
+
+  session.disconnect();
+  bool disconnected = session.waitForDisconnected(5000);
+
+  EXPECT_TRUE(disconnected);
+}
+
+TEST(QiSession, getCloseService)
+{
+  qi::Session session;
+  session.connect(connectionAddr);
+  bool connected = session.waitForConnected(5000);
+  EXPECT_TRUE(connected);
+
+  qi::Object obj;
+  qi::Server srv;
+  obj.advertiseMethod("reply", &reply);
+
+  unsigned int servicePort = qi::os::findAvailablePort(0);
+  std::stringstream serviceAddr;
+  serviceAddr << "tcp://127.0.0.1:" << servicePort;
+
+  srv.listen(&session, serviceAddr.str());
+  unsigned int id = srv.registerService("serviceTest", &obj);
+  srv.close();
+
+  qi::Object *object = session.service("serviceTest");
+  EXPECT_FALSE(object);
+
+  session.disconnect();
+  bool disconnected = session.waitForDisconnected(5000);
+
+  EXPECT_TRUE(disconnected);
+}
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+
+  qi::ServiceDirectory sd;
+  unsigned int sdPort = qi::os::findAvailablePort(5555);
+  std::stringstream sdAddr;
+  sdAddr << "tcp://127.0.0.1:" << sdPort;
+  connectionAddr = sdAddr.str();
+
+  sd.listen(sdAddr.str());
+
+  return RUN_ALL_TESTS();
+}
