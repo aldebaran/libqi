219999a82ddbdb2833b6b3b07c2b50167474f1ca
Laurent LEC <llec@aldebaran-robotics.com>
2012-09-06 17:17:40 +0200

    Move Qt headers content to .h files
    Change-Id: I01c789864ab4f1ab73fc3b748bdcca7e47239bb0
    Reviewed-on: http://gerrit.aldebaran.lan:8080/4913
    Reviewed-by: cgestes <gestes@aldebaran-robotics.com>
    Tested-by: llec <llec@aldebaran-robotics.com>

---
+++ libqimessaging/qt/qimessaging/qt/qitransportserver.h
@@ -0,0 +1,45 @@
+/*
+** Author(s):
+**  - Laurent LEC <llec@aldebaran-robotics.com>
+**
+** Copyright (C) 2012 Aldebaran Robotics
+*/
+
+#ifndef QITRANSPORTSERVER_H_
+# define QITRANSPORTSERVER_H_
+
+# include <qimessaging/api.hpp>
+# include <qimessaging/qt/QiTransportSocket>
+
+# include <QObject>
+# include <QByteArray>
+
+class QiTransportServerPrivate;
+
+class QIMESSAGING_API QiTransportServer : public QObject
+{
+  Q_OBJECT
+
+public:
+  QiTransportServer(QObject* parent = 0);
+  virtual ~QiTransportServer();
+
+  bool listen(const QUrl& listenURL);
+  QUrl listeningUrl();
+  void close();
+
+  bool hasPendingConnections() const;
+  QiTransportSocket* nextPendingConnection();
+  bool isListening() const;
+
+  void setIdentity(const QByteArray& key,
+                   const QByteArray& certificate);
+
+signals:
+  void newConnection();
+
+public:
+  QiTransportServerPrivate* _p;
+};
+
+#endif /* ifndef QITRANSPORTSERVER_H_ */
