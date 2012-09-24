a6b47affa7b0945721bf03c076716192b488125a
Pierre Roullon <proullon@aldebaran-robotics.com>
2012-09-24 13:35:52 +0200

    qiservice.py : get sys.argv only where needed
    Change-Id: I5914686ca3d4b9dd445c158b7eaef6a5a1f274c9
    Reviewed-on: http://gerrit.aldebaran.lan:8080/6074
    Reviewed-by: proullon <proullon@aldebaran-robotics.com>
    Tested-by: proullon <proullon@aldebaran-robotics.com>

--- libqimessaging/python/examples/qiservice.py
+++ libqimessaging/python/examples/qiservice.py
@@ -15,21 +15,20 @@ from qimessaging import ObjectBuilder
 def service_reply(string):
     """ Famous reply::s(s) function
     """
-    print "recv: %s" % string
     ret = "%sbim" % string
     return ret
 
-def get_servicedirectory_address(argv):
+def get_servicedirectory_address():
     """ Parse command line arguments
 
     Print usage is service directory address is not set.
     """
-    if len(argv) != 2:
-        print 'Usage : %s qi-service.py directory-address' % argv[0]
+    if len(sys.argv) != 2:
+        print 'Usage : %s qi-service.py directory-address' % sys.argv[0]
         print 'Assuming service directory address is tcp://127.0.0.1:5555'
         return "tcp://127.0.0.1:5555"
 
-    return argv[1]
+    return sys.argv[1]
 
 
 def main():
@@ -39,7 +38,7 @@ def main():
     app = Application()
 
     #1 Check if user give us service directory address.
-    sd_addr = get_servicedirectory_address(sys.argv)
+    sd_addr = get_servicedirectory_address()
 
     #2 Open a session onto service directory.
     try:
@@ -65,7 +64,7 @@ def main():
 
     #6 Clean
     session.unregister_service(idx)
-# main : Done.
+    #main : Done.
 
 if __name__ == "__main__":
     main()
