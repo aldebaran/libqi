ac5aa4a4e6a75dac944c5b18ad6e536f9441c086
Cedric GESTES <gestes@aldebaran-robotics.com>
2012-10-02 16:14:24 +0200

    plok
    Change-Id: I6f2f396c679298e6752d7e83111b98ab3a97c860

--- doc/qidoc/conf.in.py
+++ doc/qidoc/conf.in.py
@@ -1,5 +1,6 @@
 project = u'qi Messaging'
 
-sys.path.insert(0, os.path.abspath('..'))
-extensions += [ 'mycpp', 'extendcpp' ]
-
+#sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('/home/ctaf/src/qi-naoqi2/lib/qimessaging/doc/../../../tools/qibuild/python/qiapidoc'))
+extensions += [ 'extendcpp' ]
+qiapidoc_srcs = [os.path.abspath('../libqimessaging/build-doc/xml')]
