598bea4536f8b3c94fec49498d1ace3da1886f8e
Cedric GESTES <gestes@aldebaran-robotics.com>
2012-10-08 14:52:51 +0200

    add ObjectRegistrar
    Change-Id: Ie24468ac1cd672ca1d94002e963466f1ad0cc9dc

---
+++ libqimessaging/src/objectregistrar.cpp
@@ -0,0 +1,220 @@
+/*
+**  Copyright (C) 2012 Aldebaran Robotics
+**  See COPYING for the license
+*/
+#include <set>
+#include <qimessaging/genericobject.hpp>
+#include <qimessaging/transportserver.hpp>
+#include <qimessaging/serviceinfo.hpp>
+#include <qimessaging/objecttypebuilder.hpp>
+#include "server.hpp"
+#include "objectregistrar.hpp"
+#include "serverresult.hpp"
+#include "transportserver_p.hpp"
+#include <qi/os.hpp>
+#include <boost/thread/mutex.hpp>
+#include "servicedirectoryclient.hpp"
+#include "signal_p.hpp"
+
+namespace qi {
+
+
+  ObjectRegistrar::ObjectRegistrar(ServiceDirectoryClient *sdClient)
+    : _dying(false)
+    , _sdClient(sdClient)
+  {
+  }
+
+  ObjectRegistrar::~ObjectRegistrar()
+  {
+    _dying = true;
+  }
+
+//  //this function throw if the object is not found.
+//  //returning a null object would be more efficient in the normal case but...
+//  //this function is always used from bound method that will fail on error
+//  //so the metacall catch the exception and that's cool!
+//  qi::ObjectPtr ObjectRegistrar::object(unsigned int serviceId) {
+//    qi::ObjectPtr ret;
+
+//    {
+//      boost::mutex::scoped_lock sl(_servicesMutex);
+//      BoundServiceMap::iterator it;
+//      it = _services.find(serviceId);
+//      if (it != _services.end())
+//        ret = it->second.object;
+//    }
+//    if (!ret) {
+//      std::stringstream ss;
+//      ss << "Could not find service for id: " << serviceId;
+//      throw std::runtime_error(ss.str());
+//    }
+//    return ret;
+//  }
+
+
+  void ObjectRegistrar::onFutureFinished(qi::Future<unsigned int> fut, long id, qi::Promise<unsigned int> result)
+  {
+    qi::ServiceInfo              si;
+    RegisterServiceMap::iterator it;
+
+    {
+      boost::mutex::scoped_lock sl(_registerServiceRequestMutex);
+      it = _registerServiceRequest.find(id);
+      if (it != _registerServiceRequest.end())
+        si = it->second.second;
+      if (fut.hasError()) {
+        _registerServiceRequest.erase(id);
+        result.setError(fut.error());
+        return;
+      }
+    }
+    unsigned int idx = fut.value();
+    si.setServiceId(idx);
+    {
+      boost::mutex::scoped_lock sl(_servicesMutex);
+      BoundService bs;
+      bs.id          = idx;
+      bs.object      = it->second.first;
+      bs.serviceInfo = si;
+      bs.name        = si.name();
+      BoundServiceMap::iterator it;
+      it = _services.find(idx);
+      if (it != _services.end()) {
+        qiLogError("qi.server") << "A service is already registered with that id:" << idx;
+        result.setError("Service already registered.");
+        return;
+      }
+      _services[idx] = bs;
+      //todo register the object on the server (find a better way)
+      Server::addObject(idx, bs.object);
+    }
+
+    // ack the Service directory to tell that we are ready
+    //TODO: async handle.
+    _sdClient->serviceReady(idx);
+    {
+      boost::mutex::scoped_lock sl(_serviceNameToIndexMutex);
+      _serviceNameToIndex[si.name()] = idx;
+    }
+    {
+      boost::mutex::scoped_lock sl(_registerServiceRequestMutex);
+      _registerServiceRequest.erase(it);
+    }
+
+    result.setValue(idx);
+  }
+
+  qi::Future<unsigned int> ObjectRegistrar::registerService(const std::string &name, qi::ObjectPtr obj)
+  {
+    if (Server::endpoints().empty()) {
+      qiLogError("qimessaging.Server") << "Could not register service: " << name << " because the current server has not endpoint";
+      return qi::Future<unsigned int>();
+    }
+    qi::ServiceInfo si;
+    si.setName(name);
+    si.setProcessId(qi::os::getpid());
+    si.setMachineId(qi::os::getMachineId());
+
+    si.setEndpoints(Server::endpoints());
+
+    long id = ++_registerServiceRequestIndex;
+    {
+      boost::mutex::scoped_lock sl(_registerServiceRequestMutex);
+      _registerServiceRequest[id] = std::make_pair(obj, si);
+    }
+
+    qi::Promise<unsigned int> prom;
+    qi::Future<unsigned int>  future;
+    future = _sdClient->registerService(si);
+    future.connect(boost::bind<void>(&ObjectRegistrar::onFutureFinished, this, _1, id, prom));
+
+    return prom.future();
+  };
+
+  qi::Future<void> ObjectRegistrar::unregisterService(unsigned int idx)
+  {
+    qi::Future<void> future = _sdClient->unregisterService(idx);
+
+    std::string name;
+    {
+      boost::mutex::scoped_lock sl(_servicesMutex);
+      BoundServiceMap::iterator it = _services.find(idx);
+      if (it != _services.end()) {
+        name = it->second.name;
+      } else {
+        qiLogVerbose("qimessaging.Server") << "Can't find name associated to id:" << idx;
+      }
+      _services.erase(idx);
+      _server->removeObject(idx);
+    }
+    if (!name.empty())
+    {
+      boost::mutex::scoped_lock sl(_serviceNameToIndexMutex);
+      ServiceNameToIndexMap::iterator it = _serviceNameToIndex.find(name);
+      if (it != _serviceNameToIndex.end())
+        _serviceNameToIndex.erase(it);
+      else
+        qiLogVerbose("qimessaging.Server") << "Can't find idx associated to name :" << name;
+    }
+    return future;
+  }
+
+  std::vector<qi::ServiceInfo> ObjectRegistrar::registeredServices() {
+    std::vector<qi::ServiceInfo> ssi;
+    {
+      boost::mutex::scoped_lock sl(_servicesMutex);
+      BoundServiceMap::iterator it = _services.begin();
+      for (; it != _services.end(); ++it) {
+        //drop 0 => it's the server itself.
+        if (it->first == 0)
+          continue;
+        ssi.push_back(it->second.serviceInfo);
+      }
+    }
+    return ssi;
+  }
+
+  //return 0 on error (0 is the server which have no name)
+  unsigned int ObjectRegistrar::objectId(const std::string &name)
+  {
+    {
+      boost::mutex::scoped_lock sl(_serviceNameToIndexMutex);
+      ServiceNameToIndexMap::iterator it;
+      it = _serviceNameToIndex.find(name);
+      if (it != _serviceNameToIndex.end())
+        return it->second;
+    }
+    return 0;
+  }
+
+  qi::ServiceInfo ObjectRegistrar::registeredService(const std::string &service) {
+    unsigned int serviceId = objectId(service);
+
+    if (!serviceId)
+      return qi::ServiceInfo();
+
+    {
+      boost::mutex::scoped_lock sl(_servicesMutex);
+      BoundServiceMap::iterator it = _services.find(serviceId);
+      if (it != _services.end())
+        return it->second.serviceInfo;
+    }
+    return qi::ServiceInfo();
+  }
+
+  qi::ObjectPtr ObjectRegistrar::registeredServiceObject(const std::string &service) {
+    unsigned int serviceId = objectId(service);
+    if (!serviceId)
+      return qi::ObjectPtr();
+
+    {
+      boost::mutex::scoped_lock sl(_servicesMutex);
+      BoundServiceMap::iterator it = _services.find(serviceId);
+      if (it != _services.end())
+        return it->second.object;
+    }
+    return ObjectPtr();
+  }
+
+}
---
+++ libqimessaging/src/objectregistrar.hpp
@@ -0,0 +1,100 @@
+#pragma once
+/*
+**  Copyright (C) 2012 Aldebaran Robotics
+**  See COPYING for the license
+*/
+
+#ifndef _SRC_OBJECT_HPP_
+#define _SRC_OBJECT_HPP_
+
+#include <string>
+#include <set>
+#include <boost/thread/recursive_mutex.hpp>
+#include <qimessaging/api.hpp>
+#include <qimessaging/session.hpp>
+#include <qimessaging/transportserver.hpp>
+#include <qi/atomic.hpp>
+#include "server.hpp"
+
+namespace qi {
+
+  class GenericObject;
+  class ServiceDirectoryClient;
+
+  //hold all information about a service
+  struct BoundService {
+    std::string   name;
+    qi::ObjectPtr object;
+    unsigned int  id;
+    ServiceInfo   serviceInfo;
+  };
+
+
+  /** the Server 0 object
+   * handle metaObject request, event connection/disconnection
+   */
+  class ObjectRegistrar : private Server {
+
+  public:
+    ObjectRegistrar(ServiceDirectoryClient *sdClient);
+    virtual ~ObjectRegistrar();
+
+    //register/unregister services
+    qi::Future<unsigned int>     registerService(const std::string &name, qi::ObjectPtr obj);
+    qi::Future<void>             unregisterService(unsigned int idx);
+
+    //list services
+    std::vector<qi::ServiceInfo>  registeredServices();
+    qi::ServiceInfo               registeredService(const std::string &service);
+    qi::ObjectPtr                 registeredServiceObject(const std::string &service);
+
+
+    using Server::close;
+    using Server::listen;
+    using Server::listenUrl;
+
+  private:
+    //throw on error
+    qi::ObjectPtr  object(unsigned int serviceId);
+    //0 on error
+    unsigned int   objectId(const std::string &name);
+
+  private:
+    //Future
+    void onFutureFinished(qi::Future<unsigned int> future, long id, qi::Promise<unsigned int> result);
+
+  private:
+    typedef std::map<unsigned int, BoundService>                       BoundServiceMap;
+    typedef std::map<long, std::pair<qi::ObjectPtr, qi::ServiceInfo> > RegisterServiceMap;
+    typedef std::map<std::string, unsigned int>                        ServiceNameToIndexMap;
+
+  public:
+    BoundServiceMap                     _services;
+    boost::mutex                        _servicesMutex;
+
+    ServiceNameToIndexMap               _serviceNameToIndex;
+    boost::mutex                        _serviceNameToIndexMutex;
+
+
+    //used by registerService
+    RegisterServiceMap                  _registerServiceRequest;
+    qi::atomic<long>                    _registerServiceRequestIndex;
+    boost::mutex                        _registerServiceRequestMutex;
+
+    //no lock needed
+//    std::set<TransportSocketPtr>        _clients;
+//    boost::recursive_mutex              _clientsMutex;
+
+    bool                                _dying;
+    ServiceDirectoryClient             *_sdClient;
+
+    Server                             *_server;
+  };
+
+}
+
+QI_TYPE_NOT_CONSTRUCTIBLE(qi::ObjectRegistrar);
+QI_TYPE_NOT_CLONABLE(qi::ObjectRegistrar);
+
+
+#endif  // _SRC_SESSIONSERVER_HPP_
