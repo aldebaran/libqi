738b4cf00f852cb37887b13525e340cb1e6433a3
Cedric GESTES <gestes@aldebaran-robotics.com>
2012-10-08 14:52:43 +0200

    add qi::Server
    Change-Id: I254f62c86b7b3f3022afec44cfd9012c3f1e7acf

---
+++ libqimessaging/src/server.cpp
@@ -0,0 +1,181 @@
+/*
+**  Copyright (C) 2012 Aldebaran Robotics
+**  See COPYING for the license
+*/
+#include <set>
+#include <qimessaging/genericobject.hpp>
+#include <qimessaging/transportserver.hpp>
+#include <qimessaging/serviceinfo.hpp>
+#include <qimessaging/objecttypebuilder.hpp>
+#include "objectregistrar.hpp"
+#include "serverresult.hpp"
+#include "transportserver_p.hpp"
+#include <qi/os.hpp>
+#include <boost/thread/mutex.hpp>
+#include "servicedirectoryclient.hpp"
+#include "signal_p.hpp"
+
+namespace qi {
+
+  //Server
+  Server::Server()
+    : _server()
+    , _dying(false)
+  {
+    _server.newConnection.connect(boost::bind<void>(&Server::onTransportServerNewConnection, this, _1));
+  }
+
+  Server::~Server()
+  {
+    //we can call reset on server and socket they are only owned by us.
+    //when it's close it's close
+    _server.newConnection._p->reset();
+    boost::recursive_mutex::scoped_lock sl(_socketsMutex);
+    _dying = true;
+    for (std::set<TransportSocketPtr>::iterator i = _sockets.begin();
+      i != _sockets.end(); ++i)
+    {
+      // We do not want onSocketDisconnected called
+      //TODO: move that logic into TransportServer.
+      (*i)->disconnected._p->reset();
+      (*i)->messageReady._p->reset();
+      (*i)->disconnect();
+    }
+  }
+
+  bool Server::addObject(unsigned int id, qi::ObjectPtr obj)
+  {
+    if (!obj)
+      return false;
+    BoundObjectPtr bop = makeServiceBoundObjectPtr(id, obj);
+    return addObject(id, bop);
+  }
+
+  bool Server::addObject(unsigned int id, qi::BoundObjectPtr obj)
+  {
+    if (!obj)
+      return false;
+    //register into _boundObjects
+    {
+      boost::mutex::scoped_lock sl(_boundObjectsMutex);
+      BoundObjectPtrMap::iterator it;
+      it = _boundObjects.find(id);
+      if (it != _boundObjects.end()) {
+        return false;
+      }
+      _boundObjects[id] = obj;
+      return true;
+    }
+  }
+
+  bool Server::removeObject(unsigned int idx)
+  {
+    {
+      boost::mutex::scoped_lock sl(_boundObjectsMutex);
+      BoundObjectPtrMap::iterator it;
+      it = _boundObjects.find(idx);
+      if (it != _boundObjects.end()) {
+        return false;
+      }
+      _boundObjects.erase(idx);
+    }
+    return true;
+  }
+
+  void Server::onTransportServerNewConnection(TransportSocketPtr socket)
+  {
+    boost::recursive_mutex::scoped_lock sl(_socketsMutex);
+    if (!socket)
+      return;
+    _sockets.insert(socket);
+    socket->disconnected.connect(boost::bind<void>(&Server::onSocketDisconnected, this, socket, _1));
+    socket->messageReady.connect(boost::bind<void>(&Server::onMessageReady, this, _1, socket));
+  }
+
+
+  qi::Url Server::listenUrl() const {
+    return _server.listenUrl();
+  }
+
+  void Server::onMessageReady(const qi::Message &msg, TransportSocketPtr socket) {
+    qi::BoundObjectPtr obj;
+    {
+      boost::mutex::scoped_lock sl(_boundObjectsMutex);
+      BoundObjectPtrMap::iterator it;
+
+      it = _boundObjects.find(msg.service());
+      if (it == _boundObjects.end())
+      {
+        if (msg.type() == qi::Message::Type_Call) {
+          qi::Message       retval(Message::Type_Error, msg.address());
+          qi::Buffer        error;
+          qi::ODataStream   ds(error);
+          std::stringstream ss;
+          ss << "can't find service id: " << msg.id();
+          ds << qi::signatureFromType<std::string>::value();
+          ds << ss.str();
+          retval.setBuffer(error);
+          socket->send(retval);
+        }
+        qiLogError("qi::Server") << "Can't find service: " << msg.service();
+        return;
+      }
+      obj            = it->second;
+    }
+    obj->onMessage(msg, socket);
+  }
+
+  void Server::close()
+  {
+    {
+      boost::recursive_mutex::scoped_lock sl(_socketsMutex);
+      std::set<TransportSocketPtr>::iterator it;
+      //TODO move that logic into TransportServer
+      for (it = _sockets.begin(); it != _sockets.end(); ++it) {
+        (*it)->connected._p->reset();
+        (*it)->disconnected._p->reset();
+        (*it)->messageReady._p->reset();
+      }
+    }
+    _server.close();
+  }
+
+  bool Server::listen(const qi::Url &address)
+  {
+    qi::Url url(address);
+
+    if (url.protocol() != "tcp") {
+      qiLogError("qi::Server") << "Protocol " << url.protocol() << " not supported.";
+      return false;
+    }
+    if (!_server.listen(url))
+      return false;
+
+    std::vector<qi::Url>                 epsUrl = _server.endpoints();
+    std::vector<qi::Url>::const_iterator epsUrlIt;
+    _endpoints.clear();
+    for (epsUrlIt = epsUrl.begin(); epsUrlIt != epsUrl.end(); epsUrlIt++) {
+      _endpoints.push_back((*epsUrlIt).str());
+    }
+    qiLogVerbose("qimessaging.Server") << "Started Server at " << _server.listenUrl().str();
+    return true;
+  }
+
+  void Server::onSocketDisconnected(TransportSocketPtr socket, int error)
+  {
+    BoundObjectPtrMap::iterator it;
+    {
+      boost::mutex::scoped_lock sl(_boundObjectsMutex);
+      for (it = _boundObjects.begin(); it != _boundObjects.end(); ++it) {
+        BoundObjectPtr o = it->second;
+        o->onSocketDisconnected(socket, error);
+      }
+    }
+  }
+
+  std::vector<std::string> Server::endpoints() const {
+    return _endpoints;
+  }
+
+
+}
---
+++ libqimessaging/src/server.hpp
@@ -0,0 +1,78 @@
+#pragma once
+/*
+**  Copyright (C) 2012 Aldebaran Robotics
+**  See COPYING for the license
+*/
+
+#ifndef _SRC_SERVER_HPP_
+#define _SRC_SERVER_HPP_
+
+#include <string>
+#include <set>
+#include <boost/thread/recursive_mutex.hpp>
+#include <qimessaging/api.hpp>
+#include <qimessaging/session.hpp>
+#include <qimessaging/transportserver.hpp>
+#include <qi/atomic.hpp>
+#include "boundobject.hpp"
+
+namespace qi {
+
+  /**
+   * Do all the plumbing between sockets and objects.
+   *
+   * Support a special kind of objects: (SocketObject, that are aware of Socket)
+   *
+   * Threadsafety warning: do not call listen and addSocketObject at the same time
+   *
+   */
+  class Server {
+  public:
+    QI_DISALLOW_COPY_AND_ASSIGN(Server);
+    Server();
+    ~Server();
+
+    //make the server listening
+    bool listen(const qi::Url &address);
+    void close();
+    qi::Url listenUrl() const;
+
+    //Create a BoundObject
+    bool addObject(unsigned int idx, qi::ObjectPtr obj);
+    bool addObject(unsigned int idx, qi::BoundObjectPtr obj);
+    bool removeObject(unsigned int idx);
+
+    std::vector<std::string> endpoints() const;
+
+  private:
+    void setSocketObjectEndpoints();
+
+  private:
+    //TransportServer
+    void onTransportServerNewConnection(TransportSocketPtr socket);
+
+    //TransportSocket
+    void onSocketDisconnected(TransportSocketPtr socket, int error);
+    void onMessageReady(const qi::Message &msg, TransportSocketPtr socket);
+
+  private:
+    //bool: true if it's a socketobject
+    typedef std::map<unsigned int, BoundObjectPtr> BoundObjectPtrMap;
+
+    //ObjectList
+    BoundObjectPtrMap                   _boundObjects;
+    boost::mutex                        _boundObjectsMutex;
+
+    //SocketList
+    std::set<TransportSocketPtr>        _sockets;
+    boost::recursive_mutex              _socketsMutex;
+
+    std::vector<std::string>            _endpoints;
+  public:
+    TransportServer                     _server;
+    bool _dying;
+  };
+}
+
+
+#endif  // _SRC_SESSIONSERVER_HPP_
