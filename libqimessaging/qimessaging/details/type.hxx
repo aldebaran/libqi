734f8edb7f379960f487d6791bcd7d72251d9ae3
Matthieu Nottale <mnottale@aldebaran-robotics.com>
2012-10-10 15:09:14 +0200

    Fix suprious warning under win32.
    Change-Id: Ie66337dfb199e734103cc8adf4e2554217df8cc6

--- libqimessaging/qimessaging/details/type.hxx
+++ libqimessaging/qimessaging/details/type.hxx
@@ -71,7 +71,7 @@ namespace qi  {
           defaultResult = new TypeImpl<T>();
         result = defaultResult;
       }
-      if (typeid(T).name() != result->infoString() && typeid(T)!= typeid(bool))
+      if (strcmp(typeid(T).name(), result->infoString()) && typeid(T)!= typeid(bool))
         qiLogWarning("qi.meta") << "typeOfBackend: type mismatch " << typeid(T).name() << " "
        << result <<" " << result->infoString();
       return result;
