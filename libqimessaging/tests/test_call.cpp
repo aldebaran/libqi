82fa7941c063543812324d67f2ec2a62e305235b
Pierre Roullon <proullon@aldebaran-robotics.com>
2012-10-17 16:00:47 +0200

    Add test_call
    Change-Id: I4e88ad0efecf5c069b37a999f50df96e1ede6dee
    Reviewed-on: http://gerrit.aldebaran.lan:8080/6842
    Reviewed-by: cgestes <gestes@aldebaran-robotics.com>
    Tested-by: proullon <proullon@aldebaran-robotics.com>

---
+++ libqimessaging/tests/test_call.cpp
@@ -0,0 +1,52 @@
+/*
+ *  Author(s):
+ *  - Pierre Roullon <proullon@aldebaran-robotics.com>
+ *
+ *  Copyright (c) 2012 Aldebaran Robotics. All rights reserved.
+ */
+
+#include <list>
+#include <iostream>
+
+#include <gtest/gtest.h>
+
+#include <qi/qi.hpp>
+#include <qi/application.hpp>
+#include <qitype/genericobject.hpp>
+#include <qitype/genericobjectbuilder.hpp>
+#include <qimessaging/session.hpp>
+#include <testsession/testsessionpair.hpp>
+#include <qimessaging/servicedirectory.hpp>
+#include <testsession/testsessionpair.hpp>
+
+int print(std::list<std::pair<std::string, int> > robots)
+{
+  for(std::list<std::pair<std::string, int> >::iterator it = robots.begin(); it != robots.end(); ++it)
+    std::cout << "Robot " << (*it).first << " has serial ID " << (*it).second << std::endl;
+
+  return robots.size();
+}
+
+TEST(TestCall, CallComplexType)
+{
+  std::list<std::pair<std::string, int> >  robots;
+  TestSessionPair          p;
+  qi::GenericObjectBuilder ob;
+  int serviceID;
+
+  ob.advertiseMethod("print", &print);
+  qi::ObjectPtr obj(ob.object());
+
+  serviceID = p.server()->registerService("serviceCall", obj);
+
+  qi::ObjectPtr proxy = p.client()->service("serviceCall");
+
+  robots.push_back(std::make_pair("Gibouna", 1234567));
+  robots.push_back(std::make_pair("Wall-E", 2345678));
+  robots.push_back(std::make_pair("Billy West", 3456789));
+  robots.push_back(std::make_pair("33CL", 4567890));
+
+  ASSERT_ANY_THROW(proxy->call<int>("print", robots));
+
+  p.server()->unregisterService(serviceID);
+}
