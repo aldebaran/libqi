23c6f9538e658d7fd73c38f587439a4bc8845c8b
Matthieu Nottale <mnottale@aldebaran-robotics.com>
2012-10-25 11:37:34 +0200

    test_call: Use qi::Application.
    Change-Id: Ifc2216af381d20063a60dbae8689729a10e25055

--- libqimessaging/tests/test_call.cpp
+++ libqimessaging/tests/test_call.cpp
@@ -102,3 +102,9 @@ TEST(TestCall, CallVoidErr)
   ASSERT_TRUE(fut.hasError());
   p.server()->unregisterService(serviceID);
 }
+
+int main(int argc, char **argv) {
+  qi::Application app(argc, argv);
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
