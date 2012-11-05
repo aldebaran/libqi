dd81e62a6e7ba92ddad074979bf7cb4fcfbd2c71
Cedric GESTES <gestes@aldebaran-robotics.com>
2012-11-05 11:20:13 +0100

    test_service: test ABA problems, test server close
    Change-Id: I35bad40f16b721a2a96ea6f4a7631061c295b451
    Reviewed-on: http://gerrit.aldebaran.lan:8080/7135
    Reviewed-by: cgestes <gestes@aldebaran-robotics.com>
    Tested-by: cgestes <gestes@aldebaran-robotics.com>

---
+++ libqimessaging/tests/test_service.cpp
@@ -0,0 +1,189 @@
+/*
+ ** Author(s):
+ **  - Herve Cuche <hcuche@aldebaran-robotics.com>
+ **
+ ** Copyright (C) 2010, 2012 Aldebaran Robotics
+ */
+
+#include <vector>
+#include <iostream>
+#include <string>
+
+#include <gtest/gtest.h>
+
+#include <qimessaging/session.hpp>
+#include <qitype/genericobject.hpp>
+#include <qitype/genericobjectbuilder.hpp>
+#include <qimessaging/servicedirectory.hpp>
+#include <qimessaging/gateway.hpp>
+#include <qi/os.hpp>
+#include <qi/application.hpp>
+#include <testsession/testsessionpair.hpp>
+
+static std::string reply(const std::string &msg)
+{
+  return msg;
+}
+
+//check for server closed
+//check for socket disconnected
+//check for service unregistered
+//check for service unregistered, then readded
+
+TEST(QiService, RemoteObjectCacheServerClose)
+{
+  TestSessionPair p;
+
+  qi::GenericObjectBuilder ob;
+  ob.advertiseMethod("reply", &reply);
+  qi::ObjectPtr obj(ob.object());
+
+  p.server()->registerService("serviceTest", obj);
+  qi::Future<qi::ObjectPtr> fut;
+  fut = p.client()->service("serviceTest");
+  EXPECT_FALSE(fut.hasError());
+
+  EXPECT_EQ(std::string("titi"), fut.value()->call<std::string>("reply", "titi").value());
+
+  p.server()->close();
+
+  fut = p.client()->service("serviceTest");
+  EXPECT_FALSE(fut.hasError());
+}
+
+
+TEST(QiService, RemoteObjectCacheUnregister)
+{
+  TestSessionPair p;
+
+  qi::GenericObjectBuilder ob;
+  ob.advertiseMethod("reply", &reply);
+  qi::ObjectPtr obj(ob.object());
+
+  unsigned int idx = p.server()->registerService("serviceTest", obj);
+  qi::Future<qi::ObjectPtr> fut;
+  fut = p.client()->service("serviceTest");
+  EXPECT_FALSE(fut.hasError());
+
+  EXPECT_EQ(std::string("titi"), fut.value()->call<std::string>("reply", "titi").value());
+
+  p.server()->unregisterService(idx);
+
+  fut = p.client()->service("serviceTest");
+  EXPECT_FALSE(fut.hasError());
+}
+
+
+TEST(QiService, RemoteObjectCacheABAUnregister)
+{
+  TestSessionPair p;
+
+  qi::GenericObjectBuilder ob;
+  ob.advertiseMethod("reply", &reply);
+  qi::ObjectPtr obj(ob.object());
+
+  unsigned int idx = p.server()->registerService("serviceTest", obj);
+  qi::Future<qi::ObjectPtr> fut;
+  fut = p.client()->service("serviceTest");
+  EXPECT_FALSE(fut.hasError());
+
+  EXPECT_EQ(std::string("titi"), fut.value()->call<std::string>("reply", "titi").value());
+
+  p.server()->unregisterService(idx);
+
+  fut = p.client()->service("serviceTest");
+  EXPECT_FALSE(fut.hasError());
+
+  unsigned int idx2 = p.server()->registerService("serviceTest", obj);
+  //new service should not have a previoulsy registered ID
+  EXPECT_NE(idx2, idx);
+
+  fut = p.client()->service("serviceTest");
+  EXPECT_FALSE(fut.hasError());
+
+  qi::Future<std::string> fret = fut.value()->call<std::string>("reply", "titi");
+  if (fret.hasError()) {
+    std::cout << "Error returned:" << fret.error();
+  }
+  EXPECT_FALSE(fret.hasError());
+  EXPECT_EQ(std::string("titi"), fret.value());
+}
+
+
+TEST(QiService, RemoteObjectCacheABANewServer)
+{
+  TestSessionPair p;
+  qi::Session     ses;
+
+  qi::GenericObjectBuilder ob;
+  ob.advertiseMethod("reply", &reply);
+  qi::ObjectPtr obj(ob.object());
+
+  unsigned int idx = p.server()->registerService("serviceTest", obj);
+  qi::Future<qi::ObjectPtr> fut;
+  fut = p.client()->service("serviceTest");
+  EXPECT_FALSE(fut.hasError());
+
+  EXPECT_EQ(std::string("titi"), fut.value()->call<std::string>("reply", "titi").value());
+
+  p.server()->close();
+
+  fut = p.client()->service("serviceTest");
+  EXPECT_FALSE(fut.hasError());
+
+  EXPECT_TRUE(ses.connect(p.client()->url().str()));
+  ses.listen("tcp://0.0.0.0:0");
+  unsigned int idx2 = ses.registerService("serviceTest", obj);
+  //new service should not have a previoulsy registered ID
+  EXPECT_NE(idx2, idx);
+
+  fut = p.client()->service("serviceTest");
+  EXPECT_FALSE(fut.hasError());
+
+  qi::Future<std::string> fret = fut.value()->call<std::string>("reply", "titi");
+  if (fret.hasError()) {
+    std::cout << "Error returned:" << fret.error();
+  }
+  EXPECT_FALSE(fret.hasError());
+  EXPECT_EQ(std::string("titi"), fret.value());
+}
+
+
+TEST(QiService, RemoteObjectNackTransactionWhenServerClosed)
+{
+  TestSessionPair p;
+
+  qi::GenericObjectBuilder ob;
+  ob.advertiseMethod("msleep", &qi::os::msleep);
+  qi::ObjectPtr obj(ob.object());
+
+  p.server()->registerService("serviceTest", obj);
+  qi::Future<qi::ObjectPtr> fut;
+  fut = p.client()->service("serviceTest");
+  EXPECT_FALSE(fut.hasError());
+
+  qi::Future<void> fret = fut.value()->call<void>("msleep", 2000);
+  qi::Future<void> fclose = p.server()->close();
+  fclose.wait(1000);
+  EXPECT_TRUE(fclose.isReady());
+  EXPECT_FALSE(fclose.hasError(1));
+
+  fret.wait(1000);
+  //once the server is close, the answer should be ready.
+  EXPECT_TRUE(fret.isReady());
+  //the service is closed, so it cant send an answer.
+  EXPECT_TRUE(fret.hasError(1000));
+}
+
+
+
+int main(int argc, char **argv)
+{
+  qi::Application app(argc, argv);
+#if defined(__APPLE__) || defined(__linux__)
+  setsid();
+#endif
+  ::testing::InitGoogleTest(&argc, argv);
+  TestMode::initTestMode(argc, argv);
+  return RUN_ALL_TESTS();
+}
