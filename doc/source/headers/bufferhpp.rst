c7daa975fa8d657410ea30d6371720347ebcf7aa
Nicolas Cornu <ncornu@aldebaran-robotics.com>
2012-11-10 14:56:17 +0100

    Fix doc
    Change-Id: I5334202b104906ccb586be4c63e8f31b4a38cc8f
    Reviewed-on: http://gerrit.aldebaran.lan:8080/7916
    Reviewed-by: cgestes <gestes@aldebaran-robotics.com>
    Tested-by: ncornu <ncornu@aldebaran-robotics.com>

---
+++ doc/source/headers/bufferhpp.rst
@@ -0,0 +1,1 @@
+.. cpp:autoheader:: buffer.hpp
--- qi/threadpool.hpp
+++ qi/threadpool.hpp
@@ -17,27 +17,42 @@ namespace qi
 {
   class ThreadPoolPrivate;
 
+  /// Pool of workers
   class QI_API ThreadPool
   {
     public:
+      /// ThreadPool constructor.
       ThreadPool(unsigned int minWorkers = 2, unsigned int maxWorkers = 8,
                  unsigned int minIdleWorkers = 1, unsigned int maxIdleWorkers = 4);
+      /// ThreadPool destructor.
       ~ThreadPool();
 
+      /// Returns the number of workers in the pool.
       unsigned int size() const;
+      /// Returns the number of active workers in the pool.
       unsigned int active() const;
 
+      /// Resizes the pool.
       void setMaxWorkers(unsigned int n);
+      /// Sets minimum number of workers in the pool.
       unsigned int getMaxWorkers() const;
+      /// Sets max idle workers.
       void setMinWorkers(unsigned int n);
+      /// Sets min idle workers.
       unsigned int getMinWorkers() const;
+      /// Returns maximum number of workers in the pool.
       void setMinIdleWorkers(unsigned int n);
+      /// Returns minimum number of workers in the pool.
       unsigned int getMinIdleWorkers() const;
+      /// Returns maximum number of inactive workers in the pool.
       void setMaxIdleWorkers(unsigned int n);
+      /// Returns minimum number of inactive workers in the pool.
       unsigned int getMaxIdleWorkers() const;
 
+      /// Sleeps until all tasks are completed
       void waitForAll();
 
+      /// Adds a task to the pool.
       bool schedule(const boost::function<void(void)>& f);
 
     private:
