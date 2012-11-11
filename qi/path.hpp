e0de6255956f3a4b0549147e1d4f486b29b3646e
Nicolas Cornu <ncornu@aldebaran-robotics.com>
2012-11-11 10:44:28 +0100

    Add documentation to path
    Change-Id: Id5329400f8927e38ac6b9883881ae490e5e8a929
    Reviewed-on: http://gerrit.aldebaran.lan:8080/7915
    Reviewed-by: cgestes <gestes@aldebaran-robotics.com>
    Reviewed-by: ncornu <ncornu@aldebaran-robotics.com>
    Tested-by: ncornu <ncornu@aldebaran-robotics.com>

--- qi/path.hpp
+++ qi/path.hpp
@@ -16,24 +16,13 @@
 # include <vector>
 # include <qi/api.hpp>
 
-/**
- *  @namespace qi::path
- * \ingroup qipath
- *  @brief Set of tools to handle SDK layouts.
- *
- * \warning Every string MUST be encoded in UTF8. Every returned string are encoded in UTF-8.
- *
- */
 namespace qi
 {
+  /// Set of tools to handle SDK layouts.
   namespace path
   {
 
-    /**
-     * \brief Return the default SDK prefix path.
-     * \ingroup qipath
-     * It's always a complete, native path.
-     */
+    /// Return the default SDK prefix path.
     QI_API std::string sdkPrefix();
 
     // not thread-safe, must be kept internal
@@ -66,171 +55,39 @@ namespace qi
 
     }
 
-
-    /** \brief Look for a binary.
-     * \ingroup qipath
-     * This will search in all SDK prefixes for a file named 'name'.
-     * It will then add '.exe' suffix if needed.
-     * \param name The full name of the binary, or just the name
-     * (without '.exe') (in UTF-8).
-     * \return The complete, native path to the file found,
-     * an empty string otherwise.
-     */
+    /// Look for a binary.
     QI_API std::string findBin(const std::string& name);
 
-    /** \brief Look for a library.
-     * \ingroup qipath
-     *
-     * This will search in all SDK prefixes for a file named 'name'.
-     * It will then add 'lib' prefix, and appropriated suffixes
-     * ('.dll' on windows, '.so' on linux, '.dylib' on mac).
-     * \param name The full name of the library, or just the name
-     * (without '.dll', '.so') (in UTF-8).
-     * \return The complete, native path to the file found,
-     * an empty string otherwise.
-     *
-     * You can specify subdirectories using "/" as directory separator
-     * (in UTF-8).
-     */
+    /// Look for a library.
     QI_API std::string findLib(const std::string& name);
 
-    /**
-     * \brief Look for a configuration file.
-     * \ingroup qipath
-     *
-     * \verbatim
-     * The file is searched in a list of possible directories,
-     * the first match is returned.
-     *
-     * The list of paths is constructed like this:
-     *
-     * - first, a standard path in the home directory (like
-     *   ~/.config/<applicationName>/<filename>)
-     * - then: <sdk_prefix>/etc/<applicationName>/<filename> for each known SDK
-     *   prefix.
-     * - then a standard path in the system. (like
-     *   /etc/<applicationName>/<filename>)
-     * \endverbatim
-     * @param applicationName Name of the application (in UTF-8).
-     * @param filename Name of the file to look for (in UTF-8).
-     * You can specify subdirectories using "/" as directory separator.
-     * @return The complete, native path of the file if it was found,
-     * an empty string otherwise.
-     */
+    /// Look for a configuration file.
     QI_API std::string findConf(const std::string& applicationName,
                                 const std::string& filename);
 
-    /**
-     * \brief Look for a data file.
-     * \ingroup qipath
-     *
-     * \verbatim
-     * The file is search in a list of possible directories,
-     * the first match is returned.
-     * The list of paths is constructed like this:
-     *
-     * - first, a standard path in the home directory (like
-     *   ~/.local/share/<applicationName>/<filename>)
-     * - then <sdk_prefix>/share/<applicationName>/<filename> for each known SDK
-     *   prefix.
-     * \endverbatim
-     *
-     * @param applicationName Name of the application (in UTF-8).
-     * @param filename Name of the file to look for (in UTF-8).
-     * You can specify subdirectories using "/" as directory separator.
-     * @return The complete, native path of the file if it was found,
-     * an empty string otherwise.
-     */
+    /// Look for a data file.
     QI_API std::string findData(const std::string& applicationName,
                                 const std::string& filename);
 
 
-    /**
-     * \brief Get the list of directories used when searching for
-     *  configuration files for the given application name.
-     * \ingroup qipath
-     *
-     * This is used by the qi::path::findConfigurationPath method.
-     * @param applicationName Name of the application (in UTF-8).
-     * @return List of configuration directories.
-     *
-     * Warning: you should not assume those directories exist, nor that they are
-     * writeable.
-     */
+    /// Get the list of directories used when searching for configuration files for the given application name.
     QI_API std::vector<std::string> confPaths(const std::string& applicationName="");
 
-    /**
-     * \brief Get the list of directories used when searching for data files
-     * for the given application name.
-     * \ingroup qipath
-     *
-     * This is used by the qi::path::findDataPath method.
-     * @param applicationName Name of the application (in UTF-8).
-     * @return A list of directories.
-     *
-     * Warning: you should not assume those directories exist, nor that they are
-     * writeable.
-     */
+    /// Get the list of directories used when searching for data files for the given application name.
     QI_API std::vector<std::string> dataPaths(const std::string& applicationName="");
 
-    /**
-     * \brief Get the list of directories used when searching for
-     *  binaries.
-     * \ingroup qipath
-     *
-     * This is used by the qi::path::findBin() method.
-     * @return A list of directories.
-     *
-     * Warning: you should not assume those directories exist, nor that they are
-     * writeable.
-     */
+    /// Get the list of directories used when searching for binaries.
     QI_API std::vector<std::string> binPaths();
 
-    /**
-     * \brief Get the list of directories used when searching for
-     *  libraries.
-     * \ingroup qipath
-     *
-     * This is used by the qi::path::findLib() method.
-     * @return A list of directories.
-     *
-     * Warning: you should not assume those directories exist, nor that they are
-     * writeable.
-     */
+    /// Get the list of directories used when searching for libraries.
     QI_API std::vector<std::string> libPaths();
 
 
-    /**
-     * \brief Get the writable data files path for users.
-     * \ingroup qipath
-     *
-     * @param applicationName The name of the application (in UTF-8).
-     * @param filename If filename is empty, return the directory
-     * in which to write. Otherwise the path is constructed like this:
-     * \verbatim
-     * <home>/.local/share/<applicatioName>/<filename> (on linux)
-     * %AppData%\<applicatioName>\<filename> (windows)
-     * You can specify subdirectories using "/" as directory separator
-     * (in UTF-8).
-     * \endverbatim
-     */
+    /// Get the writable data files path for users.
     QI_API std::string userWritableDataPath(const std::string& applicationName,
                                             const std::string& filename);
 
-    /**
-     * \brief Get the writable configuration files path for users.
-     * \ingroup qipath
-     *
-     * @param applicationName The name of the application.
-     * @param filename If filename is empty, return the directory
-     * in which to write. Otherwise the path is constructed like this:
-     * \verbatim
-     * <home>/.config/<applicatioName>/<filename> (on linux)
-     * %AppData%\<applicatioName>\<filename> (windows)
-     * You can specify subdirectories using "/" as directory separator
-     * (in UTF-8).
-     * \endverbatim
-     */
+    /// Get the writable configuration files path for users.
     QI_API std::string userWritableConfPath(const std::string& applicationName,
                                             const std::string& filename="");
 
