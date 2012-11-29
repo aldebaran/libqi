eb7dbc79361ff665f69c01f465ede4fe88581308
Matthieu Nottale <mnottale@aldebaran-robotics.com>
2012-11-29 17:03:50 +0100

    codegen: Proxy generation.
    Change-Id: Iee5aec0cb3eb4665e92cbda732b15424f993b8a9

--- codegen/src/cxx2idl.py
+++ codegen/src/cxx2idl.py
@@ -17,8 +17,21 @@ TYPE_MAP = {
   'unsigned char': 'uchar',
   'long': 'int64',
   'char*': 'string',
-  'GenericValue': 'dynamic',
+  'qi::GenericValue': 'dynamic',
 }
+
+REV_MAP = {
+    'uint' : 'unsigned int',
+    'uint64' : 'unsigned long',
+    'dynamic': 'qi::GenericValue'
+}
+
+def idltype_to_cxxtype(t):
+  t = t.replace('{', 'std::map<').replace('}', ' >')
+  t = t.replace('[', 'std::vector<').replace(']', ' >')
+  for e in REV_MAP:
+    t = t.replace(e, REV_MAP[e])
+  return t
 
 def cxx_type_parse(t):
   pre = ''
@@ -72,7 +85,6 @@ def cxx_type_to_signature(t):
   #Huge hack, we do not realy parse 'a,b' in template
   parsed = cxx_type_parse(t)
   sig = cxx_parsed_to_sig(parsed)
-  print(parsed)
   return sig
 
 def run_doxygen(files):
@@ -123,8 +135,6 @@ def raw_to_idl(dstruct):
   print(dstruct)
   for cls in dstruct:
     e = etree.SubElement(root, 'class', name=cls)
-    print("--\n")
-    print(dstruct[cls])
     for method_name in dstruct[cls][0]:
       m = etree.SubElement(e, 'method', name=method_name)
       method_raw = dstruct[cls][0][method_name]
@@ -142,10 +152,76 @@ def raw_to_text(dstruct):
       result += "    " + method_raw[0] + " " + method_name +"(" + ",".join(method_raw[1]) + ")\n"
   return result
 
+def idl_to_raw(root):
+  result = dict()
+  for cls in root.findall("class"):
+    methods = dict()
+    for m in cls.findall("method"):
+      r = m.find("return").get("type")
+      args = [a.get("type") for a in m.find("argument")]
+      methods[m.get(name)] = (r, args)
+    result[cls.get("name")] =  (methods,)
+  return result
+
+def raw_to_proxy(class_name, data, return_future):
+  skeleton = """
+#ifndef @GARD@
+#define @GARD@
+
+
+#include <vector>
+#include <string>
+#include <map>
+
+#include <qitype/genericobject.hpp>
+class @className@Proxy
+{
+public:
+  @className@Proxy(qi::ObjectPtr obj)
+  : _obj(obj)
+  {}
+  qi::ObjectPtr asObject() { return _obj;}
+@methods@
+  private:
+    qi::ObjectPtr _obj;
+};
+
+typedef boost::shared_ptr<@className@Proxy> @className@ProxyPtr;
+#endif //@GARD@
+"""
+  #generate methods
+  methods = data[0]
+  methodImpls = ""
+  for method_name in methods:
+    method = methods[method_name]
+    iret = method[0]
+    cret = idltype_to_cxxtype(iret)
+    if (return_future):
+      cret = 'qi::FutureSync<' + cret + ' >'
+    iargs = method[1]
+    cargs = map(idltype_to_cxxtype, iargs)
+    argNames = 'p' + ", p".join(map(str, range(len(cargs))))
+    typedArgs = map(lambda x: cargs[x] + ' ' + argNames[x], range(len(cargs)))
+    typedArgs = ''.join(typedArgs)
+    #NOTE: should we return the future?
+    methodImpls += '  ' + cret + " " + method_name + "(" + typedArgs + ") {\n    "
+    if (cret != "void" or return_future):
+      methodImpls += "return "
+    methodImpls += '_obj->call<' + cret + ' >' + '("' + method_name + '", ' + argNames + ");\n  }\n"
+  result = skeleton
+  replace = {
+      'GARD': '_' + class_name.upper() + '_PROXY_HPP_',
+      'className': class_name,
+      'methods': methodImpls
+  }
+  for k in replace:
+    result = result.replace('@' + k + '@', replace[k])
+  return result
+
 def main(args):
   parser = argparse.ArgumentParser()
   parser.add_argument("--output-file","-o", help="output file (stdout)")
-  parser.add_argument("--output-mode","-m", default="idl", choices=["txt", "idl"], help="output mode (stdout)")
+  parser.add_argument("--output-mode","-m", default="idl", choices=["txt", "idl", "proxy", "proxyFuture"], help="output mode (stdout)")
   parser.add_argument("input", nargs='+', help="input file(s)")
   pargs = parser.parse_args(args)
   doxy_dir = run_doxygen(pargs.input)
@@ -158,6 +234,10 @@ def main(args):
     res = raw_to_text(raw)
   elif pargs.output_mode == "idl":
     res = etree.tostring(raw_to_idl(raw))
+  elif pargs.output_mode == "proxy":
+    res = raw_to_proxy(raw.keys()[0], raw[raw.keys()[0]], False)
+  elif pargs.output_mode == "proxyFuture":
+    res = raw_to_proxy(raw.keys()[0], raw[raw.keys()[0]], True)
   out.write(res)
 
 main(sys.argv)
