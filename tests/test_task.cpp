41fe663db10f7d13641476bc7b2384cdf3767514
Matthieu Nottale <mnottale@aldebaran-robotics.com>
2012-12-19 10:47:39 +0100

    test_task: Test signals.
    Change-Id: I02a03707275b2157df61f4b4bf136db9be4aaaac

--- tests/test_task.cpp
+++ tests/test_task.cpp
@@ -75,10 +75,27 @@ TEST_F(TestTask, Task)
   task->step(42);
   std::string lr = task->getLastResult();
   ASSERT_EQ("coin foo 42 1", lr);
+  // Release the task, ensure it gets destroyed
   task.reset();
   qi::os::msleep(400);
   count = taskGenProxy->taskCount();
   ASSERT_EQ(0U, count);
+}
+
+TEST_F(TestTask, Signals)
+{
+  ITaskPtr task = taskGenProxy->newTask("coin");
+  ASSERT_TRUE(task);
+  qi::Promise<std::string> p;
+  task->onParamChanged.connect(
+    boost::bind(&qi::Promise<std::string>::setValue, p, _1));
+  task->setParam("foo");
+  ASSERT_EQ("foo", p.future().value());
+  std::vector<ITaskPtr> tasks = taskGenProxy->tasks();
+  ASSERT_EQ(1U, tasks.size());
+  p.reset();
+  tasks[0]->setParam("bar");
+  ASSERT_EQ("bar", p.future().value());
 }
 
 int main(int argc, char **argv) {
