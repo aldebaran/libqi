03501b2fa66e3e7b6b456851c63a7ef935ed9799
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2013-01-15 17:08:35 +0100

    fix using qimessaging from a relocatable python SDK
    Change-Id: I2312fc511dfb3c052d07297ef17f293c722862d5
    Reviewed-on: http://gerrit.aldebaran.lan:8080/10201
    Reviewed-by: mnottale <mnottale@aldebaran-robotics.com>
    Reviewed-by: cgestes <gestes@aldebaran-robotics.com>
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
    Tested-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>

--- python/qimessaging/__init__.py
+++ python/qimessaging/__init__.py
@@ -10,3 +10,30 @@
 
 Provided features are very close to C++, Python style.
 """
+
+import os
+import sys
+import ctypes
+
+def load_libqimessaging():
+    """ Load qimessaging.so and its dependencies.
+
+    This makes qimessaging usable from a relocatable
+    SDK without having to set LD_LIBRARY_PATH
+    """
+    deps = [
+            "libqi.so",
+            "libqitype.so",
+            "libqimessaging.so",
+    ]
+    this_dir = os.path.abspath(os.path.dirname(__file__))
+    for dep in deps:
+        full_path = os.path.join(this_dir, "..", dep)
+        ctypes.cdll.LoadLibrary(full_path)
+
+
+if sys.platform.startswith("linux"):
+    try:
+        load_libqimessaging()
+    except Exception, e:
+        print e
