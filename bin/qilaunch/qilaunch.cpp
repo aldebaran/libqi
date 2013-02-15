6cb5d24b142ded1f530ce016c545b16d9cb4a854
Matthieu Nottale <mnottale@aldebaran-robotics.com>
2013-02-15 11:56:16 +0100

    qi-launch: Improve argument parsing.
    Change-Id: I3a6075432ed5a5174b8137a3940ff4c6e916231d
    Reviewed-on: http://gerrit.aldebaran.lan:8080/12741
    Reviewed-by: mnottale <mnottale@aldebaran-robotics.com>
    Tested-by: mnottale <mnottale@aldebaran-robotics.com>

--- bin/qilaunch/qilaunch.cpp
+++ bin/qilaunch/qilaunch.cpp
@@ -8,27 +8,69 @@ qiLogCategory("qilaunch");
 static std::vector<std::string> modules;
 static std::string listenTo;
 static std::string sd;
+static std::string proto;
+static bool any = false;
 
 _QI_COMMAND_LINE_OPTIONS(
   "Launcher options",
   ("module,m",value<std::vector<std::string> >(&modules) , "Load given module")
   ("listen,l", value<std::string>(&listenTo), "Address to listen to (default: localhost)")
-  ("master-address,m", value<std::string>(&sd), "Address of the service directory")
+  ("protocol,p", value<std::string>(&proto), "Protocol to listen on (default: tcp)")
+  ("all,a", bool_switch(&any), "Listen on all interfaces (default localhost)")
+  ("service-directory,s", value<std::string>(&sd), "Address of the service directory (default: tcp://localhost:9559)")
 )
 
 
 int main(int argc, char** argv)
 {
+  /* User tool, be over-leniant in what we accept.
+  */
   qi::Application app(argc, argv);
   qi::Session s;
-  qiLogDebug() << "Connection to sd at " << sd;
+  if (sd.empty())
+    sd = "tcp://localhost:9559";
+  else
+  {
+    size_t p = sd.find(':');
+    if (p == sd.npos)
+    { // no proto no port3
+      sd = "tcp://" + sd + ":9559";
+    }
+    else
+    {
+      if (sd[p+1] != '/')
+      { // not a proto: it is a port, so there is no proto
+        sd = "tcp://" + sd;
+      }
+      // if user specified a proto, do not try to append a port
+    }
+  }
+  qiLogInfo() << "Connection to sd at " << sd;
   s.connect(sd);
   if (listenTo.empty())
-    listenTo = "tcp://localhost:0";
+  {
+    if (!proto.empty())
+      listenTo = proto;
+    else
+      listenTo = "tcp";
+    listenTo += "://";
+    if (any)
+      listenTo += "0.0.0.0";
+    else
+      listenTo += "localhost";
+    listenTo += ":0";
+  }
+  qiLogInfo() << "Listening on " << listenTo;
   s.listen(listenTo);
+  for (int a = 1; a < argc; ++a)
+  {
+    if (argv[a][0] == '-')
+      continue; // leftover arg, ignore
+    modules.push_back(argv[a]);
+  }
   for (unsigned i=0; i<modules.size(); ++i)
   {
-    qiLogDebug() << "loading " << modules[i];
+    qiLogInfo() << "loading " << modules[i];
     s.loadService(modules[i]);
   }
   app.run();
