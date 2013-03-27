ac329f631afed9cb1bdd271a6bf31a9c556d5589
goreal <goreal@aldebaran-robotics.com>
2013-03-27 17:39:31 +0100

    test to reproduce #12982 only on robot(segfault after a while on desktop)
    Change-Id: Iaa16f60c6b6650d895e9cab6c888fb58b6e96679
    Reviewed-on: http://gerrit.aldebaran.lan:8080/15209
    Reviewed-by: goreal <goreal@aldebaran-robotics.com>
    Tested-by: goreal <goreal@aldebaran-robotics.com>

---
+++ tests/test_call_on_close_session.cpp
@@ -0,0 +1,85 @@
+/*
+** Author(s):
+**  - Guillaume OREAL <goreal@aldebaran-robotics.com>
+**
+** Copyright (C) 2010, 2012 Aldebaran Robotics
+*/
+
+#include <vector>
+#include <iostream>
+#include <string>
+
+#include <boost/thread/thread.hpp>
+#include <qimessaging/session.hpp>
+#include <qitype/genericobject.hpp>
+#include <qitype/genericobjectbuilder.hpp>
+#include <qimessaging/servicedirectory.hpp>
+#include <qimessaging/gateway.hpp>
+#include <qi/os.hpp>
+#include <qi/application.hpp>
+#include <testsession/testsessionpair.hpp>
+
+static std::string reply(const std::string &msg)
+{
+  qi::os::msleep(300);
+  std::cout << msg << std::endl;
+  return msg;
+}
+
+void myCall(qi::ObjectPtr myService)
+{
+  try
+  {
+    myService->call<std::string>("reply::s(s)", "ok");
+    qi::os::msleep(300);
+  }
+  catch(std::exception e)
+  {
+    std::cout << e.what() << std::endl;
+  }
+}
+
+//TEST(QiSession, Services)
+void test(void)
+{
+  qi::ServiceDirectory sd;
+  qi::Future<void> f = sd.listen("tcp://0.0.0.0:0");
+  int timeToWait = 1;
+
+  while(true)
+  {
+    for (int i = 0; i < 1000; i++)
+    {
+      TestSessionPair p;
+      std::cout << "time to wait is:" << timeToWait << std::endl;
+
+      qi::Session* s1 = p.server();
+      qi::Session* s2 = p.client();
+
+      qi::GenericObjectBuilder ob;
+      ob.advertiseMethod("reply", &reply);
+      qi::ObjectPtr obj(ob.object());
+
+      s1->registerService("service1", obj);
+
+      qi::ObjectPtr myService;
+      myService = s2->service("service1");
+
+      boost::thread myThread(boost::bind(&myCall, myService));
+
+      qi::os::msleep(timeToWait);
+      s1->close();
+      qi::os::msleep(3);
+    }
+    timeToWait = timeToWait +1;
+  }
+}
+int main(int argc, char **argv)
+{
+  qi::Application app(argc, argv);
+  TestMode::forceTestMode(TestMode::Mode_SD);
+
+  test();
+  return 1;
+
+}
