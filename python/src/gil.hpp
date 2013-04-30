a24d476b6dd66d0388024a485683d65b4ea19861
Cedric GESTES <gestes@aldebaran-robotics.com>
2013-04-30 15:15:40 +0200

    python: GIL RAII locks: add debug log
    Change-Id: Idf246794d7db910de8efcd4f0ed703866b0b608a

--- python/src/gil.hpp
+++ python/src/gil.hpp
@@ -10,26 +10,60 @@
 #ifndef   	GIL_HPP_
 # define   	GIL_HPP_
 
+#include <qi/log.hpp>
 #include <boost/python.hpp>
+
+
 
 namespace qi {
   namespace py {
+
+    //Allow calling python from an unknown thread
+    // (acquire the GIL and setup the thread)
     class GILScopedLock
     {
     public:
-      GILScopedLock() : fState(PyGILState_Ensure()){}
-      ~GILScopedLock() {PyGILState_Release(fState);}
+      GILScopedLock() {
+        qiLogCategory("qi.py.gil");
+        qiLogDebug() << "ScopedLockEnter(Begin)";
+        _state = PyGILState_Ensure();
+        qiLogDebug() << "ScopedLockEnter(End)";
+      }
+
+      ~GILScopedLock() {
+        qiLogCategory("qi.py.gil");
+        qiLogDebug() << "ScopedLockQuit(Begin)";
+        PyGILState_Release(_state);
+        qiLogDebug() << "ScopedLockQuit(End)";
+      }
+
     private:
-      PyGILState_STATE fState;
+      PyGILState_STATE _state;
     };
 
+    //Unlock the GIL, allow python to process other thread
+    //SHOULD NEVER BE CALLED TWICE, DO NOT NEST
     class GILScopedUnlock
     {
     public:
-      GILScopedUnlock() : fSave(PyEval_SaveThread()) {}
-      ~GILScopedUnlock() {PyEval_RestoreThread(fSave);}
+      GILScopedUnlock()
+        : _save(0)
+      {
+        qiLogCategory("qi.py.gil");
+        qiLogDebug() << "ScopedUnLockEnter(Begin)";
+        _save = PyEval_SaveThread();
+        qiLogDebug() << "ScopedUnLockEnter(End)";
+      }
+
+      ~GILScopedUnlock() {
+        qiLogCategory("qi.py.gil");
+        qiLogDebug() << "ScopedUnLockQuit(Begin)";
+        PyEval_RestoreThread(_save);
+        qiLogDebug() << "ScopedUnLockQuit(End)";
+      }
+
     private:
-      PyThreadState* fSave;
+      PyThreadState* _save;
     };
   }
 }
