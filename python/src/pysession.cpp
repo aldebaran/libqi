7298a33b9593dc55ebb0e45a8167be7002073893
Cedric GESTES <gestes@aldebaran-robotics.com>
2013-05-02 17:40:20 +0200

    python: session: add services()
    Change-Id: Iac9736af4c44eb18fc3cf639061638b86f7daec5

--- python/src/pysession.cpp
+++ python/src/pysession.cpp
@@ -61,6 +61,17 @@ namespace qi { namespace py {
         }
       }
 
+      boost::python::object services(bool _async=false) {
+        if (_async)
+          return boost::python::object(toPyFuture(_ses->services()));
+        else {
+          qi::Future< std::vector<ServiceInfo> >  fut = _ses->services();
+          std::vector<ServiceInfo> si = fut.value(); //throw on error.
+          qi::GenericValueRef r(si);
+          return r.to<boost::python::object>(); //throw on error
+        }
+      }
+
       boost::python::object registerService(const std::string &name, boost::python::object obj, bool _async=false) {
         qi::Future<unsigned int> fut = _ses->registerService(name, qi::GenericValueRef(obj).toObject());
         if (_async)
@@ -77,6 +88,7 @@ namespace qi { namespace py {
           .def("connect", &PySession::connect, (boost::python::arg("url"), boost::python::arg("_async") = false))
           .def("close", &PySession::close, (boost::python::arg("_async") = false))
           .def("service", &PySession::service, (boost::python::arg("service"), boost::python::arg("_async") = false))
+          .def("services", &PySession::services, (boost::python::arg("_async") = false))
           .def("register_service", &PySession::registerService, (boost::python::arg("name"), boost::python::arg("object"), boost::python::arg("_async") = false))
           ;
     }
