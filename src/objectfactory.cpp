e34fa851ebcc5ce5317f0ca30f693316639849b9
Cedric GESTES <gestes@aldebaran-robotics.com>
2013-05-16 15:13:30 +0200

    objectfactory: loadObject: always return the list of loaded object
    even when the library is already loaded.
    
    Change-Id: I2ed0b7ff8d33603413356a07eaf05efd5ace47b9
    Reviewed-on: http://gerrit.aldebaran.lan:8080/17444
    Reviewed-by: mnottale <mnottale@aldebaran-robotics.com>
    Tested-by: cgestes <gestes@aldebaran-robotics.com>

--- src/objectfactory.cpp
+++ src/objectfactory.cpp
@@ -18,8 +18,14 @@ namespace qi {
   static boost::recursive_mutex *_f_mutex_struct = 0;
   static boost::recursive_mutex *_f_mutex_load = 0;
   static std::vector<std::string>* _f_keys = 0;
+
+  typedef std::map< std::string, std::vector<std::string> > LoadedServiceMap;
+  static LoadedServiceMap       *_f_loadedService;
+
+
   typedef std::map<std::string, boost::function<qi::ObjectPtr (const std::string&)> > FactoryMap;
   static FactoryMap* _f_map = 0;
+
   static void _f_init()
   {
     if (!_f_mutex_struct)
@@ -28,6 +34,7 @@ namespace qi {
       _f_mutex_load = new boost::recursive_mutex;
       _f_keys = new std::vector<std::string>;
       _f_map = new FactoryMap;
+      _f_loadedService = new LoadedServiceMap;
     }
   }
 
@@ -65,24 +72,38 @@ namespace qi {
   std::vector<std::string> loadObject(const std::string& name, int flags)
   {
     /* Do not hold mutex_struct while calling dlopen/loadModule,
-  * just in case static initialization of the module happens
-  * in an other thread: it will likely call registerObjectFactory
-  * which will acquire the mutex_struct.
-  * We are still asserting that said initialization synchronously
-  * finishes before dlopen/loadModule returns.
-  */
+     * just in case static initialization of the module happens
+     * in an other thread: it will likely call registerObjectFactory
+     * which will acquire the mutex_struct.
+     * We are still asserting that said initialization synchronously
+     * finishes before dlopen/loadModule returns.
+     */
     _f_init();
+    std::vector<std::string> ret;
     std::vector<std::string>& keys = *_f_keys;
-    boost::recursive_mutex::scoped_lock sl(*_f_mutex_load);
-    unsigned int count = keys.size();
-    qiLogDebug() << count <<" object before load";
-    Application::loadModule(name, flags);
-    boost::recursive_mutex::scoped_lock sl2(*_f_mutex_struct);
-    qiLogDebug() << keys.size() <<" object after load";
-    if (count != keys.size())
-      return std::vector<std::string>(&keys[count], &keys[0] + keys.size());
-    else
-      return std::vector<std::string>();
+    {
+      boost::recursive_mutex::scoped_lock sl(*_f_mutex_load);
+
+      //already loaded?
+      LoadedServiceMap::iterator it;
+      it = _f_loadedService->find(name);
+      if (it != _f_loadedService->end()) {
+        qiLogDebug() << "Library " << name << " already loaded.";
+        return it->second;
+      }
+
+      unsigned int count = keys.size();
+      qiLogDebug() << count <<" object before load";
+      Application::loadModule(name, flags);
+      {
+        boost::recursive_mutex::scoped_lock sl2(*_f_mutex_struct);
+        qiLogDebug() << keys.size() <<" object after load";
+        if (count != keys.size())
+          ret = std::vector<std::string>(&keys[count], &keys[0] + keys.size());
+        (*_f_loadedService)[name] = ret;
+      }
+    }
+    return ret;
   }
 
 }
