6e2e5621363387750a7bbcae23a71ca0a0516af9
vbarbaresi <vbarbaresi@aldebaran-robotics.com>
2013-05-24 17:06:17 +0200

    Add some type tests
    Change-Id: Ibf612f3b6a2cfee113f504f0119dfd9b528b7826

--- python/test/test_servicedirectory.py
+++ python/test/test_servicedirectory.py
@@ -5,7 +5,6 @@ from qi import Session
 
 class TestService:
     def display(self, t):
-        print(t)
         return t
 
 
@@ -37,11 +36,64 @@ def test_unicode_strings():
     m = TestService()
     s.register_service("TestService", m)
     service = s.service("TestService")
-    # Only 1 character is actually used
-    mystring = service.display(u"my unicode poney")
-    assert mystring == "my unicode poney"
+    # ASCII range
+    unicode_string = ''.join([unichr(i) for i in range(1, 128)])
+    mystring = service.display(unicode_string)
+    assert type(mystring) == str
+    assert mystring == unicode_string.encode("ascii")
 
+    # Wide unicode
+    wide_string = "\U00010000" * 39 + "\uffff" * 4096
+    mystring = service.display(wide_string)
+    assert mystring == wide_string
+
+    # String with many unicode chars
+    unicode_string = ''.join([unichr(i) for i in range(1, 50000)])
+    service.display(unicode_string)
+    time.sleep(0.01)
     s.close()
+
+
+def test_builtin_types():
+    local = "tcp://127.0.0.1:5555"
+    sd = ServiceDirectory()
+    sd.listen(local)
+
+    s = Session()
+    s.connect(local)
+
+    m = TestService()
+    s.register_service("TestService", m)
+    service = s.service("TestService")
+
+    # None
+
+    # bool
+    t, f = service.display(True), service.display(False)
+    assert t == 1L  # is True ?
+    assert f == 0L  # is False ?
+
+    # int
+    assert type(service.display(42)) == int
+    assert service.display(42) == 42
+
+    # float
+    assert service.display(0.1337) == 0.1337
+
+    # long
+    assert type(service.display(2 ** 64)) == long
+    assert service.display(2 ** 64) == 18446744073709551616L
+
+    # list
+    # set
+    # tuple
+    # dict
+    # buffer
+
+    time.sleep(0.01)
+    s.close()
+
 if __name__ == "__main__":
-    #test_throwing_callback()
+    test_throwing_callback()
     test_unicode_strings()
+    test_builtin_types()
