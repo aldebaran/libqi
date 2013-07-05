dcd1ef5a158c57df71f69aba6325e3598a010389
Cedric GESTES <gestes@aldebaran-robotics.com>
2013-07-05 16:46:58 +0200

    functionsignature: fix a racecond in functionArgumentsSignature
    Change-Id: I39b75a876b9a61b065f031fb6a569e3dafe4d052

--- qitype/details/functionsignature.hxx
+++ qitype/details/functionsignature.hxx
@@ -26,6 +26,8 @@
 #ifndef _QITYPE_DETAILS_FUNCTIONSIGNATURE_HXX_
 #define _QITYPE_DETAILS_FUNCTIONSIGNATURE_HXX_
 
+#include <boost/thread/mutex.hpp>
+
 namespace qi {
   namespace detail {
     struct signature_function_arg_apply {
@@ -108,7 +110,7 @@ namespace qi {
         // Reconstruct the boost::bind(instance, _1, _2...) signature
         typedef typename boost::function_types::result_type<T>::type     RetType;
         typedef typename boost::function_types::parameter_types<T>::type MemArgsType;
-        typedef typename boost::mpl::pop_front< MemArgsType >::type                ArgsType;
+        typedef typename boost::mpl::pop_front< MemArgsType >::type      ArgsType;
         typedef typename boost::mpl::push_front<ArgsType, RetType>::type EffectiveType;
         typedef typename boost::function_types::function_type<EffectiveType>::type type;
         return RawFunctionSignature<type>::makeSignature();
@@ -145,15 +147,19 @@ namespace qi {
       static std::string sigs;
       if (!done)
       {
-        sigs += '(';
-        typedef typename boost::function_types::parameter_types<T>::type ArgsType;
-        boost::mpl::for_each<
-        boost::mpl::transform_view<ArgsType,
-        boost::add_pointer<
-        boost::remove_const<
-        boost::remove_reference<boost::mpl::_1> > > > > (qi::detail::signature_function_arg_apply(&sigs));
-        sigs += ')';
-        done = true;
+        static boost::mutex mut;
+        boost::mutex::scoped_lock _lock(mut);
+        if (!done) {
+          sigs += '(';
+          typedef typename boost::function_types::parameter_types<T>::type ArgsType;
+          boost::mpl::for_each<
+              boost::mpl::transform_view<ArgsType,
+              boost::add_pointer<
+              boost::remove_const<
+              boost::remove_reference<boost::mpl::_1> > > > > (qi::detail::signature_function_arg_apply(&sigs));
+          sigs += ')';
+          done = true;
+        }
       }
       return Signature(sigs);
     }
