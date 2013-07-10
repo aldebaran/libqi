db6a10e6d7c01a581d19c5a8786f6bc0be542a5a
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2013-07-10 13:06:57 +0200

    port to qidoc3
    Change-Id: Ie3c42deef204b5fafb72e90a3b04aa0108f06d67
    Reviewed-on: http://gerrit.aldebaran.lan/24566
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
    Tested-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>

--- doc/source/conf.in.py
+++ doc/source/conf.in.py
@@ -5,4 +5,3 @@ extensions.extend(['qiapidoc', 'sphinx.ext.graphviz'])
 import os
 
 qiapidoc_srcs = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', '..', 'build-doc', 'xml'))
-qiapidoc_gen_skeleton = '/tmp/skeleton'
