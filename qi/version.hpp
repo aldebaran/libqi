7a4ff6f0ed1cfd664c89d85cfae78586f0935043
llec <llec@aldebaran-robotics.com>
2013-07-10 09:26:14 +0200

    Revert "Change namespace qi::version to class qi::Version"
    This reverts commit 654b63d1abcf64143780021e4b6901d2fe304142
    
    Change-Id: I918d3f9f40f2d33a331211cae89b1e280d11e4ce
    Reviewed-on: http://gerrit.aldebaran.lan/20885
    Reviewed-by: llec <llec@aldebaran-robotics.com>
    Tested-by: llec <llec@aldebaran-robotics.com>

--- qi/version.hpp
+++ qi/version.hpp
@@ -9,32 +9,37 @@
 #ifndef _QI_VERSION_HPP_
 #define _QI_VERSION_HPP_
 
-#include <boost/shared_ptr.hpp>
-#include <qi/api.hpp>
-#include <vector>
-#include <string>
+# include <qi/api.hpp>
+# include <vector>
+# include <string>
 
 namespace qi {
-  class VersionPrivate;
-  class QI_API Version
-  {
-    public:
-      Version();
-      Version(const std::string& version);
-      Version(const char* version);
+  namespace version {
 
-      const std::string& toString() const;
+    struct QI_API Version
+    {
+      std::string version;
 
-      bool operator< (const Version& rhs) const;
-      bool operator> (const Version& rhs) const;
-      bool operator==(const Version& rhs) const;
-      bool operator!=(const Version& rhs) const;
-      bool operator<=(const Version& rhs) const;
-      bool operator>=(const Version& rhs) const;
+      std::string& operator()();
+      const std::string& operator()() const;
+      bool operator< (const Version& pi) const;
+      bool operator> (const Version& pi) const;
+      bool operator==(const Version& pi) const;
+      bool operator!=(const Version& pi) const;
+      bool operator<=(const Version& pi) const;
+      bool operator>=(const Version& pi) const;
+    };
 
-    private:
-      boost::shared_ptr<VersionPrivate> _p;
-  };
+    //convert a version's string into a vector<string> with each comparable part
+    QI_API std::vector<std::string> explode(const std::string &version);
+
+    //compare two version's strings. a < b return -1
+    QI_API int                      compare(const std::string &versionA,
+                                            const std::string &versionB);
+
+    QI_API std::string              extract(const std::string &version);
+
+  }
 }
 
 #endif  // _QI_VERSION_HPP_
--- src/version.cpp
+++ src/version.cpp
@@ -12,208 +12,189 @@
 #include <locale>
 
 namespace qi {
-  class VersionPrivate {
-    public:
-      VersionPrivate();
-      VersionPrivate(const std::string& version);
+  namespace version {
 
-      int compare(const std::string &versionA, const std::string &versionB);
-      std::vector<std::string> explode(const std::string &version);
+    static std::string eat_number(const std::string &str, unsigned int &index)
+    {
+      std::locale              loc("C");
+      unsigned int             first = index;
 
-      std::string _version;
-
-    private:
-      int compare_sub(const std::string &subA, const std::string &subB);
-      std::string eat_number(const std::string &str, unsigned int &index);
-      std::string eat_alpha(const std::string &str, unsigned int &index);
-  };
-
-  VersionPrivate::VersionPrivate()
-    : _version(std::string())
-  { }
-
-  VersionPrivate::VersionPrivate(const std::string& version)
-  {
-    _version = version;
-  }
-
-  int VersionPrivate::compare(const std::string &versionA, const std::string &versionB)
-  {
-    std::locale              loc("C");
-    std::vector<std::string> vA = explode(versionA);
-    std::vector<std::string> vB = explode(versionB);
-    int                      ret = 0;
-    int                      sepA = 0;
-    int                      sepB = 0;
-    std::string              cA;
-    std::string              cB;
-
-    while(1) {
-      if (!vA.size())
-        cA = std::string();
-      else {
-        cA = vA.front();
-        vA.erase(vA.begin());
-      }
-      if (!vB.size())
-        cB = std::string();
-      else {
-        cB = vB.front();
-        vB.erase(vB.begin());
-      }
-
-      if (cA.empty() && cB.empty())
-        return 0;
-      if (cA.empty())
-        return -1;
-      if (cB.empty())
-        return 1;
-      //dont ask why... but that how the python code works
-      if (!std::isdigit(cA[0], loc))
-        sepA = (cA == "." || cA == "-");
-      if (!std::isdigit(cB[0], loc))
-        sepB = (cB == "." || cB == "-");
-      if (sepA && !sepB)
-        return -1;
-      if (!sepA && sepB)
-        return 1;
-      ret = compare_sub(cA, cB);
-      if (ret)
-        return ret;
-    }
-    return 0;
-  }
-
-  std::vector<std::string> VersionPrivate::explode(const std::string &version)
-  {
-    std::locale              loc("C");
-    std::vector<std::string> result;
-    unsigned int             index = 0;
-
-    while (index < version.length()) {
-      if (std::isdigit(version[index], loc))
-        result.push_back(eat_number(version, index));
-      else if (std::isalpha(version[index], loc))
-        result.push_back(eat_alpha(version, index));
-      else
+      while (index < str.length())
       {
-        result.push_back(std::string(version, index, 1));
+        if (!std::isdigit(str[index], loc))
+          break;
         index++;
       }
+      return std::string(str, first, index - first);
     }
-    return result;
-  }
 
-  int VersionPrivate::compare_sub(const std::string &subA, const std::string &subB)
-  {
-    std::locale loc("C");
-    int         digitA;
-    int         digitB;
-
-    digitA = std::isdigit(subA[0], loc);
-    digitB = std::isdigit(subB[0], loc);
-
-    // string > int
-    if (digitA && !digitB)
-      return -1;
-    if (!digitA && digitB)
-      return 1;
-    if (digitA && digitB)
+    static std::string eat_alpha(const std::string &str, unsigned int &index)
     {
-      int intA = atoi(subA.c_str());
-      int intB = atoi(subB.c_str());
-      if (intA > intB)
-        return 1;
-      if (intA < intB)
+      std::locale              loc("C");
+      unsigned int             first = index;
+
+      while (index < str.length())
+      {
+        if (!std::isalpha(str[index], loc))
+          break;
+        index++;
+      }
+      return std::string(str, first, index - first);
+    }
+
+    ///split a version string into a list
+    std::vector<std::string> explode(const std::string &version){
+      std::locale              loc("C");
+      std::vector<std::string> result;
+      unsigned int             index = 0;
+
+      while (index < version.length()) {
+        if (std::isdigit(version[index], loc))
+          result.push_back(eat_number(version, index));
+        else if (std::isalpha(version[index], loc))
+          result.push_back(eat_alpha(version, index));
+        else
+        {
+          result.push_back(std::string(version, index, 1));
+          index++;
+        }
+      }
+      return result;
+    }
+
+    //compare two substring
+    static int compare_sub(const std::string &subA, const std::string &subB)
+    {
+      std::locale loc("C");
+      int         digitA;
+      int         digitB;
+
+      digitA = std::isdigit(subA[0], loc);
+      digitB = std::isdigit(subB[0], loc);
+
+      // string > int
+      if (digitA && !digitB)
         return -1;
-    }
-    else
-    {
-      //compare string
-      if (subA > subB)
+      if (!digitA && digitB)
         return 1;
-      if (subA < subB)
-        return -1;
+      if (digitA && digitB)
+      {
+        int intA = atoi(subA.c_str());
+        int intB = atoi(subB.c_str());
+        if (intA > intB)
+          return 1;
+        if (intA < intB)
+          return -1;
+      }
+      else
+      {
+        //compare string
+        if (subA > subB)
+          return 1;
+        if (subA < subB)
+          return -1;
+      }
+      return 0;
     }
-    return 0;
-  }
 
-  std::string VersionPrivate::eat_number(const std::string &str, unsigned int &index)
-  {
-    std::locale              loc("C");
-    unsigned int             first = index;
+    /// compare two version
+    int compare(const std::string &versionA, const std::string &versionB){
+      std::locale              loc("C");
+      std::vector<std::string> vA = explode(versionA);
+      std::vector<std::string> vB = explode(versionB);
+      int                      ret = 0;
+      int                      sepA = 0;
+      int                      sepB = 0;
+      std::string              cA;
+      std::string              cB;
 
-    while (index < str.length())
+      while(1) {
+        if (!vA.size())
+          cA = std::string();
+        else {
+          cA = vA.front();
+          vA.erase(vA.begin());
+        }
+        if (!vB.size())
+          cB = std::string();
+        else {
+          cB = vB.front();
+          vB.erase(vB.begin());
+        }
+
+        if (cA.empty() && cB.empty())
+          return 0;
+        if (cA.empty())
+          return -1;
+        if (cB.empty())
+          return 1;
+        //dont ask why... but that how the python code works
+        if (!std::isdigit(cA[0], loc))
+          sepA = (cA == "." || cA == "-");
+        if (!std::isdigit(cB[0], loc))
+          sepB = (cB == "." || cB == "-");
+        if (sepA && !sepB)
+          return -1;
+        if (!sepA && sepB)
+          return 1;
+        ret = compare_sub(cA, cB);
+        if (ret)
+          return ret;
+      }
+      return 0;
+    }
+
+    std::string extract(const std::string &version) {
+      //use cryptic var name for regex
+      boost::regex sverptn("(([0-9]+)\\.){1,3}([0-9]+)((-rc[0-9]+)|(-oe[0-9]+)){0,1}");
+      boost::match_results<std::string::const_iterator> what;
+      if (boost::regex_search(version, what, sverptn))
+        return what[0];
+      return std::string();
+    }
+
+    std::string& Version::operator()()
     {
-      if (!std::isdigit(str[index], loc))
-        break;
-      index++;
+      return version;
     }
-    return std::string(str, first, index - first);
-  }
 
-  std::string VersionPrivate::eat_alpha(const std::string &str, unsigned int &index)
-  {
-    std::locale              loc("C");
-    unsigned int             first = index;
-
-    while (index < str.length())
+    const std::string& Version::operator()() const
     {
-      if (!std::isalpha(str[index], loc))
-        break;
-      index++;
+      return version;
     }
-    return std::string(str, first, index - first);
-  }
 
-  Version::Version()
-    :_p(new VersionPrivate)
-  { }
+    bool Version::operator<(const Version& pi) const
+    {
+      return qi::version::compare(version, pi.version) < 0;
+    }
 
-  Version::Version(const std::string& version)
-    : _p(new VersionPrivate(version))
-  { }
+    bool Version::operator==(const Version& pi) const
+    {
+      return !qi::version::compare(version, pi.version);
+    }
 
-  Version::Version(const char* version)
-    : _p(new VersionPrivate(version))
-  { }
+    bool Version::operator>(const Version& pi) const
+    {
+      const Version& cpi = *this;
+      return (cpi != pi) && !(cpi < pi);
+    }
 
-  const std::string& Version::toString() const
-  {
-    return _p->_version;
-  }
+    bool Version::operator!=(const Version& pi) const
+    {
+      const Version& cpi = *this;
+      return !(cpi == pi);
+    }
 
-  bool Version::operator<(const Version& rhs) const
-  {
-    return _p->compare(_p->_version, rhs.toString()) < 0;
-  }
+    bool Version::operator<=(const Version& pi) const
+    {
+      const Version& cpi = *this;
+      return !(cpi > pi);
+    }
 
-  bool Version::operator==(const Version& rhs) const
-  {
-    return !_p->compare(_p->_version, rhs.toString());
-  }
-
-  bool Version::operator>(const Version& rhs) const
-  {
-    const Version& cpi = *this;
-    return (cpi != rhs) && !(cpi < rhs);
-  }
-
-  bool Version::operator!=(const Version& rhs) const
-  {
-    const Version& cpi = *this;
-    return !(cpi == rhs);
-  }
-
-  bool Version::operator<=(const Version& rhs) const
-  {
-    const Version& cpi = *this;
-    return !(cpi > rhs);
-  }
-
-  bool Version::operator>=(const Version& rhs) const
-  {
-    const Version& cpi = *this;
-    return !(cpi < rhs);
+    bool Version::operator>=(const Version& pi) const
+    {
+      const Version& cpi = *this;
+      return !(cpi < pi);
+    }
   }
 }
