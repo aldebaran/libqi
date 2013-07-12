efed3504e4fb0a6b964a1a3fb7b113a8006c5aff
Cedric GESTES <gestes@aldebaran-robotics.com>
2013-07-12 11:32:45 +0200

    add directive to suppress spurious deprecated warnings
    Change-Id: I877b5a542aa2c6b7553397f228df02602218fc2f
    Reviewed-on: http://gerrit.aldebaran.lan/21124
    Reviewed-by: mnottale <mnottale@aldebaran-robotics.com>
    Tested-by: cgestes <gestes@aldebaran-robotics.com>

---
+++ qi/details/warn_pop_ignore_deprecated.hpp
@@ -0,0 +1,11 @@
+/*
+**  Copyright (C) 2012 Aldebaran Robotics
+**  See COPYING for the license
+*/
+
+// clang and GCC
+#if defined(__GNUC__)
+# pragma GCC diagnostic pop
+#elif defined(_MSC_VER)
+# pragma warning(pop)
+#endif
---
+++ qi/details/warn_push_ignore_deprecated.hpp
@@ -0,0 +1,25 @@
+/*
+**  Copyright (C) 2012 Aldebaran Robotics
+**  See COPYING for the license
+*/
+
+/*
+ * this allow removing spurious warning when a deprecated declaration use another deprecated declaration.
+ *
+ * QI_API_DEPRECATED class Foo;
+ *
+ * #include <qi/details/diagnostic_push_ignore_deprecated.hpp>
+ * //Would emit a spurious deprecated warning about Foo otherwise
+ * QI_API_DEPRECATED void bar(Foo foo);
+ * #include <qi/details/diagnostic_pop.hpp>
+ */
+
+
+// clang and GCC
+#if defined(__GNUC__)
+# pragma GCC diagnostic push
+# pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#elif defined(_MSC_VER)
+# pragma warning(push)
+# pragma warning(disable : 4996)
+#endif
