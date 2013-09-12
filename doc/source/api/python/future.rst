fd213413619ed50bb66e28aceb5595c788f647ed
Cedric GESTES <gestes@aldebaran-robotics.com>
2013-09-12 13:41:53 +0200

    python: document pyfuture
    Change-Id: I313b31f751f3f362a86e91d0f86b35422e51d7a0

--- doc/source/api/python/future.rst
+++ doc/source/api/python/future.rst
@@ -3,12 +3,103 @@
 qi.Future API
 =============
 
-.. autodata:: _qi.FutureState
+Introduction
+------------
 
-.. autoattribute:: qi.FutureTimeout.Infinite
+Promise and Future are a way to synchronise data between multiples threads.
+The number of future associated to a promise is not limited.
+Promise is the setter, future is the getter.
+Basically, you have a task to do, that will return a value, you give it a Promise.
+Then you have others thread that depend on the result of that task, you give them future
+associated to that promise, the future will block until the value or an error is set
+by the promise.
+
+Simple example:
+
+.. code-block:: python
+
+  import qi
+  import time
+
+  def doSomeWork(p):
+    #do your work here instead of sleeping
+    time.sleep(1)
+    p.setValue(42)
+
+  p = qi.Promise()
+  f = p.future()
+  threading.Thread(target=doSomeWork, args=[p]).start()
+  print "result:", f.value()
+
+With callback:
+
+.. code-block:: python
+
+  import qi
+  import time
+
+  def doSomeWork(p):
+    #do your work here instead of sleeping
+    time.sleep(1)
+    p.setValue(42)
+
+  def resultReady(f):
+    if f.hasValue():
+      print "Value:", f.value()
+    elif f.hasError():
+      print "Error:", f.error()
+
+  p = qi.Promise()
+  f = p.future()
+  threading.Thread(target=doSomeWork, args=[p]).start()
+
+  #resultReady will be called even if the result is already there.
+  f.addCallback(resultReady)
+
+
+Reference
+---------
+
+.. py:data:: qi.FutureState
+
+  global constant that describe the state of the Future.
+  This is the status returned by :py:func:`qi.Future.wait`
+
+  .. py:data:: qi.FutureState.None
+
+    The future is not bound.
+
+  .. py:data:: qi.FutureState.Running
+
+    The future is attached to a Promise.
+
+  .. py:data:: qi.FutureState.Canceled
+
+    The future has been canceled
+
+  .. py:data:: qi.FutureState.FinishedWithError
+
+    The future has finished and a value is available
+
+  .. py:data:: qi.FutureState.FinisehdWithValue
+
+    The future has finished and an error is available
+
+
+.. py:data:: qi.FutureTimeout
+
+  global constant to use for timeout arguments.
+
+  .. py:data:: qi.FutureTimeout.None
+
+    Do not wait.
+
+  .. py:data:: qi.FutureTimeout.Infinite
+
+    Block forever
 
 .. autoclass:: qi.Promise
    :members:
 
-.. autoclass:: _qi.Future
+.. autoclass:: qi.Future
    :members:
--- python/src/pyfuture.cpp
+++ python/src/pyfuture.cpp
@@ -100,47 +100,83 @@ namespace qi {
     }
 
     void export_pyfuture() {
-      boost::python::enum_<qi::FutureState>("FutureState", "Represent the state of the Future")
+      boost::python::enum_<qi::FutureState>("FutureState")
           .value("None", qi::FutureState_None)
           .value("Running", qi::FutureState_Running)
           .value("Canceled", qi::FutureState_Canceled)
           .value("FinishedWithError", qi::FutureState_FinishedWithError)
           .value("FinishedWithValue", qi::FutureState_FinishedWithValue);
 
+
       boost::python::enum_<qi::FutureTimeout>("FutureTimeout")
           .value("None", qi::FutureTimeout_None)
           .value("Infinite", qi::FutureTimeout_Infinite);
 
+
       boost::python::class_<PyPromise>("Promise")
           .def(boost::python::init<boost::python::object>())
-          .def("setCanceled", &PyPromise::setCanceled, "Set the state of the promise to Canceled")
-          .def("setError", &PyPromise::setError, "Set the error of the promise")
-          .def("setValue", &PyPromise::setValue, "Set the value of the promise")
-          .def("future", &PyPromise::future, "Get a qi.Future from the promise, you can get multiple from the same promise.");
+
+          .def("setCanceled", &PyPromise::setCanceled,
+               "setCanceled() -> None\n"
+               "Set the state of the promise to Canceled")
+
+          .def("setError", &PyPromise::setError,
+               "setError(error) -> None\n"
+               "Set the error of the promise")
+
+          .def("setValue", &PyPromise::setValue,
+               "setValue(value) -> None\n"
+               "Set the value of the promise")
+
+          .def("future", &PyPromise::future,
+               "future() -> qi.Future\n"
+               "Get a qi.Future from the promise, you can get multiple from the same promise.");
 
       boost::python::class_<PyFuture>("Future", boost::python::no_init)
           .def("value", &PyFuture::value, (boost::python::args("timeout") = qi::FutureTimeout_Infinite),
+               "value(timeout) -> value\n"
                "return the value of the Future. Block until the future is ready. Raise an exception if the future has error. "
                "If a timeout is specified in parameter, and the future is not ready past that time, the function raise an exception.")
+
           .def("error", &PyFuture::error, (boost::python::args("timeout") = qi::FutureTimeout_Infinite),
+               "error(timeout) -> None\n"
                "return the error of the Future. Block until the future is ready. "
                "Raise an exception if the function timeout or the future has no error")
+
           .def("wait", &PyFuture::wait, (boost::python::args("timeout") = qi::FutureTimeout_Infinite),
-               "Wait for the future to be ready. Raise an exception on timeout." )
+               "wait(timeout) -> qi.FutureState\n"
+               "Wait for the future to be ready. Raise an exception on timeout. Return a qi.FutureState." )
+
           .def("hasError", &PyFuture::hasError, (boost::python::args("timeout") = qi::FutureTimeout_Infinite),
+               "hasError(timeout) -> bool\n"
                "Return true or false depending on the future having an error. Raise an exception on timeout")
+
           .def("hasValue", &PyFuture::hasValue, (boost::python::args("timeout") = qi::FutureTimeout_Infinite),
+               "hasValue(timeout) -> bool\n"
                "Return true or false depending on the future having a value. Raise an exception on timeout")
+
           .def("cancel", &PyFuture::cancel,
+               "cancel() -> None\n"
                "If the future is cancelable, ask for cancelation.")
+
           .def("isFinished", &PyFuture::isFinished,
+               "isFinished() -> bool\n"
                "Is the future not running anymore? (true if hasError or hasValue or isCanceled)")
+
           .def("isRunning", &PyFuture::isRunning,
+               "isRunning() -> bool\n"
                "Is the future still running?")
+
           .def("isCanceled", &PyFuture::isCanceled,
+               "isCanceled() -> bool\n"
                "Is the future canceled?")
-          .def("isCanceleable", &PyFuture::isCanceleable, "Is the future canceleable. (not all future are canceleable)")
+
+          .def("isCanceleable", &PyFuture::isCanceleable,
+               "isCanceleable() -> bool\n"
+               "Is the future canceleable. (not all future are canceleable)")
+
           .def("addCallback", &PyFuture::addCallback,
+               "addCallback(cb) -> None\n"
                "Add a callback that will be called then the future become ready. The callback will be called even if the future is already ready."
                "The first argument of the callback is the future itself.");
     }
