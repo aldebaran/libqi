636925907d4117cecaddde4aecaea33b287f50a4
Sébastien Barthélémy <sbarthelemy@aldebaran-robotics.com>
2013-09-12 15:40:23 +0200

    improve qi::path doc
    and add a test about finding directories
    
    Change-Id: Ieaed604cc1da4363a81f473d6bc3ac1d649ae754
    Reviewed-on: http://gerrit.aldebaran.lan/26618
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
    Reviewed-by: jfremy <jfremy@aldebaran-robotics.com>
    Reviewed-by: cgestes <gestes@aldebaran-robotics.com>
    Reviewed-by: sbarthelemy <sbarthelemy@aldebaran-robotics.com>
    Tested-by: sbarthelemy <sbarthelemy@aldebaran-robotics.com>

--- qi/path.hpp
+++ qi/path.hpp
@@ -65,15 +65,20 @@ namespace qi
     QI_API std::string findConf(const std::string& applicationName,
                                 const std::string& filename);
 
-    /// Look for a data file.
+    /// Look for a file in all dataPaths(applicationName) directories,
+    /// return the first match.
     QI_API std::string findData(const std::string& applicationName,
                                 const std::string& filename);
 
-    /// Look for all data files.
+    /// Look for a file in all dataPaths(applicationName) directories,
+    /// return all the matches.
     QI_API std::vector<std::string> findListData(const std::string& applicationName,
                                                  const std::string& filename);
 
-    /// List data files matching the given pattern. Directories are discarded.
+    /// List data files matching the given pattern in all
+    /// dataPaths(applicationName) directories. For each match, return the
+    ///  occurence from the first dataPaths prefix.
+    /// Directories are discarded.
     QI_API std::vector<std::string> listData(const std::string& applicationName,
                                              const std::string& pattern="*");
 
