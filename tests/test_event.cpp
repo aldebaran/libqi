091df2d421eda2430baea799c5fabd66e707306c
Matthieu Nottale <mnottale@aldebaran-robotics.com>
2013-12-04 14:03:33 +0100

    TraceAnalyzer: New.
    Change-Id: Iebc7b019a95bebb2ee988fe1d5e4d651b0faa008

--- tests/test_event.cpp
+++ tests/test_event.cpp
@@ -5,10 +5,16 @@
 
 
 #include <map>
+
+#include <boost/algorithm/string.hpp>
+
 #include <gtest/gtest.h>
 #include <qi/application.hpp>
 #include <qitype/anyobject.hpp>
 #include <qitype/dynamicobjectbuilder.hpp>
+#include <qitype/traceanalyzer.hpp>
+
+qiLogCategory("test");
 
 static int lastPayload = 0;
 static qi::Promise<int> pPayload;
@@ -81,6 +87,123 @@ TEST(TestObject, EmitMethod)
   obj.post("fire", 23);
   EXPECT_TRUE(pPayload.future().wait(2000) != qi::FutureState_Running);
   EXPECT_EQ(23, pPayload.future().value());
+}
+
+using qi::EventTrace;
+
+inline qi::os::timeval ts(qi::int64_t v)
+{
+  qi::os::timeval res;
+  res.tv_sec = v / 1000000;
+  res.tv_usec = v % 1000000;
+  return res;
+}
+
+std::string sort(const std::string& input)
+{
+  std::vector<std::string> s;
+  boost::algorithm::split(s, input, boost::algorithm::is_any_of("\n"));
+  if (s.back().empty())
+    s.pop_back();
+  std::sort(s.begin(), s.end());
+  return boost::algorithm::join(s, "\n") + "\n";
+}
+
+TEST(TestTraceAnalyzer, Basic)
+{
+  qi::TraceAnalyzer ta;
+  qi::AnyValue noargs;
+  EXPECT_EQ(ta.dumpTraces(), "");
+  ta.addTrace(qi::EventTrace(10, EventTrace::Event_Call,   100, noargs, ts(10), 0, 0, 50, 50), 1);
+  ta.addTrace(qi::EventTrace(10, EventTrace::Event_Result, 100, noargs, ts(11), 0, 0, 50, 50), 1);
+  EXPECT_EQ(ta.dumpTraces(), "50 10:1.100\n");
+  ta.clear();
+  ta.addTrace(qi::EventTrace(10, EventTrace::Event_Result, 100, noargs, ts(11), 0, 0, 50, 50), 1);
+  ta.addTrace(qi::EventTrace(10, EventTrace::Event_Call,   100, noargs, ts(10), 0, 0, 50, 50), 1);
+  EXPECT_EQ(ta.dumpTraces(), "50 10:1.100\n");
+  ta.addTrace(qi::EventTrace(11, EventTrace::Event_Result, 100, noargs, ts(13), 0, 0, 50, 50), 1);
+  ta.addTrace(qi::EventTrace(11, EventTrace::Event_Call,   100, noargs, ts(12), 0, 0, 50, 50), 1);
+  EXPECT_EQ(ta.dumpTraces(), "50 10:1.100 11:1.100\n");
+  ta.addTrace(qi::EventTrace(12, EventTrace::Event_Result, 100, noargs, ts(11), 0, 0, 51, 51), 1);
+  ta.addTrace(qi::EventTrace(12, EventTrace::Event_Call,   100, noargs, ts(10), 0, 0, 51, 51), 1);
+  EXPECT_EQ(ta.dumpTraces(), "51 12:1.100\n50 10:1.100 11:1.100\n");
+  ta.clear();
+}
+
+TEST(TestTraceAnalyzer, Children)
+{
+  qi::TraceAnalyzer ta;
+  qi::AnyValue noargs;
+  // try all possible orders
+  std::vector<qi::EventTrace> v;
+  v.push_back(qi::EventTrace(10, EventTrace::Event_Result, 100, noargs, ts(20), 0, 0, 50, 50));
+  v.push_back(qi::EventTrace(10, EventTrace::Event_Call,   100, noargs, ts(10), 0, 0, 50, 50));
+  v.push_back(qi::EventTrace(11, EventTrace::Event_Result, 101, noargs, ts(14), 0, 0, 50, 50));
+  v.push_back(qi::EventTrace(11, EventTrace::Event_Call,   101, noargs, ts(12), 0, 0, 50, 50));
+  int permutator[] = { 0,1,2,3};
+  unsigned count = 0;
+  do
+  {
+    ++count;
+    ta.clear();
+    for (unsigned i=0; i<4; ++i)
+      ta.addTrace(v[permutator[i]], 1);
+    std::set<qi::TraceAnalyzer::FlowLink> fl;
+    ta.analyze(fl);
+    EXPECT_EQ(ta.dumpTraces(), "50 10:1.100< 11:1.101>\n");
+  } while (std::next_permutation(permutator, permutator + 4));
+  EXPECT_EQ(24U, count);
+}
+
+TEST(TestTraceAnalyzer, AsyncChildren)
+{
+  qi::TraceAnalyzer ta;
+  qi::AnyValue noargs;
+  std::vector<qi::EventTrace> v;
+  v.push_back(qi::EventTrace(10, EventTrace::Event_Result, 100, noargs, ts(20), 0, 0, 50, 50));
+  v.push_back(qi::EventTrace(10, EventTrace::Event_Call,   100, noargs, ts(10), 0, 0, 50, 50));
+  v.push_back(qi::EventTrace(11, EventTrace::Event_Result, 101, noargs, ts(25), 0, 0, 50, 51));
+  v.push_back(qi::EventTrace(11, EventTrace::Event_Call,   101, noargs, ts(21), 0, 0, 50, 51, ts(12)));
+  int permutator[] = { 0,1,2,3};
+  unsigned count = 0;
+  do
+  {
+    ++count;
+    ta.clear();
+    qiLogDebug() << "New iteration";
+    for (unsigned i=0; i<4; ++i)
+      ta.addTrace(v[permutator[i]], 1);
+    std::set<qi::TraceAnalyzer::FlowLink> fl;
+    ta.analyze(fl);
+    ASSERT_EQ(sort(ta.dumpTraces()), "50 10:1.100{11,}\n51 11:1.101\n");
+  } while (std::next_permutation(permutator, permutator + 4));
+  EXPECT_EQ(24U, count);
+}
+
+
+TEST(TestTraceAnalyzer, BogusChildren)
+{
+  qi::TraceAnalyzer ta;
+  qi::AnyValue noargs;
+  std::vector<qi::EventTrace> v;
+  v.push_back(qi::EventTrace(10, EventTrace::Event_Result, 100, noargs, ts(20), 0, 0, 50, 50));
+  v.push_back(qi::EventTrace(10, EventTrace::Event_Call,   100, noargs, ts(10), 0, 0, 50, 50));
+  v.push_back(qi::EventTrace(11, EventTrace::Event_Result, 101, noargs, ts(30), 0, 0, 50, 50));
+  v.push_back(qi::EventTrace(11, EventTrace::Event_Call,   101, noargs, ts(21), 0, 0, 50, 50));
+  int permutator[] = { 0,1,2,3};
+  unsigned count = 0;
+  do
+  {
+    ++count;
+    ta.clear();
+    qiLogDebug() << "New iteration";
+    for (unsigned i=0; i<4; ++i)
+      ta.addTrace(v[permutator[i]], 1);
+    std::set<qi::TraceAnalyzer::FlowLink> fl;
+    ta.analyze(fl);
+    ASSERT_EQ(sort(ta.dumpTraces()), "50 10:1.100 11:1.101\n");
+  } while (std::next_permutation(permutator, permutator + 4));
+  EXPECT_EQ(24U, count);
 }
 
 int main(int argc, char **argv) {
