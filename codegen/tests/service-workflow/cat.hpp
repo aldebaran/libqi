bbf641118419e07f953b9ef7afdf989d1316b1b7
Matthieu Nottale <mnottale@aldebaran-robotics.com>
2014-01-16 17:41:43 +0100

    codegen: pass/return object in test.
    Change-Id: Iefe4c8a4f1e3c712f1d3822f344ffa7d7849f707

--- codegen/tests/service-workflow/cat.hpp
+++ codegen/tests/service-workflow/cat.hpp
@@ -1,4 +1,5 @@
 #include <qitype/anyobject.hpp>
+#include <qitype/property.hpp>
 
 namespace animals
 {
@@ -7,12 +8,25 @@ struct Mosquito
 {
   double yaw,theta,distance;
 };
+
+/// Possible action a cat can perform
+class CatAction
+{
+public:
+  /// Run the action
+  virtual void run() = 0;
+  virtual std::vector<float> expectedResult() = 0;
+};
+
+/** Interface to a cat */
 class Cat
 {
   public:
     virtual ~Cat() {}
     virtual void meow(int volume) = 0;
     virtual bool setTarget(const Mosquito& m) = 0;
+    virtual qi::Object<CatAction> selectTask() = 0;
+    virtual bool canPerform(qi::Object<CatAction> task) = 0;
 
     qi::Property<float> hunger;
     qi::Property<float> boredom;
