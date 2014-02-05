adf7ded0c1b935e05c77e3c1a5e41a279c54f0c6
Nicolas Cornu <ncornu@aldebaran-robotics.com>
2014-02-05 10:52:57 +0100

    Do more pythonic logger
    Change-Id: Ibd7e1ee80da3dd949bf7d7f6ccab8f69eeafa9b5
    Reviewed-on: http://gerrit.aldebaran.lan/35597
    Reviewed-by: hcuche <hcuche@aldebaran-robotics.com>
    Tested-by: ncornu <ncornu@aldebaran-robotics.com>

--- python/qi/logging.py
+++ python/qi/logging.py
@@ -35,57 +35,60 @@ def setContext(context):
 def setFilters(filters):
     pysetfilters(filters)
 
+def _printToString(*args):
+    return ' '.join(str(x) for x in args)
+
 class PyLogger:
     def __init__(self, category):
         self.category = category
 
-    def fatal(self, message):
+    def fatal(self, *args):
         info = logGetTraceInfo()
-        pylog(1, self.category, message, info.filename, info.function, info.lineno)
+        pylog(1, self.category, _printToString(*args), info.filename, info.function, info.lineno)
 
-    def error(self, message):
+    def error(self, *args):
         info = logGetTraceInfo()
-        pylog(2, self.category, message, info.filename, info.function, info.lineno)
+        pylog(2, self.category, _printToString(*args), info.filename, info.function, info.lineno)
 
-    def warning(self, message):
+    def warning(self, *args):
         info = logGetTraceInfo()
-        pylog(3, self.category, message, info.filename, info.function, info.lineno)
+        pylog(3, self.category, _printToString(*args), info.filename, info.function, info.lineno)
 
-    def info(self, message):
+    def info(self, *args):
         info = logGetTraceInfo()
-        pylog(4, self.category, message, info.filename, info.function, info.lineno)
+        pylog(4, self.category, _printToString(*args), info.filename, info.function, info.lineno)
 
-    def debug(self, message):
+    def debug(self, *args):
         info = logGetTraceInfo()
-        pylog(5, self.category, message, info.filename, info.function, info.lineno)
+        pylog(5, self.category, _printToString(*args), info.filename, info.function, info.lineno)
 
 def getLogger( name):
     return PyLogger(name)
 
-def logSilent(cat, msg):
+def logSilent(cat, *args):
     info = logGetTraceInfo()
-    pylog(0, cat, msg, info.filename, info.function, info.lineno)
+    pylog(0, cat, _printToString(*args), info.filename, info.function, info.lineno)
 
-def logFatal(cat, msg):
+def logFatal(cat, *args):
     info = logGetTraceInfo()
-    pylog(1, cat, msg, info.filename, info.function, info.lineno)
+    pylog(1, cat, _printToString(*args), info.filename, info.function, info.lineno)
 
-def logError(cat, msg):
+def logError(cat, *args):
     info = logGetTraceInfo()
-    pylog(2, cat, msg, info.filename, info.function, info.lineno)
+    pylog(2, cat, _printToString(*args), info.filename, info.function, info.lineno)
 
-def logWarning(cat, msg):
+def logWarning(cat, *args):
     info = logGetTraceInfo()
-    pylog(3, cat, msg, info.filename, info.function, info.lineno)
+    pylog(3, cat, _printToString(*args), info.filename, info.function, info.lineno)
 
-def logInfo(cat, msg):
+def logInfo(cat, *args):
     info = logGetTraceInfo()
-    pylog(4, cat, msg, info.filename, info.function, info.lineno)
+    pylog(4, cat, _printToString(*args), info.filename, info.function, info.lineno)
 
-def logVerbose(cat, msg):
+def logVerbose(cat, *args):
     info = logGetTraceInfo()
-    pylog(5, cat, msg, info.filename, info.function, info.lineno)
+    pylog(5, cat, _printToString(*args), info.filename, info.function, info.lineno)
 
-def logDebug(cat, msg):
+def logDebug(cat, *args):
     info = logGetTraceInfo()
-    pylog(6, cat, msg, info.filename, info.function, info.lineno)
+    pylog(6, cat, _printToString(*args), info.filename, info.function, info.lineno)
--- python/test/test_log.py
+++ python/test/test_log.py
@@ -11,6 +11,12 @@ def test_directlog():
     qi.logVerbose("test.logger", "log verbose")
     qi.logDebug("test.logger",  "log debug")
 
+    qi.logFatal("test.logger", "log fatal", 1)
+    qi.logError("test.logger", "log error", 1)
+    qi.logWarning("test.logger", "log warning", 1)
+    qi.logInfo("test.logger", "log info", 1)
+    qi.logVerbose("test.logger", "log verbose", 1)
+    qi.logDebug("test.logger",  "log debug", 1)
 
 def test_loggingLevel():
     logger = qi.logging.getLogger("test.logging")
@@ -22,6 +28,12 @@ def test_loggingLevel():
     logger.info("log info")
     logger.debug("log debug")
 
+    logger.fatal("log fatal", 1)
+    logger.error("log error", 1)
+    logger.warning("log warning", 1)
+    logger.info("log info", 1)
+    logger.debug("log debug", 1)
+
 def test_loggingFilters():
     logger = qi.logging.getLogger("test.logging")
     qi.logging.setContext(254)
@@ -31,6 +43,12 @@ def test_loggingFilters():
     logger.warning("log warning")
     logger.info("log info")
     logger.debug("log debug")
+
+    logger.fatal("log fatal", 1)
+    logger.error("log error", 1)
+    logger.warning("log warning", 1)
+    logger.info("log info", 1)
+    logger.debug("log debug", 1)
 
 def main():
     test_directlog()
