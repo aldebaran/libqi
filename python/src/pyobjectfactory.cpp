cf51eea0303e7b7352dd9ebcb542f60b5381d2c6
Herve Cuche <hcuche@aldebaran-robotics.com>
2014-04-25 17:20:06 +0200

    Add createObject with args in Python
    Change-Id: I63384bf9c2c48755051bcbbeb9495982fe2abff3
    Reviewed-on: http://gerrit.aldebaran.lan/39476
    Reviewed-by: pdaouadi <pdaouadi@aldebaran-robotics.com>
    Reviewed-by: hcuche <hcuche@aldebaran-robotics.com>
    Tested-by: hcuche <hcuche@aldebaran-robotics.com>

--- python/src/pyobjectfactory.cpp
+++ python/src/pyobjectfactory.cpp
@@ -11,6 +11,7 @@
 #include <qipython/pyobjectfactory.hpp>
 #include <qipython/pyobject.hpp>
 #include <qipython/error.hpp>
+#include <boost/python/raw_function.hpp>
 
 qiLogCategory("qipy.factory");
 
@@ -19,7 +20,7 @@ namespace qi {
     class PyCreateException : std::exception
     {
     public:
-      PyCreateException(std::string &name) : _name(name) {}
+      PyCreateException(const std::string &name) : _name(name) {}
       virtual ~PyCreateException() throw() {}
       char const * what() const throw() { return std::string(_name + " not found").c_str(); }
 
@@ -30,18 +31,6 @@ namespace qi {
     void translate_pycreateexception(PyCreateException const& e)
     {
       PyErr_SetString(PyExc_RuntimeError, e.what());
-    }
-
-    static boost::python::object pycreate_object(boost::python::str name)
-    {
-      GILScopedLock lock;
-      std::string objectName = boost::python::extract<std::string>(name);
-      qi::AnyObject object = qi::createObject(objectName);
-
-      if(!object)
-        throw PyCreateException(objectName);
-
-      return makePyQiObject(object, objectName);
     }
 
     static qi::AnyObject pyconstruct_object(boost::python::object class_)
@@ -59,10 +48,27 @@ namespace qi {
       qi::registerObjectFactory(name, AnyFunction::from(func));
     }
 
+    static boost::python::object pycreate_object_args(boost::python::tuple pyargs,
+                                                      boost::python::dict kwargs)
+    {
+      int           len        = boost::python::len(pyargs);
+      std::string   objectName = boost::python::extract<std::string>(pyargs[0]);
+      qi::AnyObject object;
+
+      qi::AnyReferenceVector args;
+      for (int i = 1; i < len; ++i)
+        args.push_back(AnyReference::from(boost::python::object(pyargs[i])));
+      object = qi::createObject(objectName, args);
+
+      if(!object)
+        throw PyCreateException(objectName);
+      return makePyQiObject(object, objectName);
+    }
+
     void export_pyobjectfactory()
     {
       boost::python::register_exception_translator<PyCreateException>(&translate_pycreateexception);
-      boost::python::def("createObject", &qi::py::pycreate_object);
+      boost::python::def("createObject", boost::python::raw_function(&pycreate_object_args, 1));
       boost::python::def("registerObjectFactory", &qi::py::pyregister_object_factory);
     }
   }
