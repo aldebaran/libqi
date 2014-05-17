a13c3c597a846b3ce6d3a3bf231bc4a919dfb1bc
Cedric GESTES <gestes@aldebaran-robotics.com>
2014-05-17 18:41:21 +0200

    indent
    Change-Id: Id6ccdc79786cb794b5ced7a9a20d9dcbb8419665
    Reviewed-on: http://gerrit.aldebaran.lan/40145
    Reviewed-by: cgestes <gestes@aldebaran-robotics.com>
    Tested-by: cgestes <gestes@aldebaran-robotics.com>

--- src/messagedispatcher.hpp
+++ src/messagedispatcher.hpp
@@ -38,7 +38,7 @@ namespace qi {
 
     static const unsigned int ALL_OBJECTS;
     qi::SignalLink messagePendingConnect(unsigned int serviceId, unsigned int objectId, boost::function<void (const qi::Message&)> fun);
-    bool                 messagePendingDisconnect(unsigned int serviceId, unsigned int objectId, qi::SignalLink linkId);
+    bool           messagePendingDisconnect(unsigned int serviceId, unsigned int objectId, qi::SignalLink linkId);
 
   public:
     typedef std::pair<unsigned int, unsigned int> Target;
