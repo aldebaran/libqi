3402fc7873138bc28bcdfc43785441bd378846c0
tfontenay <tfontenay@aldebaran-robotics.com>
2015-04-28 11:05:29 +0200

    Set the socket's callback as direct in the server and ROs
    Drop the asynchronicity so we're guaranteed the messages are received
    in the same order they've been sent.
    
    Change-Id: Idd1527cdbbca513d6191ad6c5232dcc37d31afab
    Reviewed-on: http://gerrit.aldebaran.lan/57453
    Tested-by: gerrit
    Reviewed-by: pdaouadi <pdaouadi@aldebaran-robotics.com>
    Approved: tfontenay <tfontenay@aldebaran.com>
    Reviewed-by: tfontenay <tfontenay@aldebaran.com>
    Tested-by: tfontenay <tfontenay@aldebaran.com>

--- src/messaging/server.cpp
+++ src/messaging/server.cpp
@@ -112,11 +112,11 @@ namespace qi {
       SignalSubscriberPtr sub(new SignalSubscriber);
       boost::shared_ptr<bool> first = boost::make_shared<bool>(true);
       // We are reading on the socket for the first time : the first message has to be the capabilities
-      *sub = socket->messageReady.connect(&Server::onMessageReadyNotAuthenticated, this, _1, socket, _authProviderFactory->newProvider(), first, sub);
+      *sub = socket->messageReady.connect(&Server::onMessageReadyNotAuthenticated, this, _1, socket, _authProviderFactory->newProvider(), first, sub).setCallType(MetaCallType_Direct);
       socket->startReading();
     }
     else
-      socket->messageReady.connect(&Server::onMessageReady, this, _1, socket);
+      socket->messageReady.connect(&Server::onMessageReady, this, _1, socket).setCallType(MetaCallType_Direct);
   }
 
   void Server::onMessageReadyNotAuthenticated(const Message &msg, TransportSocketPtr socket, AuthProviderPtr auth,
@@ -154,7 +154,7 @@ namespace qi {
       {
         sendCapabilities(socket);
         qiLogVerbose() << "Authentication is not enforced. Skipping...";
-        socket->messageReady.connect(&Server::onMessageReady, this, _1, socket);
+        socket->messageReady.connect(&Server::onMessageReady, this, _1, socket).setCallType(MetaCallType_Direct);
         onMessageReady(msg, socket);
       }
       return;
@@ -174,7 +174,7 @@ namespace qi {
     case AuthProvider::State_Done:
       qiLogVerbose() << "Client " << socket->remoteEndpoint().str() << " successfully authenticated.";
       socket->messageReady.disconnect(*oldSignal);
-      socket->messageReady.connect(&Server::onMessageReady, this, _1, socket);
+      socket->messageReady.connect(&Server::onMessageReady, this, _1, socket).setCallType(MetaCallType_Direct);
     case AuthProvider::State_Cont:
       if (*first)
       {
