c7d1164c80e27c981e66f133001c3394fb188457
Philippe Daouadi <pdaouadi@aldebaran.com>
2015-05-19 09:39:54 +0200

    FutureBarrier: remove throw on cancel when all futures are finished
    Change-Id: I3b6b700d85973acaaf5e20dabeca3a93310ade53
    Reviewed-on: http://gerrit.aldebaran.lan/58173
    Reviewed-by: pdaouadi <pdaouadi@aldebaran-robotics.com>
    Tested-by: pdaouadi <pdaouadi@aldebaran-robotics.com>

--- qi/detail/futurebarrier.hpp
+++ qi/detail/futurebarrier.hpp
@@ -167,7 +167,8 @@ public:
   FutureBarrier(FutureCallbackType async = FutureCallbackType_Async)
     : _p(boost::make_shared<detail::FutureBarrierPrivate<T> >(async))
   {
-    _p->_promise.setOnCancel(qi::bind<void(qi::Promise<std::vector<qi::Future<T> > >&)>(
+    _p->_promise.setOnCancel(qi::bindWithFallback<void(qi::Promise<std::vector<qi::Future<T> > >&)>(
+          boost::function<void()>(),
           &detail::FutureBarrierPrivate<T>::cancelAll,
           boost::weak_ptr<detail::FutureBarrierPrivate<T> >(_p)));
   }
