441acbe4b789eb05aec06d4f3ee6b46451075db9
Philippe Daouadi <pdaouadi@aldebaran.com>
2015-10-22 18:28:54 +0200

    TestSD: add test for an sd that forwards a service
    Change-Id: I8f566a24a7ae985bf61ff69607079ffb06292651
    Reviewed-on: http://gerrit.aldebaran.lan/63942
    Reviewed-by: vpaleologue <vpaleologue@aldebaran-robotics.com>
    Tested-by: pdaouadi <pdaouadi@aldebaran-robotics.com>

--- tests/messaging/test_sd.cpp
+++ tests/messaging/test_sd.cpp
@@ -55,6 +55,32 @@ TEST(ServiceDirectory, MultiRegister)
   }
 }
 
+TEST(ServiceDirectory, Republish)
+{
+  qi::Session sd1;
+  qi::Session sd2;
+
+  sd1.listenStandalone("tcp://127.0.0.1:0");
+  sd2.listenStandalone("tcp://127.0.0.1:0");
+
+  sd1.registerService("Serv", boost::make_shared<Serv>());
+
+  sd2.registerService("Serv", sd1.service("Serv"));
+
+  ASSERT_EQ(4242, sd1.service("Serv").value().call<int>("f"));
+  ASSERT_EQ(4242, sd2.service("Serv").value().call<int>("f"));
+  {
+    qi::Session client;
+    client.connect(sd1.url());
+    ASSERT_EQ(4242, client.service("Serv").value().call<int>("f"));
+  }
+  {
+    qi::Session client;
+    client.connect(sd2.url());
+    ASSERT_EQ(4242, client.service("Serv").value().call<int>("f"));
+  }
+}
+
 int main(int argc, char **argv) {
   qi::Application app(argc, argv);
   ::testing::InitGoogleTest(&argc, argv);
