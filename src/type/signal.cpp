e5fe3dd319640b26ac4cd0e03611bb249cd7a10f
vpaleologue <vpaleologue@aldebaran.com>
2016-01-27 16:13:05 +0100

    Signal: clearer code in subscriber connection
    "sig" and "sub" looked like hungarian notation.
    
    Change-Id: I3b3278d0f28b2f41c8f53e06f3c353f5e3570335
    Reviewed-on: http://gerrit.aldebaran.lan/67925
    Reviewed-by: jlamotte <jlamotte@aldebaran.com>
    Tested-by: vpaleologue <vpaleologue@aldebaran-robotics.com>

--- src/type/signal.cpp
+++ src/type/signal.cpp
@@ -404,26 +404,26 @@ namespace qi {
       _p = boost::make_shared<SignalBasePrivate>();
     }
     // Check arity. Does not require to acquire weakLock.
-    int sigArity = signature().children().size();
-    int subArity = -1;
-    Signature subSignature = src.signature();
-    if (subSignature.isValid())
-      subArity = subSignature.children().size();
+    int signalArity = signature().children().size();
+    int subscriberArity = -1;
+    Signature subscriberSignature = src.signature();
+    if (subscriberSignature.isValid())
+      subscriberArity = subscriberSignature.children().size();
 
-    if (signature() != "m" && subSignature.isValid())
+    if (signature() != "m" && subscriberSignature.isValid())
     {
-      if (sigArity != subArity)
+      if (signalArity != subscriberArity)
       {
         std::stringstream s;
-        s << "Subscriber has incorrect arity (expected "
-          << sigArity << " , got " << subArity << ")";
+        s << "Subscriber has incorrect arity (expected maximum "
+          << signalArity << " , got " << subscriberArity << ")";
         throw std::runtime_error(s.str());
       }
-      if (!signature().isConvertibleTo(subSignature))
+      if (!signature().isConvertibleTo(subscriberSignature))
       {
         std::stringstream s;
         s << "Subscriber is not compatible to signal : "
-          << signature().toString() << " vs " << subSignature.toString();
+          << signature().toString() << " vs " << subscriberSignature.toString();
         throw std::runtime_error(s.str());
       }
     }
