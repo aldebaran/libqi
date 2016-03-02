5abc3fffb6423c24fda8263ec58ca2c9e5b6604b
vpaleologue <vpaleologue@aldebaran.com>
2016-03-02 14:21:51 +0100

    Url: new, very attractive, functions
    Now you can know which parts of the url are set, and set them separately.
    As to demonstrate such a magnificent feature, the function specifyUrl was
    written to default unspecified parts of a given url.
    
    isValid() had to be changed a little bit so that not to accept the absurd
    case where the host is empty.
    
    Change-Id: I9f455e8174a27004af8248efb3327fe1d3191f1e
    Reviewed-on: http://gerrit.aldebaran.lan/69509
    Reviewed-by: jlamotte <jlamotte@aldebaran.com>
    Tested-by: vpaleologue <vpaleologue@aldebaran-robotics.com>

--- tests/messaging/test_url.cpp
+++ tests/messaging/test_url.cpp
@@ -22,6 +22,10 @@ TEST(TestURL, EmptyUrl)
   EXPECT_EQ(0, url.port());
   EXPECT_EQ("", url.protocol());
   EXPECT_FALSE(url.isValid());
+
+  EXPECT_FALSE(url.hasProtocol());
+  EXPECT_FALSE(url.hasHost());
+  EXPECT_FALSE(url.hasPort());
 }
 
 
@@ -80,7 +84,7 @@ TEST(TestURL, ValidUrl)
   EXPECT_EQ("tcp", url.protocol());
   EXPECT_EQ("", url.host());
   EXPECT_EQ(5, url.port());
-  EXPECT_TRUE(url.isValid());
+  EXPECT_FALSE(url.isValid());
   EXPECT_EQ("tcp://:5", url.str());
 
   url = qi::Url("tcp://example.com", 5);
@@ -171,4 +175,88 @@ TEST(TestURL, TypeSystem)
   args.push_back(qi::AnyReference::from(u));
   std::string res = fin.call(args).to<std::string>();
   EXPECT_EQ(res, u.str());
+}
+
+TEST(TestURL, SetProtocol)
+{
+  qi::Url url;
+  url.setProtocol("tcps");
+  EXPECT_TRUE(url.hasProtocol());
+  EXPECT_FALSE(url.hasHost());
+  EXPECT_FALSE(url.hasPort());
+  EXPECT_FALSE(url.isValid());
+}
+
+TEST(TestURL, SetHost)
+{
+  qi::Url url;
+  url.setHost("0.0.0.0");
+  EXPECT_FALSE(url.hasProtocol());
+  EXPECT_TRUE(url.hasHost());
+  EXPECT_FALSE(url.hasPort());
+  EXPECT_FALSE(url.isValid());
+}
+
+TEST(TestURL, SetPort)
+{
+  qi::Url url;
+  url.setPort(0u);
+  EXPECT_FALSE(url.hasProtocol());
+  EXPECT_FALSE(url.hasHost());
+  EXPECT_TRUE(url.hasPort());
+  EXPECT_FALSE(url.isValid());
+}
+
+TEST(TestURL, SetAll)
+{
+  qi::Url url;
+  url.setProtocol("tcps");
+  url.setHost("0.0.0.0");
+  url.setPort(0u);
+  EXPECT_TRUE(url.hasProtocol());
+  EXPECT_TRUE(url.hasHost());
+  EXPECT_TRUE(url.hasPort());
+  EXPECT_TRUE(url.isValid());
+}
+
+TEST(TestURL, SpecifyUrl)
+{
+  qi::Url specific("0.0.0.0:0");
+  qi::Url base("tcp://127.0.0.1:9559");
+  qi::Url result = qi::specifyUrl(specific, base);
+  EXPECT_TRUE(result.hasProtocol());
+  EXPECT_TRUE(result.hasHost());
+  EXPECT_TRUE(result.hasPort());
+  EXPECT_TRUE(result.isValid());
+  EXPECT_EQ(base.protocol(), result.protocol());
+  EXPECT_EQ(specific.host(), result.host());
+  EXPECT_EQ(specific.port(), result.port());
+}
+
+TEST(TestURL, SpecifyUrlWithIncompleteBase)
+{
+  qi::Url specific("0.0.0.0:0");
+  qi::Url base("127.0.0.1:9559");
+  qi::Url result = qi::specifyUrl(specific, base);
+  EXPECT_FALSE(result.hasProtocol());
+  EXPECT_TRUE(result.hasHost());
+  EXPECT_TRUE(result.hasPort());
+  EXPECT_FALSE(result.isValid());
+  EXPECT_EQ(base.protocol(), result.protocol());
+  EXPECT_EQ(specific.host(), result.host());
+  EXPECT_EQ(specific.port(), result.port());
+}
+
+TEST(TestURL, SpecifyUrlWithEmptyDefault)
+{
+  qi::Url specific("tcp://127.0.0.1:9559");
+  qi::Url base;
+  qi::Url result = qi::specifyUrl(specific, base);
+  EXPECT_TRUE(result.hasProtocol());
+  EXPECT_TRUE(result.hasHost());
+  EXPECT_TRUE(result.hasPort());
+  EXPECT_TRUE(result.isValid());
+  EXPECT_EQ(specific.protocol(), result.protocol());
+  EXPECT_EQ(specific.host(), result.host());
+  EXPECT_EQ(specific.port(), result.port());
 }
