0d5cc7345ae3bf5fbbf1ede7a4f8b1cd96b16596
vpaleologue <vpaleologue@aldebaran.com>
2016-09-09 10:05:58 +0200

    Service Directory: new tests demonstrate some properties
    Change-Id: Iee69b920a8624fee76068a420b5750e21654e7c1
    Reviewed-on: http://gerrit.aldebaran.lan/75102
    Reviewed-by: jlamotte <jlamotte@aldebaran.com>
    Reviewed-by: jmonnon <jmonnon@aldebaran.com>
    Reviewed-by: vpaleologue <vpaleologue@aldebaran-robotics.com>
    Tested-by: vpaleologue <vpaleologue@aldebaran-robotics.com>

--- tests/messaging/test_sd.cpp
+++ tests/messaging/test_sd.cpp
@@ -81,10 +81,56 @@ TEST(ServiceDirectory, Republish)
   }
 }
 
+TEST(ServiceDirectory, ServiceEndpointMatchesServiceDirectorys)
+{
+  auto session = qi::makeSession();
+  session->listenStandalone("tcp://127.0.0.1:0");
+  session->registerService("Service", boost::make_shared<Serv>());
+  auto serviceInfo = session->services().value().back();
+  ASSERT_EQ(session->endpoints(), serviceInfo.endpoints());
+}
+
+TEST(ServiceDirectory, ReRegisterRemoteServiceRenewEndpoints)
+{
+  auto session1 = qi::makeSession();
+  auto session2 = qi::makeSession();
+  session1->listenStandalone("tcp://127.0.0.1:0");
+  session2->listenStandalone("tcp://127.0.0.1:0");
+  session1->registerService("Service", boost::make_shared<Serv>());
+  auto serviceInfo1 = session1->services().value().back();
+  auto remoteService = session1->service("Service").value();
+  session2->registerService("Service", remoteService);
+  auto serviceInfo2 = session2->services().value().back();
+  ASSERT_NE(serviceInfo1.endpoints(), serviceInfo2.endpoints());
+  ASSERT_NE(session1->endpoints(), serviceInfo2.endpoints());
+  ASSERT_EQ(session2->endpoints(), serviceInfo2.endpoints());
+}
+
+TEST(ServiceDirectory, CallMessagesAreProperlyDispatched)
+{
+  auto session1 = qi::makeSession();
+  auto session2 = qi::makeSession();
+  session1->listenStandalone("tcp://127.0.0.1:0");
+  session2->listenStandalone("tcp://127.0.0.1:0");
+
+  // Dummy services are registered to offset the indexes, so that they
+  // cannot match by chance between the two service directories
+  auto originalService = boost::make_shared<Serv>();
+  session2->registerService("A", boost::make_shared<Serv>());
+  session2->registerService("B", boost::make_shared<Serv>());
+  session2->registerService("C", boost::make_shared<Serv>());
+  session1->registerService("Service", originalService);
+
+  auto remoteService = session1->service("Service").value();
+  session2->registerService("Service", remoteService);
+
+  auto remoteRemoteService = session2->service("Service").value();
+  ASSERT_EQ(4242, remoteRemoteService.call<int>("f"));
+}
+
 int main(int argc, char **argv) {
   qi::Application app(argc, argv);
   ::testing::InitGoogleTest(&argc, argv);
-
   return RUN_ALL_TESTS();
 }
 
