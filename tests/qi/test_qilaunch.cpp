96322cac94178ef5159a71c6d6e9f0296ccef42a
vpaleologue <vpaleologue@aldebaran.com>
2016-09-19 09:53:03 +0200

    Tests: move out the ScopedProcess class, so that it can be reused
    Change-Id: Iba67581c15187a362e3a683e10e6aa5d476df567
    Reviewed-on: http://gerrit.aldebaran.lan/75409
    Reviewed-by: jmonnon <jmonnon@aldebaran.com>
    Reviewed-by: jlamotte <jlamotte@aldebaran.com>
    Tested-by: vpaleologue <vpaleologue@aldebaran-robotics.com>

--- tests/qi/test_qilaunch.cpp
+++ tests/qi/test_qilaunch.cpp
@@ -6,6 +6,7 @@
 #include <qi/log.hpp>
 #include <qi/os.hpp>
 #include <qi/path.hpp>
+#include <qi/testutils/testutils.hpp>
 
 #include <errno.h>
 
@@ -288,44 +289,6 @@ int randomWrongPid()
 {
   return randInt(100000, std::numeric_limits<int>::max());
 }
-
-class ScopedProcess
-{
-public:
-  ScopedProcess(
-      const std::string& executable,
-      const std::vector<std::string>& arguments = std::vector<std::string>())
-    : _executable(executable)
-  {
-    char** cArgs = new char*[arguments.size()+2];
-    cArgs[0] = (char*)_executable.c_str();
-    for(size_t i = 0; i < arguments.size(); ++i)
-      cArgs[i+1] = (char*)arguments[i].c_str();
-    cArgs[arguments.size()+1] = NULL;
-    _pid = qi::os::spawnvp(cArgs);
-    if (_pid <= 0)
-      throw std::runtime_error(
-          std::string("Could not start: ") + _executable);
-  }
-
-  ~ScopedProcess()
-  {
-    qi::os::kill(_pid, SIGKILL);
-    int status = 0;
-    int ret = qi::os::waitpid(_pid, &status);
-    std::cout << "Waiting for " << _executable << " has yielded the status "
-                << status << " and returned " << ret << std::endl;
-  }
-
-  int pid() const
-  {
-    return _pid;
-  }
-
-private:
-  std::string _executable;
-  int _pid;
-};
 
 void waitForProcessInfoReady()
 {
