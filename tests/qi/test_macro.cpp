d4dd588141462c92dd2fdfd1fc63809ce6327390
Joël LAMOTTE <jlamotte@aldebaran.com>
2016-11-28 15:09:49 +0100

    Fixed issues with QI_THREAD_SAFE_NEW tests.
     - removed unused code;
     - added deletion of static new-ed objects, to avoid false-positive
       leak reports;
     - added clarification of why this test cannot be launched in repeat
       mode;
     - fixed thread usage being random: our boost.thread version v2 will
       detach the thread on destruction, which made the rest of the
       test have undefined behaviour.
       Now (using std::thread) we guarantee that all the threads are joined
       before looking at the shared counters.
    
    Change-Id: I81c9470d1297417371bbc8a0ede6d387bd944805
    Reviewed-on: http://gerrit.aldebaran.lan/77733
    Reviewed-by: vpaleologue <vpaleologue@aldebaran-robotics.com>
    Reviewed-by: jlamotte <jlamotte@aldebaran.com>
    Tested-by: jlamotte <jlamotte@aldebaran.com>

--- tests/qi/test_macro.cpp
+++ tests/qi/test_macro.cpp
@@ -4,6 +4,7 @@
  * found in the COPYING file.
  */
 
+#include <thread>
 #include <gtest/gtest.h>
 
 #include <boost/noncopyable.hpp>
@@ -14,31 +15,16 @@
 #include <qi/os.hpp>
 #include <qi/preproc.hpp>
 
-class CloneOk
-{
-public:
-  int i;
-};
+/* IMPORTANT NOTE:
+   The tests related to QI_THREAD_SAFE_NEW macro will leak and cannot
+   work in "repeat" test mode because this macro generate static markers
+   to know if the 'new' call have already been done since the start
+   of the current process.
+   We still tried to avoid false-positive leak detection by
+   deleting the static objects.
+*/
 
-class CloneKo : private boost::noncopyable
-{
-public:
-  int i;
-};
-
-class CloneKo2: boost::noncopyable
-{
-  public:
-  int i;
-};
-class CloneKo3: public boost::noncopyable
-{
-  public:
-  int i;
-};
-
-
-qi::Atomic<int> counter;
+std::atomic<int> counter{0};
 
 class CCount
 {
@@ -55,78 +41,140 @@ public:
 template<int i> class StaticCounter1
 {
 public:
+  static CCount* cc1;
   static void init()
   {
-    static CCount* cc1 = nullptr;
     QI_THREADSAFE_NEW(cc1);
   }
+
+  static void clear()
+  {
+    delete cc1;
+    cc1 = nullptr;
+  }
 };
-template<int i> inline
-void staticCounter2()
+
+template<int i> CCount* StaticCounter1<i>::cc1 = nullptr;
+
+template<int i>
+struct staticCounter2
 {
-  static CCount* cc1=nullptr, *cc2=nullptr;
-  QI_THREADSAFE_NEW(cc1, cc2);
+  static CCount* cc1;
+  static CCount* cc2;
+
+  void operator()()
+  {
+    QI_THREADSAFE_NEW(cc1, cc2);
+  }
+
+  static void clear()
+  {
+    delete cc1;
+    cc1 = nullptr;
+
+    delete cc2;
+    cc2 = nullptr;
+  }
+
+};
+
+template<int i> CCount* staticCounter2<i>::cc1 = nullptr;
+template<int i> CCount* staticCounter2<i>::cc2 = nullptr;
+
+template<int k>
+struct staticCounterControlled
+{
+  static CCount* cc1;
+
+  int operator()(std::atomic<int>& start, int count, std::atomic<int>& delay, int delayDelta)
+  {
+    int res = k;
+    // mark thread started
+    ++start;
+    // wait for other threads to start
+    while (start.load() != count)
+      ;
+    // delay as asked
+    delay += delayDelta;
+    const int myDelay = delay;
+    for (int i = 0; i < myDelay; ++i)
+      res = res + i;
+    // GO!
+    QI_THREADSAFE_NEW(cc1);
+    return res;
+  }
+
+  static void clear()
+  {
+    delete cc1;
+    cc1 = nullptr;
+  }
+};
+
+template<int k> CCount* staticCounterControlled<k>::cc1 = nullptr;
+
+template<class Callable, class... Args>
+void runThreads(int threadCount, Callable&& callable, Args&&... args)
+{
+  std::vector<std::thread> threads;
+
+  for (int i = 0; i < threadCount; ++i)
+    threads.emplace_back(std::forward<Callable>(callable), std::forward<Args>(args)...);
+
+  for (auto& thread : threads)
+    thread.join();
 }
 
-template<int k> inline
-int staticCounterControlled(qi::Atomic<int>* start, int count, int delay)
-{
-  static CCount* cc1 = nullptr;
-  int res = k;
-  // mark thread started
-  ++(*start);
-  // wait for other threads to start
-  while (start->load() != count)
-    ;
-  // delay as asked
-  for (int i=0; i<delay; ++i)
-    res = res + i;
-  // GO!
-  QI_THREADSAFE_NEW(cc1);
-  return res;
-}
-
-#define EXPECTCOUNTER(v)                                 \
+#define EXPECTCOUNTER(v)                                  \
 qi::os::msleep(1);                                        \
   for (unsigned t=0; t<200 && counter.load() - prev != v; ++t)  \
-    qi::os::msleep(5);                                   \
-  EXPECT_EQ(v, counter.load() - prev);                         \
+    qi::os::msleep(5);                                    \
+  EXPECT_EQ(v, counter.load() - prev);                    \
   prev = counter.load()
 
 
 TEST(Macro, ThreadSafeNew)
 {
+  counter = 0;
   int prev = 0;
+
   EXPECTCOUNTER(0); // check the macro just in case
-  for (unsigned i=0; i<10; ++i)
-    boost::thread t(boost::bind(&staticCounter2<0>));
+  runThreads(10, staticCounter2<0>{});
   EXPECTCOUNTER(2);
+  staticCounter2<0>::clear();
+
   EXPECTCOUNTER(0);
-  for (unsigned i=0; i<10; ++i)
-    boost::thread t(&StaticCounter1<1>::init);
+  runThreads(10, &StaticCounter1<1>::init);
   EXPECTCOUNTER(1);
-  for (unsigned i=0; i<2; ++i)
-    boost::thread t(&StaticCounter1<2>::init);
+  StaticCounter1<1>::clear();
+
+  runThreads(2, &StaticCounter1<2>::init);
   EXPECTCOUNTER(1);
-  for (unsigned i=0; i<3; ++i)
-    boost::thread t(&StaticCounter1<3>::init);
+  StaticCounter1<2>::clear();
+
+  runThreads(3, &StaticCounter1<3>::init);
   EXPECTCOUNTER(1);
-  for (unsigned i=0; i<4; ++i)
-    boost::thread t(&StaticCounter1<4>::init);
+  StaticCounter1<3>::clear();
+
+  runThreads(4, &StaticCounter1<4>::init);
   EXPECTCOUNTER(1);
-  for (unsigned i=0; i<5; ++i)
-    boost::thread t(&StaticCounter1<5>::init);
+  StaticCounter1<4>::clear();
+
+  runThreads(5, &StaticCounter1<5>::init);
   EXPECTCOUNTER(1);
-  for (unsigned i=0; i<6; ++i)
-    boost::thread t(&StaticCounter1<6>::init);
+  StaticCounter1<5>::clear();
+
+  runThreads(6, &StaticCounter1<6>::init);
   EXPECTCOUNTER(1);
+  StaticCounter1<6>::clear();
 
 #define DOOM(nthread, delay0, delayDelta)                                           \
-  {                                                                                           \
-    qi::Atomic<int> a;                                                                        \
-    for (int i=0; i<nthread; ++i)                                                        \
-      boost::thread t(&staticCounterControlled<__LINE__>, &a, nthread, delay0 + i*delayDelta);  \
-    EXPECTCOUNTER(1);                                                                         \
+  {                                                                                \
+    std::atomic<int> a {0};                                                         \
+    std::atomic<int> delay{ delay0 };                                               \
+    runThreads(nthread, staticCounterControlled<__LINE__>{}, std::ref(a), nthread, std::ref(delay), delayDelta);   \
+    EXPECTCOUNTER(1);                                                              \
+    staticCounterControlled<__LINE__>::clear();                                     \
   }
   // Do not put multiple dooms on the same line or it will not work.
   DOOM(1, 0, 0);
