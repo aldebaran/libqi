747c65a59d7aa748525f6f55e6dd1334b4452dae
Jeremy Monnon <jmonnon@aldebaran.com>
2017-04-24 14:11:28 +0200

    Functional: Adds an binary operation to compose functions.
    This is a fundamental tool to facilitate function manipulation and thus to make
    the use of the functional paradigm easier.
    This in turn is important for concurrent code.
    
    Change-Id: Ib44f6fa6fa56ed46e2bb63ba0a820c0fafe96831
    Reviewed-on: http://gerrit.aldebaran.lan/82308
    Reviewed-by: jmonnon <jmonnon@aldebaran.com>
    Reviewed-by: jlamotte <jlamotte@aldebaran.com>
    Tested-by: jmonnon <jmonnon@aldebaran.com>

--- qi/functional.hpp
+++ qi/functional.hpp
@@ -79,6 +79,157 @@ namespace qi {
     }
   };
 
+// Mainly useful because of the redundancy of trailing return types.
+//
+// TODO: Remove this when get rid of C++11.
+#define QI_FWD_ARGS std::forward<Args>(args)...
+
+  namespace detail
+  {
+    template<typename Ret>
+    struct Composition
+    {
+      /// TODO: Remove the trailing return type when get rid of C++11.
+      template<typename G, typename F, typename... Args>
+      auto operator()(G& g, F& f, Args&&... args) -> decltype(g(f(QI_FWD_ARGS)))
+      {
+        return g(f(QI_FWD_ARGS));
+      }
+    };
+
+    template<>
+    struct Composition<void>
+    {
+      /// TODO: Remove the trailing return type when get rid of C++11.
+      template<typename G, typename F, typename... Args>
+      auto operator()(G& g, F& f, Args&&... args) -> decltype(g())
+      {
+        return f(QI_FWD_ARGS), g();
+      }
+    };
+  } // namespace detail
+
+  /// Composes two procedures, handling the void case.
+  ///
+  /// In C++, `g(f(x))` is not possible if `f` returns `void` and `g` takes `void`
+  /// (no argument). This case is handled as `f(x), g()`.
+  ///
+  /// Procedure<C (B)> G, Procedure<B (A...)> F
+  template<typename G, typename F>
+  struct Composition
+  {
+    G g;
+    F f;
+  // Regular (if G and F are):
+    QI_GENERATE_FRIEND_REGULAR_OPS_2(Composition, g, f)
+  // Procedure<C (A...)>:
+    /// TODO: Remove the trailing return type when get rid of C++11.
+    template<typename... Args>
+    auto operator()(Args&&... args)
+      -> decltype(detail::Composition<decltype(f(QI_FWD_ARGS))>{}(g, f, QI_FWD_ARGS))
+    {
+      return detail::Composition<decltype(f(QI_FWD_ARGS))>{}(g, f, QI_FWD_ARGS);
+    }
+  };
+
+  /// Performs a mathematical function composition.
+  ///
+  /// Example: Composing with a non-void function
+  /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  /// // `motor` takes a `string` and returns the properties of a motor.
+  /// // `heat` takes the properties of a motor and returns an `int`.
+  /// // `motorNames` is a container of string.
+  /// auto heatOfMotor = compose(heat, motor);
+  /// auto maxHeat = std::max_element(motorNames.begin(), motorNames.end(), heatOfMotor);
+  /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  ///
+  /// Example: Composing with a void function
+  /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  /// // `logName` and `logHeat` take no input (void) and returns no ouput (void).
+  /// auto logNameThenHeat = compose(logHeat, logName);
+  /// auto fut = strand.async(logNameThenHeat);
+  /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  ///
+  /// Remark: In the future, add a SFINAE guard if there is a name clash.
+  ///   Alternatively, rename this function in `funCompose`.
+  ///
+  /// Procedure<C (B)> G,
+  /// Procedure<B (Args...)> F
+  template<typename G, typename F>
+  Composition<traits::Decay<G>, traits::Decay<F>> compose(G&& g, F&& f)
+  {
+    return {std::forward<G>(g), std::forward<F>(f)};
+  }
+#undef QI_FWD_ARGS
+
+  /// Function that transform the codomain (return type) of the given procedure
+  /// into an "enriched" type.
+  ///
+  /// This is related to monad theory (see below).
+  ///
+  /// Example: Transforming a procedure of type (A -> B) in a procedure of type (A -> Future<B>):
+  /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  /// // `proc` takes an `int` and returns a `bool`
+  /// auto f = semiLift(UnitFuture{}, proc);
+  /// Future<bool> fut = f(i);
+  /// // ...
+  /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  ///
+  /// # Background on monads
+  ///
+  /// A monad can be viewed as an "enriched" type together with functions to go
+  /// from and to the corresponding "base" type.
+  /// In the above example, `int` is the base type and `Future<int>` is the
+  /// enriched, monadic, type.
+  ///
+  /// To lift is to map a function on base types to a function on the
+  /// corresponding enriched types:
+  /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  /// enriched "level":  X<int> ---> X<bool>
+  ///                            ^
+  ///                            | lift
+  ///                            |
+  /// base "level":      int    ---> bool
+  /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  /// In the above case `X` is `Future`.
+  ///
+  /// Other examples for X are: std::optional, std::vector, etc.
+  ///
+  /// Generally speaking, the enriched type (e.g. X<int>) gives access to a value
+  /// of an underlying type (e.g. int). Thus, it can be viewed as a "container".
+  ///
+  /// In our case, we have:
+  /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  /// enriched "level":       X<bool>
+  ///                         ^    ^
+  ///   semiLift(proc, unit) /     | unit
+  ///                       /      |
+  /// base "level":      int ---> bool
+  ///                        proc
+  /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  /// This diagram commutes: all paths are equivalent.
+  ///
+  /// That is we have, for all x:
+  /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  /// semiLift(proc, unit)(x) == (unit . proc)(x)
+  /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  /// where `.` stands for the function composition (see `compose`).
+  ///
+  /// `unit` is a function that sends a "base" value to an "enriched", monadic value.
+  /// Its name comes from the fact that it is a "unit" or "neutral element" for
+  /// monadic composition.
+  ///
+  /// This function is named `semiLift` because it partially "lifts" the procedure:
+  /// only the codomain is enriched.
+  ///
+  /// Procedure Proc
+  template<typename Proc, typename F>
+  auto semiLift(Proc&& p, F&& unit)
+    -> decltype(compose(std::forward<F>(unit), std::forward<Proc>(p)))
+  {
+    return compose(std::forward<F>(unit), std::forward<Proc>(p));
+  }
+
   /// Bind a data to a procedure without changing its semantics.
   /// One use is to bind a shared pointer to extend the lifetime of some data
   /// related to the procedure.
--- tests/qi/test_functional.cpp
+++ tests/qi/test_functional.cpp
@@ -1,9 +1,13 @@
 #include <vector>
+#include <limits>
+#include <cstdlib>
 #include <qi/application.hpp>
 #include <gtest/gtest.h>
 #include <qi/functional.hpp>
 #include <qi/detail/conceptpredicate.hpp>
 #include <qi/range.hpp>
+#include <qi/future.hpp>
+#include <qi/type/traits.hpp>
 
 TEST(FunctionalPolymorphicConstantFunction, RegularNonVoid)
 {
@@ -75,4 +79,271 @@ TEST(FunctionalPolymorphicConstantFunction, BasicVoid)
   ASSERT_NO_THROW(f(true));
   ASSERT_NO_THROW(f(std::vector<int>{5, 7, 2, 1}));
   ASSERT_NO_THROW(f(1, 2.345, "abcd", true));
+}
+
+namespace
+{
+  // For use to test isRegular only.
+  // The returned strings are irrelevant, the only point is that these functions
+  // are regular.
+  std::string strbool0(bool x)
+  {
+    return x ? "test test" : "1, 2, 1, 2";
+  }
+
+  std::string strbool1(bool x)
+  {
+    return x ? "mic mic" : "Vous etes chauds ce soir?!";
+  }
+}
+
+TEST(FunctionalCompose, Regular)
+{
+  using namespace qi;
+  using namespace std;
+  using C = Composition<string (*)(bool), bool (*)(float)>;
+  ASSERT_TRUE(detail::isRegular({
+    C{strbool0, isnan}, C{strbool0, isfinite}, C{strbool1, isinf}
+  }));
+}
+
+TEST(FunctionalCompose, NonVoid)
+{
+  using namespace qi;
+
+  auto half = [](int x) {
+    return x / 2.f;
+  };
+  auto greater_1 = [](float x) {
+    return x > 1.f;
+  };
+  auto half_greater_1 = compose(greater_1, half);
+  static_assert(traits::Equal<bool, decltype(half_greater_1(3))>::value, "");
+
+  ASSERT_TRUE(half_greater_1(3));
+  ASSERT_FALSE(half_greater_1(1));
+}
+
+TEST(FunctionalCompose, Void)
+{
+  using namespace qi;
+  using namespace qi::traits;
+  const int uninitialized = std::numeric_limits<int>::max();
+  int order = 0;
+  int fOrder = uninitialized;
+  int gOrder = uninitialized;
+  auto f = [&](int) {
+    fOrder = order++;
+  };
+  auto g = [&] {
+    gOrder = order++;
+  };
+  auto k = compose(g, f);
+  static_assert(Equal<void, decltype(k(3))>::value, "");
+
+  ASSERT_EQ(uninitialized, fOrder);
+  ASSERT_EQ(uninitialized, gOrder);
+  k(3);
+  ASSERT_EQ(0, fOrder);
+  ASSERT_EQ(1, gOrder);
+}
+
+TEST(FunctionalCompose, Multi)
+{
+  using namespace qi;
+  using namespace qi::traits;
+  using std::string;
+
+  auto half = [](int x) {
+    return x / 2.f;
+  };
+  auto greater_1 = [](float x) {
+    return x > 1.f;
+  };
+  auto str = [](bool x) -> string {
+    return x ? "true" : "false";
+  };
+
+  auto f = compose(str, compose(greater_1, half));
+  static_assert(Equal<string, decltype(f(3))>::value, "");
+
+  ASSERT_EQ("true", f(3));
+  ASSERT_EQ("false", f(1));
+}
+
+namespace
+{
+  template<typename G, typename F>
+  qi::Composition<qi::traits::Decay<G>, qi::traits::Decay<F>> operator*(G&& g, F&& f)
+  {
+    return {g, f};
+  }
+} // namespace
+
+TEST(FunctionalCompose, Associative)
+{
+  using namespace qi;
+  using std::string;
+
+  auto f = [](int x) {
+    return x / 2.f;
+  };
+  auto g = [](float x) {
+    return x > 1.f;
+  };
+  auto h = [](bool x) -> string {
+    return x ? "true" : "false";
+  };
+  auto i = [](const string& x) -> std::size_t {
+    return x.size();
+  };
+
+  auto a = ((i * h) * g) * f;
+  auto b = (i * (h * g)) * f;
+  auto c = i * (h * (g * f));
+  auto d = (i * h) * (g * f);
+  auto e = i * ((h * g) * f);
+
+  ASSERT_EQ(a(3), b(3));
+  ASSERT_EQ(b(3), c(3));
+  ASSERT_EQ(c(3), d(3));
+  ASSERT_EQ(d(3), e(3));
+
+  ASSERT_EQ(a(0), b(0));
+  ASSERT_EQ(b(0), c(0));
+  ASSERT_EQ(c(0), d(0));
+  ASSERT_EQ(d(0), e(0));
+}
+
+TEST(FunctionalCompose, Id)
+{
+  using namespace qi;
+  using std::string;
+
+  auto f = [](int x) {
+    return x / 2.f;
+  };
+  auto g = [](float x) {
+    return x > 1.f;
+  };
+  IdTransfo id;
+
+  auto a = f * id;
+  auto b = id * f;
+  auto c = (g * f) * id;
+  auto d = id * (g * f);
+
+  ASSERT_EQ(a(3), b(3));
+  ASSERT_EQ(c(3), d(3));
+
+  ASSERT_EQ(a(0), b(0));
+  ASSERT_EQ(c(0), d(0));
+}
+
+namespace
+{
+  struct X {
+    bool b;
+    bool operator==(X x) const {return b == x.b;}
+  };
+
+  template<typename T>
+  struct ConstantUnit
+  {
+    T operator()() const
+    {
+      return T{};
+    }
+  };
+
+  template<>
+  struct ConstantUnit<qi::Future<void>>
+  {
+    qi::Future<void> operator()() const
+    {
+      return qi::Future<void>{nullptr};
+    }
+  };
+
+  template<typename T>
+  bool equal(const T& a, const T& b)
+  {
+    return a == b;
+  }
+
+  template<typename T>
+  bool equal(const qi::Future<T>& a, const qi::Future<T>& b)
+  {
+    return a.value() == b.value();
+  }
+}
+
+using types = testing::Types<
+  X, qi::Future<bool>, boost::optional<bool>, std::list<bool>
+>;
+
+template<typename T>
+struct FunctionalSemiLift0 : testing::Test
+{
+};
+
+TYPED_TEST_CASE(FunctionalSemiLift0, types);
+
+TYPED_TEST(FunctionalSemiLift0, NonVoidCodomain)
+{
+  using namespace qi;
+  using T = TypeParam;
+
+  auto positive = [](int i) {
+    return i > 0;
+  };
+  auto unit = [](bool b) {
+    return T{b};
+  };
+  auto f = semiLift(positive, unit);
+
+  static_assert(traits::Equal<T, decltype(f(0))>::value, "");
+  ASSERT_TRUE(equal(T{true}, f(1)));
+  ASSERT_TRUE(equal(T{false}, f(-1)));
+}
+
+using void_types = testing::Types<
+  X, qi::Future<void>, boost::optional<bool>, std::list<bool>
+>;
+
+template<typename T>
+struct FunctionalSemiLift1 : testing::Test
+{
+};
+
+TYPED_TEST_CASE(FunctionalSemiLift1, void_types);
+
+TYPED_TEST(FunctionalSemiLift1, VoidCodomain)
+{
+  using namespace qi;
+  using namespace qi::traits;
+  using T = TypeParam;
+
+  auto noop = [](int) {
+  };
+  ConstantUnit<T> unit;
+  auto f = semiLift(noop, unit);
+
+  static_assert(Equal<T, decltype(f(0))>::value, "");
+  ASSERT_TRUE(equal(unit(), f(0)));
+}
+
+TYPED_TEST(FunctionalSemiLift1, VoidCodomainVoidDomain)
+{
+  using namespace qi;
+  using namespace qi::traits;
+  using T = TypeParam;
+
+  auto noop = [] {
+  };
+  ConstantUnit<T> unit;
+  auto f = semiLift(noop, unit);
+
+  static_assert(Equal<T, decltype(f())>::value, "");
+  ASSERT_TRUE(equal(unit(), f()));
 }
