aldebaran/libqi

commit ca9c664b53381c3d5321db2743b2c1847b30ea06

socket: Fixes minor things.
- Compilation problem on Mac.
- Better comment.
- CppCheck warning deactivation.

Change-Id: Ia7509d0d341b8d67f9d7fa36c3fc477ac8cd95ad
Reviewed-on: http://gerrit.aldebaran.lan/81449
Reviewed-by: jlamotte <jlamotte@aldebaran.com>
Reviewed-by: jmonnon <jmonnon@aldebaran.com>
Tested-by: jmonnon <jmonnon@aldebaran.com>

src/messaging/tcpmessagesocket.hpp
@@ -512,6 +512,8 @@ namespace qi {
       }
     };
 
+    const int defaultTimeoutInSeconds = 0;
+
   } // namespace net
 
   /// A socket to send and receive messages.
@@ -728,7 +730,6 @@ namespace qi {
     net::SslContext<N> _sslContext;
     mutable boost::recursive_mutex _stateMutex;
     net::IoService<N>& _ioService;
-    static const int defaultTimeoutInSeconds;
 
     void enterDisconnectedState(const SocketPtr& socket = {},
       Promise<void> promiseDisconnected = Promise<void>{});
@@ -776,9 +777,6 @@ namespace qi {
     FutureSync<void> doDisconnect();
   };
 
-  template<typename N>
-  const int TcpMessageSocket<N>::defaultTimeoutInSeconds = 30;
-
   using TcpMessageSocketPtr = boost::shared_ptr<TcpMessageSocket<net::NetworkAsio>>;
 
   template<typename N>
@@ -792,7 +790,7 @@ namespace qi {
   {
     if (socket)
     {
-      net::setSocketOptions<N>(*socket, getTcpPingTimeout(Seconds{defaultTimeoutInSeconds}));
+      net::setSocketOptions<N>(*socket, getTcpPingTimeout(Seconds{net::defaultTimeoutInSeconds}));
       _state = ConnectingState{io, ssl, socket, Handshake::server};
     }
   }
@@ -865,7 +863,7 @@ namespace qi {
     // This changes the status so that concurrent calls will return in error.
     using Side = net::HandshakeSide<net::SslSocket<N>>;
     _state = ConnectingState{_ioService, url, _ssl, _sslContext, disableIpV6, Side::client,
-      getTcpPingTimeout(Seconds{defaultTimeoutInSeconds})};
+      getTcpPingTimeout(Seconds{net::defaultTimeoutInSeconds})};
     _url = url;
     auto self = shared_from_this();
 

qi/functional.hpp
@@ -53,10 +53,16 @@ namespace qi {
   /// One use is to bind a shared pointer to extend the lifetime of some data
   /// related to the procedure.
   ///
-  /// This type is useful equivalent to a variadic generic lambda in C++14 ():
-  /// auto lambda = [=](auto&&... args) {
+  /// This type is useful equivalent to a variadic generic lambda in C++14:
+  /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  /// T data;
+  /// auto dataBoundProc = [data](auto&&... args) {
   ///   // ...
   /// };
+  /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  ///
+  /// Warning: This type is not strictly equivalent to a lambda (C++14 or even C++17),
+  ///   as it is regular and lambdas are not.
   ///
   /// Procedure Proc
   template<typename Proc, typename T>

tests/messaging/net/test_connect.cpp
@@ -184,6 +184,7 @@ TYPED_TEST(NetConnectFuture, ResolveCalledAfterParentHasBeenDestroyed)
     p = &connect;
   }
   // The connecting object is now destroyed and on top of that we wipe out its memory.
+  // cppcheck-suppress deadpointer
   overwrite(p);
   // Now we unblock the resolve handler.
   nukeObject.setValue(0);
@@ -396,6 +397,7 @@ TYPED_TEST(NetConnectFuture, HandshakeHandlerCalledAfterParentHasBeenDestroyed)
     p = &connect;
   }
   // The connecting object is now destroyed and on top of that we wipe out its memory.
+  // cppcheck-suppress deadpointer
   overwrite(p);
   // Now we unblock the handler.
   nukeObject.setValue(0);

tests/messaging/net/test_receive.cpp
@@ -146,6 +146,7 @@ namespace mock
       p = &receive;
     }
     // The connecting object is now destroyed and on top of that we wipe out its memory.
+    // cppcheck-suppress deadpointer
     overwrite(p);
     // Now we unblock the handler.
     nukingObject.setValue(0);
@@ -218,6 +219,7 @@ namespace mock
       p = &receive;
     }
     // The connecting object is now destroyed and on top of that we wipe out its memory.
+    // cppcheck-suppress deadpointer
     overwrite(p);
     // Now we unblock the handler.
     nukingObject.setValue(0);

tests/messaging/test_tcpmessagesocket.cpp
@@ -6,10 +6,6 @@
 #include <boost/asio.hpp>
 #include <boost/asio/ssl.hpp>
 #include <qi/messaging/net/accept.hpp>
-int scopelock(const boost::asio::ssl::stream<boost::asio::ip::tcp::socket>&)
-{
-  return 0;
-}
 #include "src/messaging/tcpmessagesocket.hpp"
 #include "src/messaging/transportserver.hpp"
 

commit c15b5b04beb111fa3abf03039cb6006d9c4f8b8e

TcpMessageSocket: Set default timeout to 30sec #38425
It was this way before.
Broken in commit ca9c664b53381c3d5321db2743b2c1847b30ea06

Change-Id: I1433af25afbf2e605e2d02f95db19eaf295a61f4
Reviewed-on: http://gerrit.aldebaran.lan/82767
Reviewed-by: jmonnon <jmonnon@aldebaran.com>
Tested-by: gvallat <gvallat@aldebaran-robotics.com>

src/messaging/tcpmessagesocket.hpp
@@ -512,7 +512,7 @@ namespace qi {
       }
     };
 
-    const int defaultTimeoutInSeconds = 0;
+    const int defaultTimeoutInSeconds = 30;
 
   } // namespace net
 