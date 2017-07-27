376ab9ffe08b55507f6e07299db18f5375bd2d4d
Sébastien Barthélémy <sbarthelemy@aldebaran-robotics.com>
2017-07-27 14:16:40 +0200

    Future: drop onCancel callback after the first call
    This is a user-visible change: the callback will only be called
    once, while previously, several calls could have occurred.
    
    Yet, this behaviour is not documented, I suspect changing it is ok.
    
    With the new behaviour, arguments captured by the callback are freed
    earlier, which is better, and could avoid shared_ptr loops in some
    cases.
    
    Change-Id: I2d7b2101ee1d34b6f610564a70838cbd581d461a
    Reviewed-on: http://gerrit.aldebaran.lan/84325
    Reviewed-by: vincent.palancher <vincent.palancher@external.softbankrobotics.com>
    Reviewed-by: jlamotte <jlamotte@aldebaran.com>
    Reviewed-by: jmonnon <jmonnon@aldebaran.com>
    Tested-by: jlamotte <jlamotte@aldebaran.com>

--- qi/detail/future.hxx
+++ qi/detail/future.hxx
@@ -181,7 +181,7 @@ namespace detail {
         if (isFinished())
           return;
         requestCancel();
-        onCancel = _onCancel;
+        std::swap(onCancel, _onCancel);
       }
       if (onCancel)
       {
