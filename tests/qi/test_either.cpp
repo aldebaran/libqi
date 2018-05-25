a15908f734a8df9f667646ea063d873ebc31270f
Jeremy Monnon <jmonnon@aldebaran.com>
2018-05-25 17:58:40 +0200

    qi.tool: Adds a `Either` type as a convenient alias to a `variant` of two types.
    Other aliases are provided such as `visit` to apply a polymorphic procedure to
    the content of a variant.
    
    Example: Defining a visitor and applying it.
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    struct Norm : VisitorBase<std::size_t>
    {
      std::size_t operator()(const std::string& x) const
      {
        return x.size();
      }
      std::size_t operator()(int i) const
      {
        return i;
      }
    };
    
    // In a function:
    using E = Either<std::string, int>;
    {
      E e{"abc"};
      std::size_t i = visit(Norm{}, e); // i == 3
    }
    {
      E e{12};
      std::size_t i = visit(Norm{}, e); // i == 12
    }
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Change-Id: Ifb5dec5d548d73bafbef9c95fcf66b2c11c3fbae
    Reviewed-on: http://gerrit.aldebaran.lan/90651
    Reviewed-by: jmonnon <jmonnon@aldebaran.com>
    Tested-by: jmonnon <jmonnon@aldebaran.com>
    Reviewed-by: vincent.palancher <vincent.palancher@external.softbankrobotics.com>
    Reviewed-by: jlamotte <jlamotte@aldebaran.com>
    Tested-by: jlamotte <jlamotte@aldebaran.com>

---
+++ tests/qi/test_either.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <gtest/gtest.h>
+#include <ka/conceptpredicate.hpp>
+#include <qi/either.hpp>
+
+TEST(Either, Regular)
+{
+  using E = qi::Either<std::string, int>;
+  ASSERT_TRUE(ka::is_regular({E{"a"}, E{0}, E{"bc"}, E{12}}));
+}
+
+namespace
+{
+  struct Norm : qi::VisitorBase<std::size_t>
+  {
+    std::size_t operator()(const std::string& x) const
+    {
+      return x.size();
+    }
+    std::size_t operator()(int i) const
+    {
+      return i;
+    }
+  };
+} // namespace
+
+TEST(Either, Visit)
+{
+  using namespace qi;
+  using E = Either<std::string, int>;
+  {
+    E e{"abc"};
+    ASSERT_EQ(3, visit(Norm{}, e));
+  }
+  {
+    E e{12};
+    ASSERT_EQ(12, visit(Norm{}, e));
+  }
+}
