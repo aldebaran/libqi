e537a85730345d3f63fa6f5e21ea7c9595526a72
Vincent Palancher <vincent.palancher@external.softbankrobotics.com>
2018-11-07 16:46:56 +0100

    qi.type: Refactors qi::detail::Less implementation.
    Change-Id: I46b13e9a29b1e901c6fabd41449a4b0c639a0778
    Reviewed-on: http://gerrit.aldebaran.lan/93127
    Reviewed-by: julien.bernard <julien.bernard@external.softbankrobotics.com>
    Reviewed-by: jlamotte <jlamotte@aldebaran.com>
    Reviewed-by: jmonnon <jmonnon@aldebaran.com>
    Reviewed-by: vincent.palancher <vincent.palancher@softbankrobotics.com>
    Tested-by: vincent.palancher <vincent.palancher@softbankrobotics.com>

--- qi/type/detail/hasless.hxx
+++ qi/type/detail/hasless.hxx
@@ -7,99 +7,95 @@
 #ifndef _QITYPE_DETAIL_HASLESS_HXX_
 #define _QITYPE_DETAIL_HASLESS_HXX_
 
+#include <qi/type/typeinterface.hpp>
+#include <ka/macroregular.hpp>
+#include <ka/typetraits.hpp>
+#include <boost/optional.hpp>
 #include <boost/type_traits/has_less.hpp>
+#include <map>
+#include <set>
+#include <tuple>
 
-namespace qi {
-  namespace detail {
+namespace qi
+{
+namespace detail
+{
 
-    template<typename T>
-    struct HasLessGuard;
+  // Some types are not handled by boost::has_less. This trait enables us to detect them and process
+  // them differently.
+  template <typename T>
+  using CanBeUsedInHasLess =
+    ka::Negation<
+      ka::Disjunction<
+        // Functions and function pointers are not handled.
+        std::is_function<ka::RemovePointer<T>>,
+        // Member function pointers are considered as member pointers. Neither member function
+        // pointers nor member object pointers are handled.
+        std::is_member_pointer<T>
+      >
+    >;
 
-    // boost::has_less gives true for a vector<F> even if has_less<F> gives false
-    template<typename T>
-    struct HasLess
+  template <typename T>
+  struct HasLess : ka::Conjunction<CanBeUsedInHasLess<T>, boost::has_less<T, T, bool>> {};
+
+  // The following types for which we define a specialisation of HasLess are types supported by
+  // the qi type system for which the less operator depends on the template parameters.
+  // STL containers use a lexicographical compare of their elements which have the type of the
+  // template parameter.
+  template<typename T, typename Alloc>
+  struct HasLess<std::vector<T, Alloc>> : HasLess<T> {};
+
+  template<typename T, typename Alloc>
+  struct HasLess<std::list<T, Alloc>> : HasLess<T> {};
+
+  // The `less` operator of `std::set` does not use the `Compare` type but instead does a
+  // lexicographical comparison of their elements. Therefore having `less` on `std::set` requires
+  // having `less` on the element type.
+  template<typename T, typename Compare, typename Alloc>
+  struct HasLess<std::set<T, Compare, Alloc>> : HasLess<T> {};
+
+  // The `less` operator of `std::map` does not use the `Compare` type but instead does a
+  // lexicographical comparison of their elements. Therefore having `less` on `std::map` requires
+  // having `less` on the element type (which is a `pair<const K, V>`, so to avoid confusion, we
+  // just refer to the value_type typedef).
+  template <typename K, typename V, typename Compare, typename Alloc>
+  struct HasLess<std::map<K, V, Compare, Alloc>>
+    : HasLess<typename std::map<K, V, Compare, Alloc>::value_type> {};
+
+  template<typename A, typename B>
+  struct HasLess<std::pair<A, B>> : ka::Conjunction<HasLess<A>, HasLess<B>> {};
+
+  template<typename... T>
+  struct HasLess<std::tuple<T...>> : ka::Conjunction<HasLess<T>...> {};
+
+  template<typename T>
+  struct HasLess<boost::optional<T>> : HasLess<T> {};
+
+#if QI_HAS_STD_OPTIONAL
+  template<typename T>
+  struct HasLess<std::optional<T>> : HasLess<T> {};
+#endif
+
+  template<typename T>
+  struct LessDeref
+  {
+  // Regular:
+    KA_GENERATE_FRIEND_REGULAR_OPS_0(LessDeref)
+
+  // Relation:
+    bool operator()(const T& a, const T& b) const
     {
-      static const bool value = boost::has_less<T, T>::value;
-    };
+      return *a < *b;
+    }
+  };
 
-    template<typename T>
-    struct HasLess<std::vector<T> >
-    {
-      static const bool value = HasLessGuard<T>::value;
-    };
+  /// model Procedure<bool(T*, T*)> Less<T>:
+  /// If T is LessComparable, then the pointers are dereferenced and the results are compared.
+  /// Otherwise, the value of the pointers are compared.
+  template<typename T>
+  using Less = ka::Conditional<HasLess<T>::value, LessDeref<T*>, std::less<T*>>;
 
-    template<typename T>
-    struct HasLess<std::list<T> >
-    {
-      static const bool value = HasLessGuard<T>::value;
-    };
-
-    template<typename K, typename V>
-    struct HasLess<std::map<K, V> >
-    {
-      static const bool value = HasLessGuard<K>::value
-        && HasLessGuard<V>::value;
-    };
-
-    template<typename A, typename B>
-    struct HasLess<std::pair<A, B> >
-    {
-      static const bool value = HasLessGuard<A>::value
-        && HasLessGuard<B>::value;
-    };
-
-    // boost::optional<T> has a less operator if and only if T has a less operator.
-    template<typename T>
-    struct HasLess<boost::optional<T>>
-    {
-      static const bool value = HasLessGuard<T>::value;
-    };
-
-    //boost::has_less fails for member function pointer, gard
-    template<typename T, bool v>
-    struct HasLessSwitch
-    {};
-
-    template<typename T>
-    struct HasLessSwitch<T, false>
-    {
-      static const bool value = false;
-    };
-
-    template<typename T> struct HasLessSwitch<T, true>
-    {
-      static const bool value = HasLess<T>::value;
-    };
-
-    template<typename T>
-    struct HasLessGuard
-    {
-      static const bool switchVal =
-          boost::is_member_function_pointer<T>::value
-          || boost::is_function<T>::value
-          || boost::is_function<typename boost::remove_pointer<T>::type>::value
-          || boost::is_member_pointer<T>::value;
-      static const bool value = HasLessSwitch<T, !switchVal>::value;
-
-    };
-
-
-    template<typename T, bool hasLessGuard>
-    struct LessHelper
-    {
-      bool operator()(T* a, T* b) { return *a<*b;}
-    };
-
-    template<typename T>
-    struct LessHelper<T, false>
-    {
-      bool operator()(T*a, T*b) { return a<b;}
-    };
-
-    template<typename T>
-    struct Less: public LessHelper<T, HasLessGuard<T>::value>
-    {};
-  }
-}
+} // namespace detail
+} // namespace qi
 
 #endif  // _QITYPE_DETAIL_HASLESS_HXX_
