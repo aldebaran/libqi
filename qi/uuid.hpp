531c6610f034395d3d3a16d84430ea8762ce368b
Joël LAMOTTE <jlamotte@softbankrobotics.com>
2019-04-10 19:18:43 +0200

    uuid: Improves cryptographical quality of our internal uuid generator.
    "Quality" here is the "quality of randomness" as described on wikipedia:
    https://en.wikipedia.org/wiki/Cryptographically_secure_pseudorandom_number_generator
    
    In Boost 1.64 (which is the currently supported version),
    the default Boost.UUID random generator uses a Mersenne Twister generator
    whose randomness is not considered "cryptographically secure".
    This change replaces that random generator by Boost.Random's
    `random_device` which is guaranteed (per the documentation) to be a
    cryptographically secure random source (of the system).
    
    
    Change-Id: I007191f1cb7c4aa05c8efaa5a3dd06e415947f0e
    Reviewed-on: http://gerrit.aldebaran.lan/94746
    Reviewed-by: jlamotte <jlamotte@aldebaran.com>
    Tested-by: jlamotte <jlamotte@aldebaran.com>

--- qi/uuid.hpp
+++ qi/uuid.hpp
@@ -3,13 +3,21 @@
 #define QI_UUID_HPP
 #include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_generators.hpp>
+#include <boost/random/random_device.hpp>
 
 /// Simple aliases to abstract a bit from implementation.
 
 namespace qi
 {
   using Uuid = boost::uuids::uuid;
-  using UuidRandomGenerator = boost::uuids::random_generator;
+  using UuidRandomGenerator =
+    boost::uuids::basic_random_generator<boost::random::random_device>;
+  // We don't use the default random uuid generator because in Boost < 1.67 it uses
+  // a Mersenne Twister engine which is randomness is not cryptographically secure.
+  // Use Boost.Random 's random device engine as it provides that guarantee.
+  // TODO: Boost >= v1.67 : use either boost::uuids::random_generator
+  // or explicitely boost::uuids::random_generator_pure for optimal case.
+
 } // qi
 
 #endif // QI_UUID_HPP
