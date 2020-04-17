079b2ff4aee2b3dd1d823d8469976bc99fc18968
Jeremy Monnon <j@par-1332-la.aldebaran.lan>
2020-04-17 15:12:32 +0200

    ka.conceptpredicate: Adds the `is_functor` function.
    True if the type is a mathematical functor for all the passed pairs of
    composable functions.
    
    A mathematical functor is a function `X` that sends morphisms `A -> B` to
    morphisms `X(A) -> X(B)`, preserving composition. In our case, the mathematical
    functor translates to a polymorphic function that "lifts" functions `A -> B` to
    functions `X<A> -> X<B>` preserving function composition, where `X` models the
    `Functor` concept (see `concept.hpp`).
    
    This function lifts passed functions, then compares them through the given
    equivalence.
    
    Example: Checking that a type template models the `Functor` concept.
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    // Pair of composable functions.
    auto const f = [](A a) -> B {return B{2 * a.value};};
    auto const g = [](B b) -> C {return C{b.value + 1};};
    
    Here, `ka::fmap` will be used to (generically) lift `f` and `g`.
    `my_functor_fn_equ` will be called with pairs of lifted functions and
    decide if they are equivalent or not (for instance, by approximating
    extensional equality and comparing functions outputs for a sample of
    inputs). It is `my_functor_fn_equ` that determines which actual model of
    `Functor` is tested, by calling the generically lifted functions with
    appropriate `Functor` values (here of some `my_functor_t<T>` type).
    
    EXPECT_TRUE(is_functor(
      ka::fmap,                    // function lifting
      array_fn(ka::product(g, f)), // pairs of composable functions
      my_functor_fn_equ            // equivalence of lifted functions
    ));
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Change-Id: Ib615e06aa1693d0df32ea0bfc4a6d0036b1fa94d
    Reviewed-on: http://gerrit.aldebaran.lan/97452
    Reviewed-by: jmonnon <jmonnon@aldebaran.com>
    Reviewed-by: vincent.palancher <vincent.palancher@softbankrobotics.com>
    Reviewed-by: jlamotte <jlamotte@aldebaran.com>
    Tested-by: jmonnon <jmonnon@aldebaran.com>

--- ka/conceptpredicate.hpp
+++ ka/conceptpredicate.hpp
@@ -5,6 +5,7 @@
 #include <functional>
 #include <utility>
 #include "concept.hpp"
+#include "functional.hpp"
 #include "macro.hpp"
 #include "range.hpp"
 #include "relationpredicate.hpp"
@@ -55,6 +56,62 @@ namespace ka {
   template<typename T>
   bool is_regular(std::initializer_list<T> l) {
     return is_regular(bounded_range(l));
+  }
+
+  /// True if the type is a mathematical functor for all the passed pairs of
+  /// composable functions.
+  ///
+  /// A mathematical functor is a function `X` that sends morphisms `A -> B` to
+  /// morphisms `X(A) -> X(B)`, preserving composition. In our case, the
+  /// mathematical functor translates to a polymorphic function that "lifts"
+  /// functions `A -> B` to functions `X<A> -> X<B>` preserving function
+  /// composition, where `X` models the `Functor` concept (see `concept.hpp`).
+  ///
+  /// This function lifts passed functions, then compares them through the given
+  /// equivalence.
+  ///
+  /// Example: Checking that a type template models the `Functor` concept.
+  /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  /// // Pair of composable functions.
+  /// auto const f = [](A a) -> B {return B{2 * a.value};};
+  /// auto const g = [](B b) -> C {return C{b.value + 1};};
+  ///
+  /// Here, `ka::fmap` will be used to (generically) lift `f` and `g`.
+  /// `my_functor_fn_equ` will be called with pairs of lifted functions and
+  /// decide if they are equivalent or not (for instance, by approximating
+  /// extensional equality and comparing functions outputs for a sample of
+  /// inputs). It is `my_functor_fn_equ` that determines which actual model of
+  /// `Functor` is tested, by calling the generically lifted functions with
+  /// appropriate `Functor` values (here of some `my_functor_t<T>` type).
+  ///
+  /// EXPECT_TRUE(is_functor(
+  ///   ka::fmap,                    // function lifting
+  ///   array_fn(ka::product(g, f)), // pairs of composable functions
+  ///   my_functor_fn_equ            // equivalence of lifted functions
+  /// ));
+  /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  ///
+  /// Precondition: equivalence(e)
+  ///
+  /// Function<Function<X<U> (X<T>)> (Function<U (T)>)> F (with Functor X, forall Type T, U)
+  /// Linearizable<Tuple<Function<C (B)>, Function<B (A)>>> L
+  /// Relation<Function<C (A)>> R
+  template<typename F, typename L, typename R = equal_t>
+  auto is_functor(F X, L composable_functions, R e = equal) -> bool {
+    using functional_ops::operator*; // Function composition.
+
+    auto const _1 = id_transfo_t{};
+
+    // Lifting of identity is the same as identity.
+    if (! e(X(_1), _1)) return false;
+
+    // Lifting of composition is the same as composition of lifting.
+    for (auto const& pair: composable_functions) {
+      auto const& g = std::get<0>(pair);
+      auto const& f = std::get<1>(pair);
+      if (! e(X(g * f), X(g) * X(f))) return false;
+    }
+    return true;
   }
 } // namespace ka
 
--- tests/ka/test_conceptpredicate.cpp
+++ tests/ka/test_conceptpredicate.cpp
@@ -1,9 +1,12 @@
 #include <ka/conceptpredicate.hpp>
 #include <ka/relationpredicate.hpp>
 #include "test_relations.hpp"
+#include <ka/functional.hpp>
+#include <ka/functor.hpp>
 #include <ka/range.hpp>
 #include <ka/typetraits.hpp>
 #include <ka/macroregular.hpp>
+#include <ka/testutils.hpp>
 #include <gtest/gtest.h>
 #include <cstdint>
 
@@ -15,7 +18,7 @@
 
 // This file contains tests on concept predicates, for example `is_regular`.
 
-namespace test {
+namespace test_conceptpredicate {
   template<typename T>
   bool operator!=(T const& a, T const& b) {
     return !(a == b);
@@ -112,7 +115,7 @@ namespace test {
     }
   } incr_member_x;
 
-} // namespace test
+} // namespace test_conceptpredicate
 
 // Spaces cause problems in the macros (e.g EXPECT_TRUE).
 using longlong = long long;
@@ -144,7 +147,7 @@ TEST(ConceptPredicate, IsRegularBuiltinTypes) {
 
 TEST(ConceptPredicate, IsRegularUserDefinedTypes) {
   using namespace ka;
-  using namespace test;
+  using namespace test_conceptpredicate;
 
   EXPECT_TRUE(is_regular(bounded_range(regular_t{0}, regular_t{10}, incr_member_x)));
   {
@@ -157,7 +160,9 @@ TEST(ConceptPredicate, IsRegularUserDefinedTypes) {
   EXPECT_FALSE(is_regular(bounded_range(bad_copy_t{0}, bad_copy_t{10}, incr_member_x)));
 }
 
-namespace test {
+namespace test_conceptpredicate {
+  using namespace test_relation;
+
   // Allows specifying equality and total ordering relations.
   template<typename Eq, typename Lt> // Relation<entity_t> Eq, Lt
   struct pseudo_regular_t {
@@ -186,11 +191,11 @@ namespace test {
     using T = pseudo_regular_t<Eq, Lt>;
     return ka::bounded_range(T{entity_begin}, T{entity_end}, incr_member_x);
   }
-} // namespace test
+} // namespace test_conceptpredicate
 
 TEST(ConceptPredicate, IsRegularWrongEquality) {
   using namespace ka;
-  using namespace test;
+  using namespace test_conceptpredicate;
   EXPECT_FALSE(is_regular(pseudo_regular_entity_range<not_reflexive_not_symmetric_not_transitive_t>()));
   EXPECT_FALSE(is_regular(pseudo_regular_entity_range<not_reflexive_symmetric_not_transitive_t>()));
   EXPECT_FALSE(is_regular(pseudo_regular_entity_range<reflexive_symmetric_not_transitive_t>()));
@@ -208,7 +213,7 @@ TEST(ConceptPredicate, IsRegularWrongEquality) {
 
 TEST(ConceptPredicate, IsRegularWrongOrdering) {
   using namespace ka;
-  using namespace test;
+  using namespace test_conceptpredicate;
   using Eq = std::equal_to<entity_t>;
   EXPECT_FALSE(is_regular(pseudo_regular_entity_range<Eq, std::less_equal<entity_t>>()));
   EXPECT_FALSE(is_regular(pseudo_regular_entity_range<Eq, not_trichotomic_not_transitive_t>()));
@@ -218,8 +223,144 @@ TEST(ConceptPredicate, IsRegularWrongOrdering) {
 
 TEST(ConceptPredicate, IsRegularCorrectOrdering) {
   using namespace ka;
-  using namespace test;
+  using namespace test_conceptpredicate;
   using Eq = std::equal_to<entity_t>;
   EXPECT_TRUE(is_regular(pseudo_regular_entity_range<Eq, std::less<entity_t>>()));
   EXPECT_TRUE(is_regular(pseudo_regular_entity_range<Eq, trichotomic_transitive_t>()));
+}
+
+namespace test_conceptpredicate {
+  // Simple test functor.
+  template<typename A>
+  struct my_functor_t {
+    A value;
+  // Regular:
+    KA_GENERATE_FRIEND_REGULAR_OPS_1(my_functor_t, value)
+  // Functor:
+    // Function<B (A)> F
+    template<typename F>
+    auto fmap(F f) const -> my_functor_t<ka::CodomainFor<F, A>> {
+      return {f(value)};
+    }
+  };
+
+  template<typename A>
+  auto my_functor(A a) -> my_functor_t<A> {
+    return {a};
+  }
+
+  using ka::test::A;
+  using ka::test::B;
+  using ka::test::C;
+
+  struct a_to_b_fn_t {
+    int i;
+    auto operator()(A a) const -> B {
+      return B{a.value + i};
+    }
+  };
+
+  struct b_to_c_fn_t {
+    int i;
+    auto operator()(B b) const -> C {
+      return C{b.value * i};
+    }
+  };
+
+  // Compares functions of `my_functor_t`.
+  struct my_functor_fn_equ_t {
+    // Function<my_functor_t<C> (my_functor_t<A>)> F, G
+    template<typename F, typename G>
+    auto operator()(F f, G g) const -> bool {
+      for (auto i = -100; i != 100; ++i) {
+        auto x = my_functor_t<A>{A{i}};
+        if (f(x) != g(x)) return false;
+      }
+      return true;
+    }
+  } my_functor_fn_equ;
+
+  // Incorrect lift: identity specialization is incorrect.
+  template<typename F>
+  struct incorrect_lifted_id_t {
+    F f;
+    template<typename T>
+    auto operator()(my_functor_t<T> x) const -> decltype(my_functor(f(x.value))) {
+      return my_functor(f(x.value)); // Ok.
+    }
+  };
+
+  template<>
+  struct incorrect_lifted_id_t<ka::id_transfo_t> {
+    ka::id_transfo_t f; // ignored
+    auto operator()(my_functor_t<A> x) const -> my_functor_t<A> {
+      return my_functor(A{x.value.value + 1}); // Incorrect: not identity.
+    }
+  };
+
+  // Transforms a function T -> U to a function
+  // my_functor_t<T> -> my_functor_t<U>.
+  struct incorrect_lift_id_t {
+    template<typename F>
+    auto operator()(F f) const -> incorrect_lifted_id_t<F> {
+      return {f};
+    }
+  };
+
+  // Incorrect lift: `a_to_b_fn_t` specialization is incorrect.
+  template<typename F>
+  struct incorrect_lifted_compo_t {
+    F f;
+    template<typename T>
+    auto operator()(my_functor_t<T> x) const -> decltype(my_functor(f(x.value))) {
+      return my_functor(f(x.value)); // Ok.
+    }
+  };
+
+  template<>
+  struct incorrect_lifted_compo_t<a_to_b_fn_t> {
+    a_to_b_fn_t f; // ignored
+    auto operator()(my_functor_t<A> x) const -> my_functor_t<B> {
+      return my_functor(f(A{x.value.value * 2})); // Incorrect
+    }
+  };
+
+  // Transforms a function T -> U to a function
+  // my_functor_t<T> -> my_functor_t<U>.
+  struct incorrect_lift_compo_t {
+    template<typename F>
+    auto operator()(F f) const -> incorrect_lifted_compo_t<F> {
+      return {f};
+    }
+  };
+
+  auto is_correct(ka::fmap_fn_t) -> bool {return true;}
+  auto is_correct(incorrect_lift_id_t) -> bool {return false;}
+  auto is_correct(incorrect_lift_compo_t) -> bool {return false;}
+
+  using lift_types = testing::Types<
+    ka::fmap_fn_t, incorrect_lift_id_t, incorrect_lift_compo_t
+  >;
+} // namespace test_conceptpredicate
+
+template<typename T>
+struct ConceptPredicateIsFunctor : testing::Test {};
+TYPED_TEST_CASE(ConceptPredicateIsFunctor, test_conceptpredicate::lift_types);
+
+TYPED_TEST(ConceptPredicateIsFunctor, Basic) {
+  using namespace ka;
+  using namespace test;
+  using namespace test_conceptpredicate;
+  using F = a_to_b_fn_t;
+  using G = b_to_c_fn_t;
+  auto lift = TypeParam{};
+
+  EXPECT_EQ(
+    is_correct(lift),
+    is_functor(
+      lift,
+      array_fn(product(G{2}, F{3}), product(G{1}, F{0}), product(G{-1}, F{7})),
+      my_functor_fn_equ
+    )
+  );
 }
