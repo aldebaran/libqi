65218b08611ec8354f2a4b21c6fba521021fa425
Victor Paleologue <victor.paleologue@softbankrobotics.com>
2021-07-22 12:23:56 +0200

    qi.type: Uses `std::is_fundamental` instead of boost's.
    This fixes a build issue with more recent versions of boost
    (tested on v1.71).
    
    Change-Id: I67b60ffcdc20e1b13c7095016e1e1dfcc8c38772
    Reviewed-on: http://gerrit2.aldebaran.lan/1450
    Reviewed-by: vincent.palancher <vincent.palancher@softbankrobotics.com>
    Reviewed-by: jmonnon <jmonnon@aldebaran.com>
    Reviewed-by: gerrit <gerrit@aldebaran-robotics.com>
    Tested-by: vincent.palancher <vincent.palancher@softbankrobotics.com>

--- qi/type/detail/anyfunctionfactory.hxx
+++ qi/type/detail/anyfunctionfactory.hxx
@@ -7,6 +7,7 @@
 #ifndef _QITYPE_DETAIL_ANYFUNCTIONFACTORY_HXX_
 #define _QITYPE_DETAIL_ANYFUNCTIONFACTORY_HXX_
 
+#include <type_traits>
 #include <boost/mpl/for_each.hpp>
 #include <boost/mpl/transform_view.hpp>
 #include <boost/mpl/find_if.hpp>
@@ -20,7 +21,6 @@
 #include <boost/type_traits/add_pointer.hpp>
 #include <boost/type_traits/remove_const.hpp>
 #include <boost/type_traits/remove_pointer.hpp>
-#include <boost/type_traits/is_fundamental.hpp>
 #include <boost/type_traits/is_member_function_pointer.hpp>
 #include <boost/function_types/function_type.hpp>
 #include <boost/function_types/function_arity.hpp>
@@ -81,8 +81,8 @@ KA_WARNING_DISABLE(, noexcept-type)
     template<typename T>
     struct EqTypeBase<T, true>
     {
-      using type = typename boost::mpl::if_<typename boost::is_fundamental<T>::type, void*, T>::type;
-      using rType =  typename boost::mpl::if_<typename boost::is_fundamental<T>::type, void*, T>::type;
+      using type = typename boost::mpl::if_<typename std::is_fundamental<T>::type, void*, T>::type;
+      using rType =  typename boost::mpl::if_<typename std::is_fundamental<T>::type, void*, T>::type;
       using isReference = typename boost::is_reference<T>::type;
       static const int dbgTag = 1;
     };
