d37afcb38712d75228474e6d4f99fde9f59a9c55
Victor Paleologue <victor.paleologue@softbankrobotics.com>
2021-09-08 16:11:34 +0200

    github.workflow: Fixes qitoolchain feed.
    Packages were missing, and the original feed appeared after the package,
    making the package improperly reference its dependencies via RUNPATH.
    
    Change-Id: Iee8570bb7e95d380dc7d8a3c2d6570d9f36d8d44

--- .github/workflows/make_feed.sh
+++ .github/workflows/make_feed.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 # Retrieves the latest release output to generate a QiToolchain XML feed.
 set -e
-ASSETS_URL=$(curl --silent "${GITHUB_API_URL}/repos/${$GITHUB_REPOSITORY}/releases/latest" |
+echo "Getting packages from current release at ${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/releases/latest"
+ASSETS_URL=$(curl --silent "${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/releases/latest" |
     jq -r '.assets_url')
 PACKAGES_URLS=$(curl --silent ${ASSETS_URL} |
     jq '.[] | .browser_download_url' |
     grep .zip | xargs -L 1 echo) # echo removes the extra quotes
 
+echo "Making a toolchain feed."
 echo "<toolchain>" > feed.xml
+echo "Adding sub-toolchain from ${FEED}"
+echo "  <feed url=\"${FEED}\" />" >> feed.xml
+echo "Adding links for the following packages:" ${PACKAGES_URLS}
 for URL in $PACKAGES_URLS; do
     ZIP=package.zip
     wget -q $URL -O $ZIP;
@@ -16,7 +21,6 @@ for URL in $PACKAGES_URLS; do
     echo "  <package name=\"${NAME}\" version=\"${VERSION}\" url=\"${URL}\" />" >> feed.xml
     rm $ZIP
 done
-echo "  <feed url=\"${FEED}\" />" >> feed.xml
 echo "</toolchain>" >> feed.xml
 
 realpath feed.xml
